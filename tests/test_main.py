54273b03a74bed55127b5e171e85e9e73262e012
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-02-05 15:22:22 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,9 +4,11 @@ from unittest import mock
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_prediction_is_not_substantive(mocked_prediction):
-    mocked_prediction.return_value = 3.15
+    mocked_prediction.return_value = 3
 
     assert cryptocurrency_action(3) == "Do nothing"
+    assert cryptocurrency_action(3.1546) == "Do nothing"
+    assert cryptocurrency_action(2.8598) == "Do nothing"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
@@ -21,5 +23,4 @@ def test_prediction_to_buy_more_cryptocurrency(mocked_prediction):
 def test_prediction_to_sell_cryptocurrency(mocked_prediction):
     mocked_prediction.return_value = 1
 
-    assert cryptocurrency_action(0.953) == "Sell all your cryptocurrency"
-    assert cryptocurrency_action(1.1) == "Sell all your cryptocurrency"
+    assert cryptocurrency_action(1.0537) == "Sell all your cryptocurrency"
