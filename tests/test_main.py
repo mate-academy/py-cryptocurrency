83c3591a3162f826b53b8b4c5653298127fc5711
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-06 00:47:44 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,12 @@
-# write your code here
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_rate_of_100(mocked_get_exchange):
+    mocked_get_exchange.return_value = 1040
+
+    cryptocurrency_action(1000)
+
+    mocked_get_exchange.assert_called_once()
