ac0d0f96ec7251deef8238fa6c74fccd6f2d38f4
Alex Zhidkov <sashazudkov.@gmail.com>
2022-02-06 13:02:22 +0200

    fix name test

--- tests/test_main.py
+++ tests/test_main.py
@@ -35,7 +35,7 @@ from app.main import cryptocurrency_action
     ],
 )
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_rate_decrease(
+def test_cryptocurrency_action(
         mocked_exchange_rate,
         exchange_rate,
         current_rate,
