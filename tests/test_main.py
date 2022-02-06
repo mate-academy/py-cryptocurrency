a282e9510d7f85bf8d5023ef2b3dce4034cebc57
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-06 23:25:26 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -22,7 +22,7 @@ from app.main import cryptocurrency_action
     ]
 )
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_rate_of_100(mocked_get_exchange, initial_value, expected_prediction):
+def test_rate_prediction(mocked_get_exchange, initial_value, expected_prediction):
     mocked_get_exchange.return_value = initial_value
 
     assert cryptocurrency_action(1000) == expected_prediction
