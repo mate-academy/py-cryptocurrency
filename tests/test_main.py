ac3cfa4c6357263235b75f38e8d6d31ec84ecbce
TJgkOsVP <sasha06052001@gmail.com>
2022-02-06 19:48:04 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,21 +3,24 @@ from unittest import mock
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_prediction_buy_more_cryptocurrency(mocked_get_exchange_rate_prediction):
-    mocked_get_exchange_rate_prediction.return_value = 10.2
+def test_prediction_buy_more_cryptocurrency(mocked_get_exchange_rate):
+    mocked_get_exchange_rate.return_value = 10.2
     current_rate = 2.3
-    assert cryptocurrency_action(current_rate) == "Buy more cryptocurrency"
+    assert cryptocurrency_action(current_rate) ==\
+           "Buy more cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_prediction_sell_all_cryptocurrency(mocked_get_exchange_rate_prediction):
-    mocked_get_exchange_rate_prediction.return_value = 4.1
+def test_sell_all_cryptocurrency(mocked_get_exchange_rate):
+    mocked_get_exchange_rate.return_value = 4.1
     current_rate = 5.1
-    assert cryptocurrency_action(current_rate) == "Sell all your cryptocurrency"
+    assert cryptocurrency_action(current_rate) ==\
+           "Sell all your cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_prediction_do_nothing(mocked_get_exchange_rate_prediction):
-    mocked_get_exchange_rate_prediction.return_value = 8
+def test_prediction_do_nothing(mocked_get_exchange_rate):
+    mocked_get_exchange_rate.return_value = 8
     current_rate = 8
-    assert cryptocurrency_action(current_rate) == "Do nothing"
+    assert cryptocurrency_action(current_rate) ==\
+           "Do nothing"
