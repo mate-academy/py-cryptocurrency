b2d6150b4e583a3c9308d9db6884416cee54c0b0
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-06 21:27:39 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,31 @@
 from unittest import mock
+import pytest
 
 from app.main import cryptocurrency_action
 
 
+@pytest.mark.parametrize(
+    "initial_value,expected_prediction",
+    [
+        (
+            1050,
+            "Do nothing"
+        ),
+        (
+            1000,
+            "Sell all your cryptocurrency"
+        ),
+        (
+            1100,
+            "Buy more cryptocurrency"
+        )
+    ]
+)
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_rate_of_100(mocked_get_exchange):
-    mocked_get_exchange.return_value = 1040
+def test_rate_of_100(mocked_get_exchange, initial_value, expected_prediction):
+    mocked_get_exchange.return_value = initial_value
 
     cryptocurrency_action(1000)
 
     mocked_get_exchange.assert_called_once()
+    assert cryptocurrency_action(1000) == expected_prediction
