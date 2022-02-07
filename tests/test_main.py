e2871b1a25dc6a2f5dad0868854a9261878b1798
nikon4uk <nikonchukmax4@gmail.com>
2022-02-07 16:28:29 +0200

    Solution with group

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,18 +3,15 @@ from app.main import cryptocurrency_action
 
 
 @mock.patch('app.main.get_exchange_rate_prediction')
-def test_call_to_buy_cryptocurrency(mock_exchange_rate):
-    mock_exchange_rate.return_value = 11
-    assert cryptocurrency_action(10) == "Buy more cryptocurrency"
+class TestActionCalls:
+    def test_call_to_buy_cryptocurrency(self, mock_exchange_rate_prediction):
+        mock_exchange_rate_prediction.return_value = 11
+        assert cryptocurrency_action(10) == "Buy more cryptocurrency"
 
+    def test_call_to_sell_cryptocurrency(self, mock_exchange_rate_prediction):
+        mock_exchange_rate_prediction.return_value = 9
+        assert cryptocurrency_action(10) == "Sell all your cryptocurrency"
 
-@mock.patch('app.main.get_exchange_rate_prediction')
-def test_call_to_sell_cryptocurrency(mock_exchange_rate):
-    mock_exchange_rate.return_value = 9
-    assert cryptocurrency_action(10) == "Sell all your cryptocurrency"
-
-
-@mock.patch('app.main.get_exchange_rate_prediction')
-def test_call_to_hold_cryptocurrency(mock_exchange_rate):
-    mock_exchange_rate.return_value = 10
-    assert cryptocurrency_action(10) == "Do nothing"
+    def test_call_to_hold_cryptocurrency(self, mock_exchange_rate_prediction):
+        mock_exchange_rate_prediction.return_value = 10
+        assert cryptocurrency_action(10) == "Do nothing"
