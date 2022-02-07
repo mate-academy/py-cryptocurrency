2f8070d0d80ec4737cd63ae3e108da4507dcadda
Alex Gergel <alexgergel@gmail.com>
2022-02-07 10:04:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import random
 def get_exchange_rate_prediction(exchange_rate):
     if random.choice(["increase", "decrease"]) == "increase":
         return round(exchange_rate / random.random(), 2)
+
     else:
         return round(exchange_rate * random.random(), 2)
 
@@ -12,6 +13,8 @@ def cryptocurrency_action(current_rate):
     prediction_rate = get_exchange_rate_prediction(current_rate)
     if prediction_rate / current_rate > 1.05:
         return "Buy more cryptocurrency"
+
     if prediction_rate / current_rate < 0.95:
         return "Sell all your cryptocurrency"
+    print("Do nothing")
     return "Do nothing"
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,19 @@
-# write your code here
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_suggested_actions_in_predicted_exchange_rate_windows(mocked_prediction):
+    mocked_prediction.return_value = 100
+
+    assert cryptocurrency_action(102) == "Do nothing"
+    assert cryptocurrency_action(104) == "Do nothing"
+    assert cryptocurrency_action(96) == "Do nothing"
+    assert cryptocurrency_action(118) == "Sell all your cryptocurrency"
+    assert cryptocurrency_action(143) == "Sell all your cryptocurrency"
+    assert cryptocurrency_action(123) == "Sell all your cryptocurrency"
+    assert cryptocurrency_action(90) == "Buy more cryptocurrency"
+    assert cryptocurrency_action(75) == "Buy more cryptocurrency"
+    assert cryptocurrency_action(23) == "Buy more cryptocurrency"
+
