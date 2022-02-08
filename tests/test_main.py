9888d81a1dcd46b52846a3f0bbca6eb195776cf2
alix makarova <alix.makarova@gmail.com>
2022-02-08 14:58:02 +0200

    added test for value equals exactly 1.05 or 0.95

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,23 +4,19 @@ from app.main import cryptocurrency_action
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_if_growth_higher_five_percent(mocked_get_exchange_rate_prediction):
-    mocked_get_exchange_rate_prediction.return_value = 41870
-
-    assert cryptocurrency_action(39876) == "Buy more cryptocurrency"
-    assert cryptocurrency_action(1) == "Buy more cryptocurrency"
+    mocked_get_exchange_rate_prediction.return_value = 1
+    assert cryptocurrency_action(0.5) == "Buy more cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_if_decline_lower_five_percent(mocked_get_exchange_rate_prediction):
-    mocked_get_exchange_rate_prediction.return_value = 41870
-
-    assert cryptocurrency_action(44074) == "Sell all your cryptocurrency"
-    assert cryptocurrency_action(60000) == "Sell all your cryptocurrency"
+    mocked_get_exchange_rate_prediction.return_value = 1
+    assert cryptocurrency_action(1.5) == "Sell all your cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_if_difference_is_not_that_much(mocked_get_exchange_rate_prediction):
-    mocked_get_exchange_rate_prediction.return_value = 41870
-
-    assert cryptocurrency_action(39877) == "Do nothing"
-    assert cryptocurrency_action(44073) == "Do nothing"
+    mocked_get_exchange_rate_prediction.return_value = 1
+    assert cryptocurrency_action(1.05) == "Do nothing"
+    mocked_get_exchange_rate_prediction.return_value = 0.95
+    assert cryptocurrency_action(1) == "Do nothing"
