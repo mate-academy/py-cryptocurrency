13243e9bf82057e6e5679c749e9ebd9e9669715b
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-02-09 07:40:37 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -11,19 +11,11 @@ def get_exchange_rate_prediction(exchange_rate):
 def cryptocurrency_action(current_rate):
     prediction_rate = get_exchange_rate_prediction(current_rate)
     if prediction_rate / current_rate > 1.05:
-        print(prediction_rate, current_rate)
-        print(prediction_rate / current_rate)
         return "Buy more cryptocurrency"
     if prediction_rate / current_rate < 0.95:
-        print(prediction_rate, current_rate)
-        print(prediction_rate / current_rate)
         return "Sell all your cryptocurrency"
-    print(prediction_rate, current_rate)
-    print(prediction_rate / current_rate)
     return "Do nothing"
 
 
 if __name__ == "__main__":
     print(cryptocurrency_action(10))
-
-
--- tests/test_main.py
+++ tests/test_main.py
@@ -14,6 +14,10 @@ def test_do_nothing(prediction):
     assert cryptocurrency_action(10) == "Do nothing"
     prediction.return_value = 10.5
     assert cryptocurrency_action(10) == "Do nothing"
+    prediction.return_value = 1.05
+    assert cryptocurrency_action(1.00) == "Do nothing"
+    prediction.return_value = 0.95
+    assert cryptocurrency_action(1.00) == "Do nothing"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
