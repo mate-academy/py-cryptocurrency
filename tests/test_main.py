da7eb3485187ba571119a7139547a4bbce94794b
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-02-10 18:20:22 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ from app.main import cryptocurrency_action
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_prediction_is_higher_exactly_5(mocked_prediction):
-    mocked_prediction.return_value = 1.5
+    mocked_prediction.return_value = 1.05
 
     assert cryptocurrency_action(1.0) == "Do nothing"
 
@@ -21,7 +21,6 @@ def test_prediction_is_lower_exactly_5(mocked_prediction):
 def test_prediction_to_buy_more_cryptocurrency(mocked_prediction):
     mocked_prediction.return_value = 2
 
-    assert cryptocurrency_action(1.9029) == "Buy more cryptocurrency"
     assert cryptocurrency_action(1.5) == "Buy more cryptocurrency"
 
 
@@ -29,4 +28,4 @@ def test_prediction_to_buy_more_cryptocurrency(mocked_prediction):
 def test_prediction_to_sell_cryptocurrency(mocked_prediction):
     mocked_prediction.return_value = 1
 
-    assert cryptocurrency_action(1.0537) == "Sell all your cryptocurrency"
+    assert cryptocurrency_action(2) == "Sell all your cryptocurrency"
