b17ebea8f46ee9d69370bb038120fa3d1379a035
Rybak Dmytro <codingclaud@gmail.com>
2022-02-12 18:17:47 +0200

    'Tests_updated'

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,9 +4,9 @@ from app.main import cryptocurrency_action
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_if_predicated_exchange_rate_bigger_then_5(mocked_exchange_rate):
-    mocked_exchange_rate.return_value = 1.49
+    mocked_exchange_rate.return_value = 1
 
-    assert cryptocurrency_action(1.49) == "Sell all your cryptocurrency"
+    assert cryptocurrency_action(2) == "Sell all your cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
@@ -18,6 +18,6 @@ def test_if_predicated_exchange_rate_lower_than_5(mocked_exchange_rate):
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_if_predicated_exchange_rate_difference_low(mocked_exchange_rate):
-    mocked_exchange_rate.return_value = 2.1
+    mocked_exchange_rate.return_value = 1.05
 
-    assert cryptocurrency_action(2) == "Do nothing"
+    assert cryptocurrency_action(1.0) == "Do nothing"
