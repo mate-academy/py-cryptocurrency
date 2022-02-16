4f0c9de14738bad379e469a74eac4a607c3ed299
Zikkolion <nmadaraosnova835@gmail.com>
2022-02-16 09:03:30 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,19 @@
-# write your code here
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+@mock.patch('app.main.get_exchange_rate_prediction')
+class TestActionCalls:
+    def test_call_to_buy_cryptocurrency(self, mock_exchange_rate_prediction):
+        mock_exchange_rate_prediction.return_value = 11
+        assert cryptocurrency_action(10) == "Buy more cryptocurrency"
+
+    def test_call_to_sell_cryptocurrency(self, mock_exchange_rate_prediction):
+        mock_exchange_rate_prediction.return_value = 9
+        assert cryptocurrency_action(10) == "Sell all your cryptocurrency"
+
+    def test_call_to_hold_cryptocurrency(self, mock_exchange_rate_prediction):
+        mock_exchange_rate_prediction.return_value = 10
+        assert cryptocurrency_action(10 / 0.95) == "Do nothing"
+        assert cryptocurrency_action(10) == "Do nothing"
+        assert cryptocurrency_action(10 / 1.05) == "Do nothing"
