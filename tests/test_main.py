e5066e1e5da8b1d6824d3cbe18b2f83bfbee3878
Rybak Dmytro <codingclaud@gmail.com>
2022-02-21 12:57:42 +0200

    'Test_added_but_it_not_pass'

--- tests/test_main.py
+++ tests/test_main.py
@@ -27,4 +27,4 @@ def test_if_predicated_exchange_rate_difference_low(mocked_exchange_rate):
 def test_prediction_is_lower_by_5_percent(mocked_exchange_rate):
     mocked_exchange_rate.return_value = 0.95
 
-    assert cryptocurrency_action(1.0) == "Sell all your cryptocurrency"
+    assert cryptocurrency_action(1.0) == "Do nothing"
