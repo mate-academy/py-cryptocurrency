366a65a19bc4cb7eda68957e60ffe6108b31485d
Serhii Leonenko <leonenko3149@gmail.com>
2022-06-05 10:16:48 +0300

    refactored

--- app/test_main.py
+++ app/test_main.py
@@ -1,40 +1,49 @@
+import pytest
 from unittest import mock
 
 from app.main import cryptocurrency_action
 
 
-@mock.patch("app.main.get_exchange_rate_prediction")
-def test_rate_prediction_less_then_5_percent_down(mocked_rate_prediction):
-    current_rate = 100
-    mocked_rate_prediction.return_value = 95
+class TestCryptocurrencyAction:
 
-    assert cryptocurrency_action(current_rate) == "Do nothing"
-    mocked_rate_prediction.assert_called_once_with(current_rate)
+    @pytest.mark.parametrize(
+        "current_rate, predicted_rate, action",
+        [
+            pytest.param(
+                100,
+                95,
+                "Do nothing",
+                id="Rate prediction less then 5 percent down"
+            ),
+            pytest.param(
+                100,
+                105,
+                "Do nothing",
+                id="Rate prediction less then 5 percent up"
+            ),
+            pytest.param(
+                100,
+                150,
+                "Buy more cryptocurrency",
+                id="Rate prediction will up"
+            ),
+            pytest.param(
+                100,
+                50,
+                "Sell all your cryptocurrency",
+                id="Rate prediction will down"
+            )
+        ]
+    )
+    @mock.patch("app.main.get_exchange_rate_prediction")
+    def test_cryptocurrency_action_should_give_correct_action(
+            self,
+            mocked_rate_prediction,
+            current_rate,
+            predicted_rate,
+            action
+    ):
+        mocked_rate_prediction.return_value = predicted_rate
 
-
-@mock.patch("app.main.get_exchange_rate_prediction")
-def test_rate_prediction_less_then_5_percent_up(mocked_rate_prediction):
-    current_rate = 100
-    mocked_rate_prediction.return_value = 105
-
-    assert cryptocurrency_action(current_rate) == "Do nothing"
-    mocked_rate_prediction.assert_called_once_with(current_rate)
-
-
-@mock.patch("app.main.get_exchange_rate_prediction")
-def test_rate_prediction_will_up(mocked_rate_prediction):
-    current_rate = 100
-    mocked_rate_prediction.return_value = 150
-
-    assert cryptocurrency_action(current_rate) == "Buy more cryptocurrency"
-    mocked_rate_prediction.assert_called_once_with(current_rate)
-
-
-@mock.patch("app.main.get_exchange_rate_prediction")
-def test_rate_prediction_will_down(mocked_rate_prediction):
-    current_rate = 100
-    mocked_rate_prediction.return_value = 50
-
-    assert cryptocurrency_action(current_rate) \
-           == "Sell all your cryptocurrency"
-    mocked_rate_prediction.assert_called_once_with(current_rate)
+        assert cryptocurrency_action(current_rate) == action
+        mocked_rate_prediction.assert_called_once_with(current_rate)
