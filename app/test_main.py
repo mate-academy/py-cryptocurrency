2baa87e672fd9112eda59c90f50f0d6e4e63fa3e
Daria <darizhur.vol2@gmail.com>
2022-06-06 13:27:19 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,43 @@
-# write your code here
+from unittest import mock
+import pytest
+from app.main import cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    "current_rate,exchange_rate,message",
+    [
+        pytest.param(
+            100,
+            150,
+            "Buy more cryptocurrency",
+            id="Predict exchange rate is more than 5% higher"
+        ),
+        pytest.param(
+            100,
+            50,
+            "Sell all your cryptocurrency",
+            id="Predict exchange rate is more than 5% lower"
+        ),
+        pytest.param(
+            100,
+            95,
+            "Do nothing",
+            id="You should not sell cryptocurrency"
+        ),
+        pytest.param(
+            100,
+            105,
+            "Do nothing",
+            id="You should not buy cryptocurrency"
+        )
+    ]
+)
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_should_return_correct_message(
+        mock_exchange,
+        current_rate,
+        exchange_rate,
+        message
+):
+    mock_exchange.return_value = exchange_rate
+    assert cryptocurrency_action(current_rate) == message
