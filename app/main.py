697ecba48561cdc92c4d01f5b25a9a895e7b76e8
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-27 16:12:22 +0300

    deleted function call

--- app/main.py
+++ app/main.py
@@ -15,8 +15,3 @@ def cryptocurrency_action(current_rate):
     if prediction_rate / current_rate < 0.95:
         return "Sell all your cryptocurrency"
     return "Do nothing"
-
-
-if __name__ == '__main__':
-    print(get_exchange_rate_prediction(1))
-    print(cryptocurrency_action(5))
