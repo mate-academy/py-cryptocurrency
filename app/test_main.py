1bf1f4d2bc7ff1974b7f532fbc9096fd10ed6494
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-27 22:07:40 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,36 +1,54 @@
 import pytest
 from app.main import cryptocurrency_action
-from unittest import mock
+from unittest.mock import patch
 
 
 @pytest.mark.parametrize(
-    "actual_prediction, expected_prediction",
+    "actual_prediction, exchange_rate, expected_prediction",
     [
         pytest.param(
-            1.9,
-            "Buy more cryptocurrency",
-            id="should return 'Buy more cryptocurrency' "
-               "if predicted exchange higher then current more then 5 %"
-        ),
-        pytest.param(
-            2.2,
+            1.5,
+            1.41,
             "Sell all your cryptocurrency",
             id="should return 'Sell all your cryptocurrency' "
                "if predicted exchange lower then current more then 5 %"
         ),
         pytest.param(
-            2,
+            1.5,
+            1.58,
+            "Buy more cryptocurrency",
+            id="should return 'Buy more cryptocurrency' "
+               "if predicted exchange higher then current more then 5 %"
+        ),
+        pytest.param(
+            1.5,
+            1.5,
             "Do nothing",
             id="should return 'Do nothing' "
                "if predicted exchange not higher and not lower current"
+        ),
+        pytest.param(
+            2,
+            2.1,
+            "Do nothing",
+            id="should return 'Do nothing' "
+               "if predicted exchange is equal 1.05"
+        ),
+        pytest.param(
+            4,
+            3.8,
+            "Do nothing",
+            id="should return 'Do nothing' "
+               "if predicted exchange is equal 0.95"
         )
     ]
 )
-@mock.patch("app.main.get_exchange_rate_prediction", return_value=2)
+@patch("app.main.get_exchange_rate_prediction")
 def test_should_return_correct_prediction(
         mocked,
         actual_prediction,
+        exchange_rate,
         expected_prediction,
-
 ):
-    assert round(cryptocurrency_action(actual_prediction), 3) == expected_prediction
+    mocked.return_value = exchange_rate
+    assert cryptocurrency_action(actual_prediction) == expected_prediction
