25b4a2a15cb2a8b7713f6fc92d2a5be39879f4d5
Anastasiia Shevchenko <anastasiia.sshevchenko@gmail.com>
2022-08-05 11:30:10 +0300

    MY_Solution

--- app/test_main.py
+++ app/test_main.py
@@ -15,12 +15,12 @@ def test_sell_all_your_cryptocurrency(mocked_prediction):
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_sell_all_your_cryptocurrency(mocked_prediction):
+def test_do_nothing_example_1(mocked_prediction):
     mocked_prediction.return_value = 1.05
     assert cryptocurrency_action(1) == "Do nothing"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_sell_all_your_cryptocurrency(mocked_prediction):
+def test_do_nothing_example_2(mocked_prediction):
     mocked_prediction.return_value = 0.95
     assert cryptocurrency_action(1) == "Do nothing"
