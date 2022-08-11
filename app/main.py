1ffbc53e2871a053b6f3e42ed5c026c032fb1ee2
Dmytro Hushchyk <D.hushchyk@gmail.com>
2022-08-11 16:46:55 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -10,8 +10,10 @@ def get_exchange_rate_prediction(exchange_rate):
 
 def cryptocurrency_action(current_rate):
     prediction_rate = get_exchange_rate_prediction(current_rate)
+
     if prediction_rate / current_rate > 1.05:
         return "Buy more cryptocurrency"
     if prediction_rate / current_rate < 0.95:
         return "Sell all your cryptocurrency"
+
     return "Do nothing"
