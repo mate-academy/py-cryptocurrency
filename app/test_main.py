8847caf226c22320b4a4462e86da5a80f35ceb31
Katya Vasylieva <katevvasylyeva@gmail.com>
2022-09-09 17:52:15 +0300

    Solution1

--- app/test_main.py
+++ app/test_main.py
@@ -6,30 +6,31 @@ from app.main import cryptocurrency_action
 
 
 @pytest.fixture()
-def mocked_prediction():
-    with mock.patch("app.main.get_exchange_rate_prediction") as mock_prediction:
+def mock_prediction():
+    with mock.patch("app.main.get_exchange_rate_prediction")\
+            as mock_prediction:
         yield mock_prediction
 
 
-def test_should_recommend_buy_when_growth_predicted(mocked_prediction):
-    mocked_prediction.return_value = 15
+def test_should_tell_buy_when_growth_predicted(mock_prediction):
+    mock_prediction.return_value = 15
     recommendation = cryptocurrency_action(10)
     assert recommendation == "Buy more cryptocurrency"
 
 
-def test_should_recommend_sell_when_drop_predicted(mocked_prediction):
-    mocked_prediction.return_value = 7
+def test_should_tell_sell_when_drop_predicted(mock_prediction):
+    mock_prediction.return_value = 7
     recommendation = cryptocurrency_action(10)
     assert recommendation == "Sell all your cryptocurrency"
 
 
-def test_should_recommend_do_nothing_when_rate_is_on_higher_border(mocked_prediction):
-    mocked_prediction.return_value = 10.5
+def test_should_tell_do_nothing_when_rate_is_higher_border(mock_prediction):
+    mock_prediction.return_value = 10.5
     recommendation = cryptocurrency_action(10)
     assert recommendation == "Do nothing"
 
 
-def test_should_recommend_do_nothing_when_rate_is_on_lower_border(mocked_prediction):
-    mocked_prediction.return_value = 9.5
+def test_should_tell_do_nothing_when_rate_is_on_lower_border(mock_prediction):
+    mock_prediction.return_value = 9.5
     recommendation = cryptocurrency_action(10)
     assert recommendation == "Do nothing"
