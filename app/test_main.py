e53fddec99ee855acf49880fdc273a0e68373373
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-09-19 23:25:34 +0300

    py_cryptocurrency_Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_do_nothing(mocked_function):
+    mocked_function.return_value = 0.95
+    assert cryptocurrency_action(1) == "Do nothing"
+    mocked_function.return_value = 1.05
+    assert cryptocurrency_action(1) == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_to_buy(mocked_function):
+    mocked_function.return_value = 0.94
+    assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_to_sell(mocked_function):
+    mocked_function.return_value = 1.06
+    assert cryptocurrency_action(1) == "Buy more cryptocurrency"
