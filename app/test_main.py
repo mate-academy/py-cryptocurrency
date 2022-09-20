743e41fe2dda645668f15c4855deb5b4b9b3c6d8
unknown <vladearth7@gmail.com>
2022-09-20 12:35:18 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,5 @@
 from unittest.mock import patch
 from app.main import cryptocurrency_action
-import pytest
 
 
 @patch("app.main.get_exchange_rate_prediction")
@@ -25,4 +24,3 @@ def test_should_do_nothing_with_rate_0_95(mock_rate_prediction):
 def test_should_do_nothing_with_rate_1_05(mock_rate_prediction):
     mock_rate_prediction.return_value = 1.05
     assert cryptocurrency_action(1) == "Do nothing"
-
