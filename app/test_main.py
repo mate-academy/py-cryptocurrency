f049980a6fd3f2514c0c5b7cb7ad717ccef47dd2
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-10-05 14:54:46 +0300

    Fix tests and flake8

--- app/test_main.py
+++ app/test_main.py
@@ -15,17 +15,18 @@ def test_should_call_function(mocked_cryptocurrency_action):
     mocked_cryptocurrency_action.assert_called()
 
 
+def test_for_do_nothing(mocked_cryptocurrency_action):
+    mocked_cryptocurrency_action.return_value = 10.5
+    assert cryptocurrency_action(10) == "Do nothing"
+    mocked_cryptocurrency_action.return_value = 249.85
+    assert cryptocurrency_action(263) == "Do nothing"
+
+
 def test_for_buy_cryptocurrency(mocked_cryptocurrency_action):
-    mocked_cryptocurrency_action.return_value = 120
-    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+    mocked_cryptocurrency_action.return_value = 25.44
+    assert cryptocurrency_action(24) == "Buy more cryptocurrency"
 
 
 def test_for_sell_cryptocurrency(mocked_cryptocurrency_action):
-    mocked_cryptocurrency_action.return_value = 0.5
-    assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
-
-
-def test_for_do_nothing(mocked_cryptocurrency_action):
-    mocked_cryptocurrency_action.return_value = 13
-    assert cryptocurrency_action(12.7) == "Do nothing"
-
+    mocked_cryptocurrency_action.return_value = 118.44
+    assert cryptocurrency_action(126) == "Sell all your cryptocurrency"
