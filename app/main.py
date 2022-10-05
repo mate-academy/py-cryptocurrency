17373f4b9f1bf67d6bbc9e84e9ad852980a3c0fa
HStakhiv <gsstahiv@gmail.com>
2022-10-05 19:17:35 +0300

    Fixed annotation

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 import random
+from typing import Union
 
 
-def get_exchange_rate_prediction(exchange_rate: int | float) -> float:
+def get_exchange_rate_prediction(exchange_rate: Union[int, float]) -> float:
     if random.choice(["increase", "decrease"]) == "increase":
         return round(exchange_rate / random.random(), 2)
     else:
         return round(exchange_rate * random.random(), 2)
 
 
-def cryptocurrency_action(current_rate: int | float) -> str:
+def cryptocurrency_action(current_rate: Union[int, float]) -> str:
     prediction_rate = get_exchange_rate_prediction(current_rate)
     if prediction_rate / current_rate > 1.05:
         return "Buy more cryptocurrency"
