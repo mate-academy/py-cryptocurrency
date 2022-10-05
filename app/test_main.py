6c6701d3d948e2faddfbbf96c1a09e8b4bef6db6
Dima Ursatii <Dimon4ir@gmail.com>
2022-10-05 23:27:43 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -11,22 +11,23 @@ def mocked_cryptocurrency_test() -> None:
         yield mocked_test
 
 
-def test_for_sale(mocked_cryptocurrency_test):
+def test_for_sale(mocked_cryptocurrency_test: int) -> None:
     mocked_cryptocurrency_test.return_value = 120
     assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
-def test_for_buy(mocked_cryptocurrency_test):
+def test_for_buy(mocked_cryptocurrency_test: int) -> None:
     mocked_cryptocurrency_test.return_value = 100
     assert cryptocurrency_action(120) == "Sell all your cryptocurrency"
 
 
-def test_for_do_nothing_when_rate_less_five_percent(mocked_cryptocurrency_test):
+def test_for_do_nothing_percen_bigger(mocked_cryptocurrency_test: float
+                                      ) -> None:
     mocked_cryptocurrency_test.return_value = 1.05
     assert cryptocurrency_action(1) == "Do nothing"
 
 
-def test_do_nothing_percent_less_five(mocked_cryptocurrency_test):
+def test_do_nothing_percent_less_five(mocked_cryptocurrency_test: float
+                                      ) -> None:
     mocked_cryptocurrency_test.return_value = 0.95
     assert cryptocurrency_action(1) == "Do nothing"
-
