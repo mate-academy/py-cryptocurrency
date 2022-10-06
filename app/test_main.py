9c2ced77e97c18a9131607358cb7731c3dd79d1d
Nikyxa <kolisova.veronika81@gmail.com>
2022-10-06 20:41:52 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -6,30 +6,31 @@ from app.main import cryptocurrency_action
 
 @pytest.fixture()
 def mocked_cryptocurrency() -> None:
-    with mock.patch("app.main.get_exchange_rate_prediction") as mock_action:
+    with mock.patch("app.main.get_exchange_rate_prediction")\
+            as mock_action:
         yield mock_action
 
 
-def test_get_exchange_rate_higher(mocked_cryptocurrency: None) -> None:
-    mocked_cryptocurrency.return_value = 10
-    assert cryptocurrency_action(2) == "Buy more cryptocurrency"
+def test_call_get_exchange_rate_higher(mocked_cryptocurrency: None) -> None:
+    mocked_cryptocurrency.return_value = 8
+    assert cryptocurrency_action(5) == "Buy more cryptocurrency"
 
 
-def test_get_exchange_rate_lower(mocked_cryptocurrency: None) -> None:
-    mocked_cryptocurrency.return_value = 7
-    assert cryptocurrency_action(15) == "Sell all your cryptocurrency"
+def test_call_get_exchange_rate_lower(mocked_cryptocurrency: None) -> None:
+    mocked_cryptocurrency.return_value = 1
+    assert cryptocurrency_action(6) == "Sell all your cryptocurrency"
 
 
-def test_get_exchange_rate_bit_higher(mocked_cryptocurrency: None) -> None:
-    mocked_cryptocurrency.return_value = 6
-    assert cryptocurrency_action(6.4) == "Do nothing"
-
-
-def test_get_exchange_rate_bit_lower(mocked_cryptocurrency: None) -> None:
-    mocked_cryptocurrency.return_value = 5
-    assert cryptocurrency_action(4.8) == "Do nothing"
-
-
-def test_get_exchange_rate_bit_equal(mocked_cryptocurrency: None) -> None:
-    mocked_cryptocurrency.return_value = 4
+def test_call_get_exchange_rate_bit_lower(mocked_cryptocurrency: None) -> None:
+    mocked_cryptocurrency.return_value = 3.8
     assert cryptocurrency_action(4) == "Do nothing"
+
+
+def test_call_get_exchange_rate_bit_higher(mocked_cryptocurrency: None) -> None:
+    mocked_cryptocurrency.return_value = 4.2
+    assert cryptocurrency_action(4) == "Do nothing"
+
+
+def test_call_get_exchange_rate_equals(mocked_cryptocurrency: None) -> None:
+    mocked_cryptocurrency.return_value = 12
+    assert cryptocurrency_action(12) == "Do nothing"
