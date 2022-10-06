2574128c5e5d00443f5a331a75a2bb59b5c276c3
Edik Marushchak <e.moscow138@gmail.com>
2022-10-06 11:30:56 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 import random
 
 
-def get_exchange_rate_prediction(exchange_rate):
+def get_exchange_rate_prediction(exchange_rate: int) -> int:
     if random.choice(["increase", "decrease"]) == "increase":
         return round(exchange_rate / random.random(), 2)
     else:
         return round(exchange_rate * random.random(), 2)
 
 
-def cryptocurrency_action(current_rate):
+def cryptocurrency_action(current_rate: int) -> int:
     prediction_rate = get_exchange_rate_prediction(current_rate)
     if prediction_rate / current_rate > 1.05:
         return "Buy more cryptocurrency"
