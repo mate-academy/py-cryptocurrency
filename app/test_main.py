ede0dece03638949688297c963bae7e216d9d455
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2022-10-06 17:11:21 +0300

    fixed

--- app/test_main.py
+++ app/test_main.py
@@ -13,23 +13,23 @@ def mocked_get_exchange_rate_prediction() -> None:
 
 def test_difference_more_5_higher_from_current(
         mocked_get_exchange_rate_prediction: float) -> None:
-    mocked_get_exchange_rate_prediction.return_value = 107
+    mocked_get_exchange_rate_prediction.return_value = 160
     assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
 def test_difference_more_5_lower_from_cur(
         mocked_get_exchange_rate_prediction: float) -> None:
-    mocked_get_exchange_rate_prediction.return_value = 30
-    assert cryptocurrency_action(60) == "Sell all your cryptocurrency"
+    mocked_get_exchange_rate_prediction.return_value = 50
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
 
 
 def test_dif_less_5_1(
         mocked_get_exchange_rate_prediction: float) -> None:
-    mocked_get_exchange_rate_prediction.return_value = 301.0
-    assert cryptocurrency_action(300.00) == "Do nothing"
+    mocked_get_exchange_rate_prediction.return_value = 105
+    assert cryptocurrency_action(100) == "Do nothing"
 
 
 def test_dif_less_5_2(
         mocked_get_exchange_rate_prediction: float) -> None:
-    mocked_get_exchange_rate_prediction.return_value = 299.0
-    assert cryptocurrency_action(300.00) == "Do nothing"
+    mocked_get_exchange_rate_prediction.return_value = 95
+    assert cryptocurrency_action(100) == "Do nothing"
