ba94ce89b35db92294634d5d2317ce9b12d03289
illia riaboi <riaboy94@gmail.com>
2022-10-07 03:29:44 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,42 @@
-# write your code here
+from unittest import mock
+import pytest
+
+from app.main import cryptocurrency_action
+
+
+@pytest.fixture()
+def mocked_get_exchange() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") as \
+            mocked_get_exchange:
+        yield mocked_get_exchange
+
+
+def test_should_call_func(mocked_get_exchange: None) -> None:
+    mocked_get_exchange.return_value = 0.4818
+    cryptocurrency_action(0.5)
+    mocked_get_exchange.assert_called()
+
+
+def test_return_buy_string(mocked_get_exchange: None) -> None:
+    mocked_get_exchange.return_value = 0.4818
+    assert cryptocurrency_action(0.35) == "Buy more cryptocurrency"
+
+
+def test_return_sell_string(mocked_get_exchange: None) -> None:
+    mocked_get_exchange.return_value = 0.4818
+    assert cryptocurrency_action(0.6) == "Sell all your cryptocurrency"
+
+
+def test_return_do_nothing(mocked_get_exchange: None) -> None:
+    mocked_get_exchange.return_value = 0.4818
+    assert cryptocurrency_action(0.5) == "Do nothing"
+
+
+def test_should_not_sell(mocked_get_exchange: None) -> None:
+    mocked_get_exchange.return_value = 1.05
+    assert cryptocurrency_action(1) == "Do nothing"
+
+
+def test_should_not_buy(mocked_get_exchange: None) -> None:
+    mocked_get_exchange.return_value = 0.95
+    assert cryptocurrency_action(1) == "Do nothing"
