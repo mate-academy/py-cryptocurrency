b14ff6dce9f20f3b9b23d5f469f506dda80696a8
Oleksandr Mamchur <irex1988@gmail.com>
2022-10-23 00:12:11 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,39 @@
-# write your code here
+from unittest import mock
+from app.main import get_exchange_rate_prediction
+from app.main import cryptocurrency_action
+
+
+def test_should_return_string() -> None:
+    assert isinstance(cryptocurrency_action(1), str)
+
+
+def test_get_exchange_rate_prediction_return_float() -> None:
+    assert isinstance(get_exchange_rate_prediction(2), float)
+
+
+def test_get_exchange_rate_prediction_return_0_when_get_0() -> None:
+    assert get_exchange_rate_prediction(0) == 0
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_should_return_buy_more(mock_func: float) -> None:
+    mock_func.return_value = 2.2
+    assert cryptocurrency_action(2) == "Buy more cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_should_return_sell_all(mock_func: float) -> None:
+    mock_func.return_value = 1.8
+    assert cryptocurrency_action(2) == "Sell all your cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_return_do_nothing_when_correlation_is_105(mock_func: float) -> None:
+    mock_func.return_value = 2.1
+    assert cryptocurrency_action(2) == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_return_do_nothing_when_correlation_is_095(mock_func: float) -> None:
+    mock_func.return_value = 1.9
+    assert cryptocurrency_action(2) == "Do nothing"
