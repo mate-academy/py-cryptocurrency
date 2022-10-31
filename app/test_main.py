85105431dea57e38e4ddad2b27b60ed549e98ca1
muhactive <muhactive@gmail.com>
2022-10-31 18:17:30 +0200

    Solution3

--- app/test_main.py
+++ app/test_main.py
@@ -8,16 +8,16 @@ class TestCryptoAction:
 
     def test_should_buy_cryptocurrency(self, mocked_exchange_rate):
         mocked_exchange_rate.return_value = 1.1
-        assert cryptocurrency_action(1) == "Buy more cryptocurrency!"
+        assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
     def test_should_sell_cryptocurrency(self, mocked_exchange_rate):
         mocked_exchange_rate.return_value = 0.9
-        assert cryptocurrency_action(1) == "Sell all your cryptocurrency!"
+        assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
 
     def test_should_output_do_nothing_if_105(self, mocked_exchange_rate):
         mocked_exchange_rate.return_value = 1.05
-        assert cryptocurrency_action(1) == "Do nothing!"
+        assert cryptocurrency_action(1) == "Do nothing"
 
     def test_should_output_do_nothing_if_095(self, mocked_exchange_rate):
         mocked_exchange_rate.return_value = 0.95
-        assert cryptocurrency_action(1) == "Do nothing!"
+        assert cryptocurrency_action(1) == "Do nothing"
