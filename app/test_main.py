af867b3807da244acbc60eaeb68516432acb7f66
Taras Martyniuk <bythewaters800@gmail.com>
2022-11-04 15:53:26 +0100

    Sol

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,7 @@ from app.main import cryptocurrency_action
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_sell_all_crypto(mocked_value: int) -> None:
-    mocked_value.return_value = 5.25
+    mocked_value.return_value = 3
 
     assert cryptocurrency_action(5) == "Sell all your cryptocurrency"
 
@@ -18,7 +18,14 @@ def test_buy_more_crypto(mocked_value: int) -> None:
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_do_nothing(mocked_value: int) -> None:
-    mocked_value.return_value = 5
+def test_do_nothing_if_105(mocked_value: int) -> None:
+    mocked_value.return_value = 5.25
+
+    assert cryptocurrency_action(5) == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing_if_095(mocked_value: int) -> None:
+    mocked_value.return_value = 4.75
 
     assert cryptocurrency_action(5) == "Do nothing"
