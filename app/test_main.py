d1732a1c0443b696bef07ffc462d2bd9fc134f28
vani@linux <winfkill@gmail.com>
2022-11-23 19:45:49 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+import pytest
+from app.main import get_exchange_rate_prediction, cryptocurrency_action
+
+
+class TestCryptoClass:
+    @pytest.mark.parametrize(
+        "current_rate,prediction_rate,action",
+        [
+            (1.4, 1.2, "Sell all your cryptocurrency"),
+            (1.4, 1.6, "Buy more cryptocurrency"),
+            (1.4, 1.4, "Do nothing"),
+        ],
+    )
+    def test_matecoin(self, current_rate: int,
+                      prediction_rate: int,
+                      action: str) -> None:
+        def mock_rate() -> None:
+
+            mock_rate.predict = get_exchange_rate_prediction(prediction_rate)
+
+            assert cryptocurrency_action(mock_rate.predict) == action
