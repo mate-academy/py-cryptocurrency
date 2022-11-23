ecc72212e9da6ff5708fdf56ab8d506c9de0e176
vani@linux <winfkill@gmail.com>
2022-11-23 20:47:40 +0200

    Solution5

--- app/test_main.py
+++ app/test_main.py
@@ -4,17 +4,23 @@ from unittest import mock
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_check_crypto_buy(mock_predict: int) -> None:
-    mock_predict.return_value = 5.3
-    assert cryptocurrency_action(5) == "Buy more cryptocurrency"
+    mock_predict.return_value = 200
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_check_crypto_sell(mock_predict: int) -> None:
-    mock_predict.return_value = 4.7
-    assert cryptocurrency_action(5) == "Sell all your cryptocurrency"
+    mock_predict.return_value = 20
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_check_crypto_hold(mock_predict: int) -> None:
-    mock_predict.return_value = 1.05
-    assert cryptocurrency_action(1) == "Do nothing"
+    mock_predict.return_value = 105
+    assert cryptocurrency_action(100) == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_check_crypto_holdX2(mock_predict: int) -> None:
+    mock_predict.return_value = 95
+    assert cryptocurrency_action(100) == "Do nothing"
