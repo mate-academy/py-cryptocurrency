7b5dd1b9b120f02adbd083591259b383e435cd63
Іван Березянський <mamanadarin@gmail.com>
2022-12-19 19:35:35 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,30 @@
-import pytest
-from unittest import mock
 from app.main import cryptocurrency_action
+from unittest import mock
+import pytest
 
 
 @pytest.fixture()
-def mocked_func() -> None:
-    with mock.patch("app.main.get_exchange_rate_prediction")\
-            as mocked_function:
-        yield mocked_function
+def mock_envision() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") \
+            as mock_prediction:
+        yield mock_prediction
 
 
-def test_func_buy(mocked_func: callable) -> None:
-    mocked_func.return_value = 1.06
+def test_if_predicted_exchange_rate_is_higher(mock_envision: callable) -> None:
+    mock_envision.return_value = 1.06
     assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
-def test_func_do_nothing(mocked_func: callable) -> None:
-    mocked_func.return_value = 1
+def test_if_predicted_exchange_rate_is_low(mock_envision: callable) -> None:
+    mock_envision.return_value = 0.94
+    assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
+
+
+def test_if_difference_is_not_that_much_1(mock_envision: callable) -> None:
+    mock_envision.return_value = 1.05
     assert cryptocurrency_action(1) == "Do nothing"
 
 
-def test_func_sell(mocked_func: callable) -> None:
-    mocked_func.return_value = 0.1
-    assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
+def test_if_difference_is_not_that_much_2(mock_envision: callable) -> None:
+    mock_envision.return_value = 0.95
+    assert cryptocurrency_action(1) == "Do nothing"
