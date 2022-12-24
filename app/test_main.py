a541a9fac240d5ec5eca108da5809e551c5c1a7a
Derkach Oleksandr <derkach03553@gmail.com>
2022-12-24 17:46:47 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.main import cryptocurrency_action
+
+
+def test_cryptocurrency_action(mocker: callable) -> None:
+    mocker.patch("app.main.get_exchange_rate_prediction",
+                 return_value=0.95)
+    assert cryptocurrency_action(1) == "Do nothing"
+    mocker.patch("app.main.get_exchange_rate_prediction",
+                 return_value=1.05)
+    assert cryptocurrency_action(1) == "Do nothing"
+    mocker.patch("app.main.get_exchange_rate_prediction",
+                 return_value=1)
+    assert cryptocurrency_action(1) == "Do nothing"
+    mocker.patch("app.main.get_exchange_rate_prediction",
+                 return_value=1.06)
+    assert cryptocurrency_action(1) == "Buy more cryptocurrency"
+    mocker.patch("app.main.get_exchange_rate_prediction",
+                 return_value=0.94)
+    assert cryptocurrency_action(1) == \
+           "Sell all your cryptocurrency"
