ddfc11d9179a9b4e2e6684932e3aa0775e1b4f0c
Haishuk Pavlo <xomas@ukr.net>
2022-12-27 02:38:09 +0200

    done

--- app/test_main.py
+++ app/test_main.py
@@ -4,18 +4,18 @@ from app.main import cryptocurrency_action
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_sell_crypto(mocked_prediction):
+def test_sell_crypto(mocked_prediction: callable) -> None:
     mocked_prediction.return_value = 8.12
     assert cryptocurrency_action(23) == "Sell all your cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_buy_crypto(mocked_prediction):
+def test_buy_crypto(mocked_prediction: callable) -> None:
     mocked_prediction.return_value = 88.92
     assert cryptocurrency_action(45) == "Buy more cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_do_nothing(mocked_prediction):
+def test_do_nothing(mocked_prediction: callable) -> None:
     mocked_prediction.return_value = 100
     assert cryptocurrency_action(101) == "Do nothing"
