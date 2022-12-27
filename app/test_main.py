60b1f6e0c148a2cf55ba52cdec30e92f6f015a50
Haishuk Pavlo <xomas@ukr.net>
2022-12-27 02:43:37 +0200

    done

--- app/test_main.py
+++ app/test_main.py
@@ -16,6 +16,12 @@ def test_buy_crypto(mocked_prediction: callable) -> None:
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_do_nothing(mocked_prediction: callable) -> None:
-    mocked_prediction.return_value = 100
-    assert cryptocurrency_action(101) == "Do nothing"
+def test_do_nothing_max(mocked_prediction: callable) -> None:
+    mocked_prediction.return_value = 5.25
+    assert cryptocurrency_action(5) == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing_min(mocked_prediction: callable) -> None:
+    mocked_prediction.return_value = 4.75
+    assert cryptocurrency_action(5) == "Do nothing"
