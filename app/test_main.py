182d12feaf3b1f5927d8a2bad285ebff6e6a04e2
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-28 16:42:06 +0200

    sol 5

--- app/test_main.py
+++ app/test_main.py
@@ -4,22 +4,22 @@ from app.main import cryptocurrency_action
 
 
 @pytest.fixture()
-def mocked_prediction() -> None:
+def mocked_predict() -> None:
     with mock.patch("app.main.get_exchange_rate_prediction") \
             as mocked_prediction:
         yield mocked_prediction
 
 
 def test_cryptocurrency_action_more_than(mocked_prediction: str) -> None:
-    mocked_prediction.return_value = 1.08
+    mocked_predict.return_value = 1.08
     assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
 def test_cryptocurrency_action_less_than(mocked_prediction: str) -> None:
-    mocked_prediction.return_value = 0.8
+    mocked_predict.return_value = 0.8
     assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
 
 
 def test_cryptocurrency_action_donothing(mocked_prediction: str) -> None:
-    mocked_prediction.return_value = 0.95
+    mocked_predict.return_value = 0.95
     assert cryptocurrency_action(1) == "Do nothing"
