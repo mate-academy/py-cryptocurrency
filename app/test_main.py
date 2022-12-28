e423e822eab0c9893b3a417062d96e9c9894ba87
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-28 16:47:09 +0200

    sol 7

--- app/test_main.py
+++ app/test_main.py
@@ -11,16 +11,26 @@ def mocked_predict() -> None:
         yield mocked_prediction
 
 
-def test_cryptocurrency_action_more_than(mocked_predict: Callable) -> None:
-    mocked_predict.return_value = 1.08
+def test_function_returns_buy(mocked_predict: Callable) -> None:
+    mocked_predict.return_value = 1.06
     assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
-def test_cryptocurrency_action_less_than(mocked_predict: Callable) -> None:
-    mocked_predict.return_value = 0.8
+def test_function_returns_sell(mocked_predict: Callable) -> None:
+    mocked_predict.return_value = 0.94
     assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
 
 
-def test_cryptocurrency_action_donothing(mocked_predict: Callable) -> None:
+def test_function_returns_do_nothing(mocked_predict: Callable) -> None:
     mocked_predict.return_value = 0.95
     assert cryptocurrency_action(1) == "Do nothing"
+
+
+def test_function_returns_do_nothing_2(mocked_predict: Callable) -> None:
+    mocked_predict.return_value = 1.05
+    assert cryptocurrency_action(1) == "Do nothing"
+
+
+def test_function_raises_error() -> None:
+    with pytest.raises(TypeError):
+        cryptocurrency_action("1")
