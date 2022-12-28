72934aa6132d5db042f0c10ce73c0e7a5f2554f4
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-28 16:18:44 +0200

    sol 2

--- app/test_main.py
+++ app/test_main.py
@@ -10,16 +10,16 @@ def mocked_prediction() -> None:
         yield mocked_prediction
 
 
-def test_cryptocurrency_action_more_than(mocked_prediction) -> None:
+def test_cryptocurrency_action_more_than(mocked_prediction: str) -> None:
     mocked_prediction.return_value = 1.08
     assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
-def test_cryptocurrency_action_less_than(mocked_prediction):
+def test_cryptocurrency_action_less_than(mocked_prediction: str) -> None:
     mocked_prediction.return_value = 0.8
     assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
 
 
-def test_cryptocurrency_action_donothing(mocked_prediction):
+def test_cryptocurrency_action_donothing(mocked_prediction: str) -> None:
     mocked_prediction.return_value == 1
     assert mocked_prediction(1) == "Do nothing"
