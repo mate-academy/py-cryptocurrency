edcd3c85f9c351d33926d82ce514d3a63274c430
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-28 16:07:54 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+import pytest
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+@pytest.fixture()
+def mocked_prediction() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") \
+            as mocked_prediction:
+        yield mocked_prediction
+
+
+def test_cryptocurrency_action_more_than(mocked_prediction) -> None:
+    mocked_prediction.return_value = 1.08
+    assert cryptocurrency_action(1) == "Buy more cryptocurrency"
+
+
+def test_cryptocurrency_action_less_than(mocked_prediction):
+    mocked_prediction.return_value = 0.8
+    assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
+
+
+def test_cryptocurrency_action_donothing(mocked_prediction):
+    mocked_prediction.return_value == 1
+    assert mocked_prediction(1) == "Do nothing"
