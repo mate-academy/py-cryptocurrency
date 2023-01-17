1f1b9d77a8c0542826a312d7c7c109d4fb35eb0f
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-01-17 19:55:21 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-
 from unittest import mock
 import pytest
 
@@ -7,38 +6,27 @@ from app.main import cryptocurrency_action
 
 
 @pytest.fixture()
-def mock_get_exchange() -> int:
+def mock_get_exchange() -> callable:
     with mock.patch("app.main.get_exchange_rate_prediction") as mock_rate:
         yield mock_rate
 
 
-def test_called_get_exchange(mock_get_exchange: callable) -> None:
-    mock_get_exchange.return_value = 100
-    cryptocurrency_action(100)
+def test_upper_bound(mock_get_exchange: callable) -> None:
+    mock_get_exchange.return_value = 105
+    assert cryptocurrency_action(100) == "Do nothing"
 
 
-def test_rate_up() -> None:
-    with mock.patch(
-            "app.main.get_exchange_rate_prediction",
-            return_value=120
-    ) as exchange_rate:
-        assert cryptocurrency_action(100) == "Buy more cryptocurrency"
-        exchange_rate.assert_called()
+def test_lower_bound(mock_get_exchange: callable) -> None:
+    mock_get_exchange.return_value = 95
+    assert cryptocurrency_action(100) == "Do nothing"
 
 
-def test_rate_down() -> None:
-    with mock.patch(
-            "app.main.get_exchange_rate_prediction",
-            return_value=80
-    ) as exchange_rate:
-        assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
-        exchange_rate.assert_called()
+def test_rate_up(mock_get_exchange: callable) -> None:
+    mock_get_exchange.return_value = 110
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
-def test_rate_not_change() -> None:
-    with mock.patch(
-            "app.main.get_exchange_rate_prediction",
-            return_value=100
-    ) as exchange_rate:
-        assert cryptocurrency_action(100) == "Do nothing"
-        exchange_rate.assert_called()
+def test_rate_lower(mock_get_exchange: callable) -> None:
+    mock_get_exchange.return_value = 80
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
