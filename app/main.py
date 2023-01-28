099aca5489ea054a3a5e20f5dc0bf8341b428134
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-01-28 09:46:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -11,6 +11,8 @@ def get_exchange_rate_prediction(exchange_rate: Union[int, float]) -> float:
 
 def cryptocurrency_action(current_rate: Union[int, float]) -> str:
     prediction_rate = get_exchange_rate_prediction(current_rate)
+    print(prediction_rate, current_rate)
+    print(prediction_rate / current_rate)
     if prediction_rate / current_rate > 1.05:
         return "Buy more cryptocurrency"
     if prediction_rate / current_rate < 0.95:
