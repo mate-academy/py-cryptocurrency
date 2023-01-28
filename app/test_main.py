f9218f645e2d7ce09c9693da23d015804816ef44
Andrii Skryl <a.skryl92@gmail.com>
2023-01-28 20:45:59 +0200

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -9,26 +9,36 @@ def mocked_prediction() -> None:
         yield mocked_rate
 
 
-def test_if_predicted_rate_higher(mocked_prediction) -> None:
+def test_if_predicted_rate_higher(
+        mocked_prediction: float
+) -> None:
     mocked_prediction.return_value = 1.10
     assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
-def test_if_predicted_rate_lower(mocked_prediction) -> None:
+def test_if_predicted_rate_lower(
+        mocked_prediction: float
+) -> None:
     mocked_prediction.return_value = 0.90
     assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
 
 
-def test_if_predicted_rate_in_5_percent_range(mocked_prediction) -> None:
+def test_if_predicted_rate_in_5_percent_range(
+        mocked_prediction: float
+) -> None:
     mocked_prediction.return_value = 1.00
     assert cryptocurrency_action(1) == "Do nothing"
 
 
-def test_if_predicted_rate_on_the_upper_limit(mocked_prediction) -> None:
+def test_if_predicted_rate_on_the_upper_limit(
+        mocked_prediction: float
+) -> None:
     mocked_prediction.return_value = 1.05
     assert cryptocurrency_action(1) == "Do nothing"
 
 
-def test_if_predicted_rate_on_the_lower_limit(mocked_prediction) -> None:
+def test_if_predicted_rate_on_the_lower_limit(
+        mocked_prediction: float
+) -> None:
     mocked_prediction.return_value = 0.95
     assert cryptocurrency_action(1) == "Do nothing"
