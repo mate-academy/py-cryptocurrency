4b3bf2f9358ea4bcd2d0ef71af334ef9047afee7
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-01-30 19:38:42 +0100

    'Solution2'

--- app/test_main.py
+++ app/test_main.py
@@ -11,21 +11,11 @@ def mocked_rate_prediction() -> None:
         yield mock_rate_prediction
 
 
-def test_get_exchange_rate_prediction_has_been_called(
-        mocked_rate_prediction: Callable
-) -> None:
-    mocked_rate_prediction.return_value = 100
-    cryptocurrency_action(99)
-    mocked_rate_prediction.assert_called_once_with(99)
-
-
 @pytest.mark.parametrize(
     "current_rate, prediction_rate, result",
     [
         (100, 104, "Do nothing"),
         (100, 96, "Do nothing"),
-        (100, 106, "Buy more cryptocurrency"),
-        (100, 30, "Sell all your cryptocurrency"),
     ]
 )
 def test_cryptocurrency_action_result(
