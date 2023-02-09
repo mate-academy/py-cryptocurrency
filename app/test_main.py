0664b25d3115f58d24a73a8b3bdc8feb95f6886e
VladyslavOladko <vladguitar03@gmail.com>
2023-02-09 14:46:57 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+from unittest import mock
+from typing import Callable
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_buy_more(mocked_rate: Callable) -> None:
+    mocked_rate.return_value = 10600
+    assert cryptocurrency_action(10000) == "Buy more cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_do_nothing(mocked_rate: Callable) -> None:
+    mocked_rate.return_value = 10500
+    assert cryptocurrency_action(10000) == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_sell_all(mocked_rate: Callable) -> None:
+    mocked_rate.return_value = 9400
+    assert cryptocurrency_action(10000) == "Sell all your cryptocurrency"
