75f554aeb84ef8ccefec8a04c03cac11c69858da
Mykhaylo Kobylianskyi <mvkobylianskyi@gmail.com>
2023-02-11 22:37:34 +0200

    solution v3

--- app/test_main.py
+++ app/test_main.py
@@ -5,22 +5,16 @@ from app.main import cryptocurrency_action
 
 
 @pytest.mark.parametrize(
-    "current_rate,prediction_rate,result",
+    "current_rate,predicted_rate,result",
     [
-        (100, 105, "Do nothing"),
         (100, 106, "Buy more cryptocurrency"),
-        (100, 94, "Sell all your cryptocurrency"),
-        (200, 200, "Do nothing"),
-        (40, 50, "Buy more cryptocurrency"),
-        (50, 49, "Do nothing"),
-        (50, 47, "Sell all your cryptocurrency")
+        (200, 210, "Do nothing"),
+        (50, 50, "Do nothing"),
+        (300, 285, "Do nothing"),
+        (300, 284, "Sell all your cryptocurrency")
     ]
 )
-def test_cryptocurrency_action(
-        current_rate: int,
-        prediction_rate: int,
-        result: str
-) -> None:
-    with mock.patch("app.main.get_exchange_rate_prediction") as prediction:
-        prediction.return_value = prediction_rate
+def test_buy(current_rate: int, predicted_rate: int, result: str) -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") as predict:
+        predict.return_value = predicted_rate
         assert cryptocurrency_action(current_rate) == result
