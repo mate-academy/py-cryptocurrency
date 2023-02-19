c779db9b2d1d95d6458bc3045464a040305e9f37
Maksym Pyshniak <pyshniakm91@gmail.com>
2023-02-19 17:03:54 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,24 @@
-# write your code here
+from unittest.mock import patch
+import pytest
+from app.main import cryptocurrency_action
+
+
+@pytest.fixture
+def mock_get_exchange_rate_prediction():
+    with patch('app.main.get_exchange_rate_prediction') as mock_get_exchange_rate_prediction:
+        yield mock_get_exchange_rate_prediction
+
+
+def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction):
+    mock_get_exchange_rate_prediction.return_value = 1.07
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+def test_sell_all_cryptocurrency(mock_get_exchange_rate_prediction):
+    mock_get_exchange_rate_prediction.return_value = 0.93
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+def test_do_nothing(mock_get_exchange_rate_prediction):
+    mock_get_exchange_rate_prediction.return_value = 1.03
+    assert cryptocurrency_action(100) == "Do nothing"
