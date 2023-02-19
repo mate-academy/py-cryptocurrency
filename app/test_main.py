38dae8b82ae607a731ac2adaeb3eaaff68b71eb8
Maksym Pyshniak <pyshniakm91@gmail.com>
2023-02-19 17:15:38 +0200

    v2

--- app/test_main.py
+++ app/test_main.py
@@ -11,14 +11,19 @@ def mock_get_exchange_rate_prediction():
 
 def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction):
     mock_get_exchange_rate_prediction.return_value = 1.07
-    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+    assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
 def test_sell_all_cryptocurrency(mock_get_exchange_rate_prediction):
     mock_get_exchange_rate_prediction.return_value = 0.93
-    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+    assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
 
 
-def test_do_nothing(mock_get_exchange_rate_prediction):
-    mock_get_exchange_rate_prediction.return_value = 1.03
-    assert cryptocurrency_action(100) == "Do nothing"
+def test_do_nothing_when_1_05(mock_get_exchange_rate_prediction):
+    mock_get_exchange_rate_prediction.return_value = 1.05
+    assert cryptocurrency_action(1) == "Do nothing"
+
+
+def test_do_nothing_when_0_95(mock_get_exchange_rate_prediction):
+    mock_get_exchange_rate_prediction.return_value = 0.95
+    assert cryptocurrency_action(1) == "Do nothing"
