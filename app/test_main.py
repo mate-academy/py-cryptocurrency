fd0d6afc0b413f11fef86e5b893e60b74ff97329
Maksym Nikityuk <maksym71417@gmail.com>
2023-02-21 19:32:45 +0200

    Cryptomock Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from __future__ import annotations
+
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_buy_action(mocked_prediction: None) -> None:
+    mocked_prediction.return_value = 1.5
+    assert cryptocurrency_action(1) == "Buy more cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_sell_action(mocked_prediction: None) -> None:
+    mocked_prediction.return_value = 0.9
+    assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_do_nothing_action(mocked_prediction: None) -> None:
+    mocked_prediction.return_value = 1.01
+    assert cryptocurrency_action(1) == "Do nothing"
