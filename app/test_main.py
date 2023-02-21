4913190bfe742ee807ab622be6896124ceb1b7df
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-21 15:46:54 +0200

    Fixed some cases

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,25 @@
 from unittest import mock
+
+import pytest
+
 from app.main import cryptocurrency_action
 
 
-def test_cryptocurrency_buy() -> None:
-    get_exchange_rate_prediction = mock.MagicMock()
-    get_exchange_rate_prediction.return_value = 1.1
+@pytest.fixture()
+def get_exchange_rate_prediction():
+    return mock.MagicMock()
+
+
+def test_cryptocurrency_buy(get_exchange_rate_prediction) -> None:
+    get_exchange_rate_prediction.return_value = 1.5
     assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
-def test_cryptocurrency_do_nothing() -> None:
-    get_exchange_rate_prediction = mock.MagicMock()
+def test_cryptocurrency_do_nothing(get_exchange_rate_prediction) -> None:
     get_exchange_rate_prediction.return_value = 0.95
-    assert cryptocurrency_action(0.95) == "Do nothing"
+    assert cryptocurrency_action(0.92) == "Do nothing"
 
 
-def test_cryptocurrency_sell() -> None:
-    get_exchange_rate_prediction = mock.MagicMock()
-    get_exchange_rate_prediction.return_value = 0.8
+def test_cryptocurrency_sell(get_exchange_rate_prediction) -> None:
+    get_exchange_rate_prediction.return_value = 0.5
     assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
