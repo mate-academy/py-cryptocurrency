318f37345fdb8239692a53c0cfabf6716ab15c8b
Ruslan <noezyboy1@gmail.com>
2023-02-22 16:02:02 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,23 +1,30 @@
 from unittest.mock import patch
-import app.test_main as main
+import app.main as main
 
 
-@patch("app.test_main.get_exchange_rate_prediction")
-def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction: None
-                                 ) -> None:
-    mock_get_exchange_rate_prediction.return_value = 1.1
+@patch("app.main.get_exchange_rate_prediction")
+def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction: None) \
+        -> None:
+    mock_get_exchange_rate_prediction.return_value = 1.06
     assert main.cryptocurrency_action(1.0) == "Buy more cryptocurrency"
 
 
-@patch("app.test_main.get_exchange_rate_prediction")
-def test_sell_all_cryptocurrency(mock_get_exchange_rate_prediction: None
-                                 ) -> None:
-    mock_get_exchange_rate_prediction.return_value = 0.9
+@patch("app.main.get_exchange_rate_prediction")
+def test_sell_all_cryptocurrency(mock_get_exchange_rate_prediction: None)\
+        -> None:
+    mock_get_exchange_rate_prediction.return_value = 0.95
     assert main.cryptocurrency_action(1.0) == "Sell all your cryptocurrency"
 
 
-@patch("app.test_main.get_exchange_rate_prediction")
-def test_do_nothing(mock_get_exchange_rate_prediction: None
-                    ) -> None:
-    mock_get_exchange_rate_prediction.return_value = 1.03
+@patch("app.main.get_exchange_rate_prediction")
+def test_rate_105_percent_do_nothing(mock_get_exchange_rate_prediction: None) \
+        -> None:
+    mock_get_exchange_rate_prediction.return_value = 1.05
     assert main.cryptocurrency_action(1.0) == "Do nothing"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_rate_95_percent_do_nothing(mock_get_exchange_rate_prediction: None)\
+        -> None:
+    mock_get_exchange_rate_prediction.return_value = 0.95
+    assert main.cryptocurrency_action(1.05) == "Do nothing"
