1fc0d16622c6027f5bb8262f13987be26a6ade6e
Yulia <Mala>
2023-02-23 18:33:21 +0200

    right solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
-
+from typing import Callable
 from app.main import cryptocurrency_action
 
 
-def test_cryptocurrency_buy_more(monkeypatch) -> None:
-    monkeypatch.setattr("app.main.get_exchange_rate_prediction", lambda *args: 1100)
+def test_cryptocurrency_buy_more(monkeypatch: Callable) -> None:
+    monkeypatch.setattr("get_exchange_rate_prediction", lambda *args: 1100)
     assert cryptocurrency_action(1000) == "Buy more cryptocurrency"
 
 
-def test_cryptocurrency_sell_all(monkeypatch) -> None:
-    monkeypatch.setattr("app.main.get_exchange_rate_prediction", lambda *args: 80)
+def test_cryptocurrency_sell_all(monkeypatch: Callable) -> None:
+    monkeypatch.setattr("get_exchange_rate_prediction", lambda *args: 80)
     assert cryptocurrency_action(90) == "Sell all your cryptocurrency"
 
 
-def test_cryptocurrency_do_nothing(monkeypatch) -> None:
-    monkeypatch.setattr("app.main.get_exchange_rate_prediction", lambda *args: 102)
+def test_cryptocurrency_do_nothing(monkeypatch: Callable) -> None:
+    monkeypatch.setattr("get_exchange_rate_prediction", lambda *args: 102)
     assert cryptocurrency_action(100) == "Do nothing"
