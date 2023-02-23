38d5c53d612e5fc39240b850e50d92129f03cba9
Yulia <Mala>
2023-02-23 19:08:00 +0200

    finally )

--- app/test_main.py
+++ app/test_main.py
@@ -18,3 +18,15 @@ def test_cryptocurrency_do_nothing(monkeypatch: Callable) -> None:
     monkeypatch.setattr("app.main.get_exchange_rate_prediction",
                         lambda *args: 102)
     assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_cryptocurrency_95(monkeypatch: Callable) -> None:
+    monkeypatch.setattr("app.main.get_exchange_rate_prediction",
+                        lambda *args: 95)
+    assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_cryptocurrency_1_05(monkeypatch: Callable) -> None:
+    monkeypatch.setattr("app.main.get_exchange_rate_prediction",
+                        lambda *args: 105)
+    assert cryptocurrency_action(100) == "Do nothing"
