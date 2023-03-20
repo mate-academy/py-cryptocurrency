2542684c7d7652d0a8b60ee6e024f38d60bcdaec
Dmytro Prokofiev <forbuffa@gmail.com>
2023-03-20 12:52:05 +0100

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -22,6 +22,16 @@ def test_sell_all_crypto(mocked_exchange_rate_prediction: None) -> None:
     assert cryptocurrency_action(1.0) == "Sell all your cryptocurrency"
 
 
+def test_buy_more_crypto_v2(mocked_exchange_rate_prediction: None) -> None:
+    mocked_exchange_rate_prediction.return_value = 1.05
+    assert cryptocurrency_action(1.0) == "Do nothing"
+
+
+def test_sell_all_crypto_v2(mocked_exchange_rate_prediction: None) -> None:
+    mocked_exchange_rate_prediction.return_value = 0.95
+    assert cryptocurrency_action(1.0) == "Do nothing"
+
+
 def test_do_nothing(mocked_exchange_rate_prediction: None) -> None:
     mocked_exchange_rate_prediction.return_value = 1
     assert cryptocurrency_action(1.0) == "Do nothing"
