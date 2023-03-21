cb2c95d7131fc23030df0513a17545c825d68c3c
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-03-21 10:02:54 +0200

    Use @pytest.mark.parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,47 +1,33 @@
 import pytest
 from unittest import mock
+from typing import Callable
 
 from app.main import cryptocurrency_action
 
 
-@pytest.fixture()
-def mocked_exchange_prediction() -> None:
-    with mock.patch("app.main.get_exchange_rate_prediction") as mocked:
-        yield mocked
-
-
-class TestCryptocurrencyAction:
-    def test_buy_more_cryptocurrency(
-            self,
-            mocked_exchange_prediction: None
-    ) -> None:
-        mocked_exchange_prediction.return_value = 1.06
-        assert cryptocurrency_action(1) == "Buy more cryptocurrency"
-
-    def test_sell_your_cryptocurrency(
-            self,
-            mocked_exchange_prediction: None
-    ) -> None:
-        mocked_exchange_prediction.return_value = 0.94
-        assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
-
-    def test_do_nothing_if_predicted_exchange_is_not_enough_higher(
-            self,
-            mocked_exchange_prediction: None
-    ) -> None:
-        mocked_exchange_prediction.return_value = 1.05
-        assert cryptocurrency_action(1) == "Do nothing"
-
-    def test_do_nothing_if_predicted_exchange_is_not_enough_lower(
-            self,
-            mocked_exchange_prediction: None
-    ) -> None:
-        mocked_exchange_prediction.return_value = 0.95
-        assert cryptocurrency_action(1) == "Do nothing"
-
-    def test_do_nothing_if_predicted_exchange_is_same(
-            self,
-            mocked_exchange_prediction: None
-    ) -> None:
-        mocked_exchange_prediction.return_value = 1
-        assert cryptocurrency_action(1) == "Do nothing"
+@pytest.mark.parametrize(
+    "current_rate,predicted_rate,output",
+    [
+        (1, 1.06, "Buy more cryptocurrency"),
+        (1, 0.94, "Sell all your cryptocurrency"),
+        (1, 1.05, "Do nothing"),
+        (1, 0.95, "Do nothing"),
+        (1, 1, "Do nothing")
+    ],
+    ids=[
+        "More than 5% higher",
+        "More than 5% lower",
+        "Difference is not that much",
+        "Difference is not that much",
+        "Current and predicted rates are the same"
+    ]
+)
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_rate(
+        mocked_function: Callable,
+        current_rate: int | float,
+        predicted_rate: int | float,
+        output: str
+) -> None:
+    mocked_function.return_value = predicted_rate
+    assert cryptocurrency_action(current_rate) == output
