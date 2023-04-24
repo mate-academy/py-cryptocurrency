38be40bf8118f72de21eac332268870c368f857f
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-24 23:18:51 +0300

    more test added

--- app/test_main.py
+++ app/test_main.py
@@ -12,17 +12,23 @@ def test_function_was_called() -> None:
 
 def test_function_should_return_buy_more_cryptocurrency() -> None:
     with mock.patch("app.main.get_exchange_rate_prediction") as mocked_func:
-        mocked_func.return_value = 2
+        mocked_func.return_value = 1.06
         assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
 def test_function_should_return_sell_all_your_cryptocurrency() -> None:
     with mock.patch("app.main.get_exchange_rate_prediction") as mocked_func:
-        mocked_func.return_value = 0.5
+        mocked_func.return_value = 0.94
         assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
 
 
-def test_function_should_return_do_nothing() -> None:
+def test_function_should_return_do_nothing_max_value() -> None:
     with mock.patch("app.main.get_exchange_rate_prediction") as mocked_func:
-        mocked_func.return_value = 1.04
+        mocked_func.return_value = 1.05
+        assert cryptocurrency_action(1) == "Do nothing"
+
+
+def test_function_should_return_do_nothing_min_value() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") as mocked_func:
+        mocked_func.return_value = 0.95
         assert cryptocurrency_action(1) == "Do nothing"
