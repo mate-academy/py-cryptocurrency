d3d1f694382530dc495677068340bba35e62c989
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-24 23:11:53 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+from unittest import mock
+
+import app
+from app.main import cryptocurrency_action, get_exchange_rate_prediction
+
+
+def test_function_was_called() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") as mocked_func:
+        mocked_func.return_value = 1
+        cryptocurrency_action(1)
+        mocked_func.assert_called_once_with(1)
+
+
+def test_function_should_return_buy_more_cryptocurrency() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") as mocked_func:
+        mocked_func.return_value = 2
+        assert cryptocurrency_action(1) == "Buy more cryptocurrency"
+
+
+def test_function_should_return_sell_all_your_cryptocurrency() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") as mocked_func:
+        mocked_func.return_value = 0.5
+        assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
+
+
+def test_function_should_return_do_nothing() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") as mocked_func:
+        mocked_func.return_value = 1.04
+        assert cryptocurrency_action(1) == "Do nothing"
