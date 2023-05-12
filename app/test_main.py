8aa07228894e94b51fd15fc06102695115b8a834
Maks Blazhko <makssimm1@gmail.com>
2023-05-12 11:56:13 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from typing import Any
+
+from app import main
+from app.main import cryptocurrency_action
+import pytest
+
+
+@pytest.mark.parametrize(
+    "prediction, rate, expected_result",
+    [
+        pytest.param(1.05, 1, "Do nothing", id="should not buy"),
+        pytest.param(0.95, 1, "Do nothing", id="should not sell"),
+    ],
+)
+def test_should_return_correct_string(
+    monkeypatch: Any,
+    prediction: int | float,
+    rate: int | float,
+    expected_result: str,
+) -> None:
+    def mock_prediction(current_rate: int | float) -> int:
+        return prediction
+
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", mock_prediction)
+
+    assert cryptocurrency_action(rate) == expected_result
