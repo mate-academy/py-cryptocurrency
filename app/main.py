aec9322ff7ea59529ab94cea26da8bcd8fb1f07f
Ivan <ivan.korshunov.1997@gmail.com>
2023-05-21 15:51:56 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -4,13 +4,14 @@ from typing import Union
 
 def get_exchange_rate_prediction(exchange_rate: Union[int, float]) -> float:
     if random.choice(["increase", "decrease"]) == "increase":
-        return round(exchange_rate / random.random(), 2)
+        return round(exchange_rate / random.random(), 2)  # more
     else:
-        return round(exchange_rate * random.random(), 2)
+        return round(exchange_rate * random.random(), 2)  # less
 
 
 def cryptocurrency_action(current_rate: Union[int, float]) -> str:
     prediction_rate = get_exchange_rate_prediction(current_rate)
+    print(prediction_rate)
     if prediction_rate / current_rate > 1.05:
         return "Buy more cryptocurrency"
     if prediction_rate / current_rate < 0.95:
--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+import pytest
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+@pytest.fixture
+def mocked_get_exchange_rate_prediction(
+        request: any) -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") \
+            as mocked_get_exchange_rate_prediction:
+        exchange_rate = request.param
+        mocked_get_exchange_rate_prediction.return_value = exchange_rate
+        yield mocked_get_exchange_rate_prediction
+
+
+@pytest.mark.parametrize("exchange_rate, expected_action", [
+    (95.0, "Sell all your cryptocurrency"),
+    (105.0, "Buy more cryptocurrency"),
+])
+def test_cryptocurrency_action(exchange_rate: int,
+                               expected_action: str,
+                               mocked_get_exchange_rate_prediction: any
+                               ) -> None:
+
+    assert cryptocurrency_action(100) == expected_action
