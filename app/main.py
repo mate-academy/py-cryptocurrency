6ee16453f316b6c739edb05369cf4266435521a5
Ivan <ivan.korshunov.1997@gmail.com>
2023-05-21 16:01:50 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -17,3 +17,5 @@ def cryptocurrency_action(current_rate: Union[int, float]) -> str:
     if prediction_rate / current_rate < 0.95:
         return "Sell all your cryptocurrency"
     return "Do nothing"
+
+print()
--- app/test_main.py
+++ app/test_main.py
@@ -8,7 +8,7 @@ from app.main import cryptocurrency_action
 def mocked_get_exchange_rate_prediction() -> None:
     with mock.patch("app.main.get_exchange_rate_prediction") \
             as mocked_get_exchange_rate_prediction:
-        mocked_get_exchange_rate_prediction.return_value = 95.0
+        mocked_get_exchange_rate_prediction.return_value = 94.0
         yield mocked_get_exchange_rate_prediction
 
 
@@ -16,8 +16,36 @@ def mocked_get_exchange_rate_prediction() -> None:
 def mocked_get_exchange_rate_prediction2() -> None:
     with mock.patch("app.main.get_exchange_rate_prediction") \
             as mocked_get_exchange_rate_prediction:
+        mocked_get_exchange_rate_prediction.return_value = 106.0
+        yield mocked_get_exchange_rate_prediction
+
+
+@pytest.fixture
+def mocked_get_exchange_rate_prediction3() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") \
+            as mocked_get_exchange_rate_prediction:
         mocked_get_exchange_rate_prediction.return_value = 105.0
         yield mocked_get_exchange_rate_prediction
+
+
+@pytest.fixture
+def mocked_get_exchange_rate_prediction4() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") \
+            as mocked_get_exchange_rate_prediction:
+        mocked_get_exchange_rate_prediction.return_value = 95.0
+        yield mocked_get_exchange_rate_prediction
+
+
+def test_cryptocurrency_action4(
+        mocked_get_exchange_rate_prediction4: any
+) -> None:
+    assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_cryptocurrency_action3(
+        mocked_get_exchange_rate_prediction3: any
+) -> None:
+    assert cryptocurrency_action(100) == "Do nothing"
 
 
 def test_cryptocurrency_action2(
