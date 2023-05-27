be3b44011a6c1f19c23b7229f28d155c126f153e
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-05-27 19:27:28 +0300

    reuturn_value_fixed

--- app/test_main.py
+++ app/test_main.py
@@ -8,12 +8,12 @@ class CryptocurrencyActionTestCase(unittest.TestCase):
     def test_cryptocurrency_action(self,
                                    mock_get_exchange_rate_prediction: None
                                    ) -> None:
-        mock_get_exchange_rate_prediction.return_value = 105.1
-        self.assertEqual(cryptocurrency_action(100),
+        mock_get_exchange_rate_prediction.return_value = 12.1
+        self.assertEqual(cryptocurrency_action(7),
                          "Buy more cryptocurrency")
-        mock_get_exchange_rate_prediction.return_value = 94.9
-        self.assertEqual(cryptocurrency_action(100),
+        mock_get_exchange_rate_prediction.return_value = 1.99
+        self.assertEqual(cryptocurrency_action(7),
                          "Sell all your cryptocurrency")
-        mock_get_exchange_rate_prediction.return_value = 101
-        self.assertEqual(cryptocurrency_action(100),
+        mock_get_exchange_rate_prediction.return_value = 7
+        self.assertEqual(cryptocurrency_action(7),
                          "Do nothing")
