adde28c2f64203a2a22a97020763876fa778bb73
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-05-27 19:57:33 +0300

    pytested

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,31 @@
-import unittest
-from unittest.mock import patch
-from app.main import cryptocurrency_action
+from typing import Callable
+from app.main import cryptocurrency_action, get_exchange_rate_prediction
+from unittest import mock
+import pytest
 
 
-class CryptocurrencyActionTestCase(unittest.TestCase):
-    @patch("app.main.get_exchange_rate_prediction")
-    def test_cryptocurrency_action(self,
-                                   mock_get_exchange_rate_prediction: None
-                                   ) -> None:
-        mock_get_exchange_rate_prediction.return_value = 12.1
-        self.assertEqual(cryptocurrency_action(7),
-                         "Buy more cryptocurrency")
-        mock_get_exchange_rate_prediction.return_value = 1.99
-        self.assertEqual(cryptocurrency_action(7),
-                         "Sell all your cryptocurrency")
-        mock_get_exchange_rate_prediction.return_value = 7
-        self.assertEqual(cryptocurrency_action(7),
-                         "Do nothing")
+@pytest.fixture()
+def mocked_exchange_rate_prediction() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") \
+            as mock_prediction:
+        yield mock_prediction
+
+
+def test_buy(mocked_exchange_rate_prediction: Callable) -> None:
+    mocked_exchange_rate_prediction.return_value = 1.0500000001
+    assert cryptocurrency_action(1) == "Buy more cryptocurrency"
+
+
+def test_sell(mocked_exchange_rate_prediction: Callable) -> None:
+    mocked_exchange_rate_prediction.return_value = 0.94999999
+    assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
+
+
+def do_nothing_high(mocked_exchange_rate_prediction: Callable) -> None:
+    mocked_exchange_rate_prediction.return_value = 1.05
+    assert cryptocurrency_action(1) == "Do nothing"
+
+
+def do_nothing_low(mocked_exchange_rate_prediction) -> None:
+    mocked_exchange_rate_prediction.return_value = 0.95
+    assert cryptocurrency_action(1) == "Do nothing"
