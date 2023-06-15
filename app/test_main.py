a58774ad9723c9cdfdb2f9f924df959a95fccfb2
Olha Tryhub <olha.tryhub@nure.ua>
2023-06-15 23:11:04 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,32 @@
-# write your code here
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+def test_cryptocurrency_increase_much_more_than_5() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", lambda a: 150):
+        assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+def test_cryptocurrency_increase_little_more_than_5() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", lambda a: 106):
+        assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+def test_cryptocurrency_increase_is_5() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", lambda a: 105):
+        assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_cryptocurrency_decrease_is_5() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", lambda a: 95):
+        assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_cryptocurrency_decrease_much_less_than_5() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", lambda a: 50):
+        assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+def test_cryptocurrency_decrease_little_less_than_5() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", lambda a: 94):
+        assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
