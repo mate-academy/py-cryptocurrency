a8f32c84f3dec8e0610b279fd1ee5f5fef578754
Tatasha <natashaleonidovna319@gmail.com>
2023-06-16 00:14:50 +0300

    'Solution3'

--- app/test_main.py
+++ app/test_main.py
@@ -7,14 +7,14 @@ class TestCryptoCurrency:
     @pytest.mark.parametrize(
         "prediction_rate,result",
         [
-            (127, "Sell all your cryptocurrency"),
-            (166, "Buy more cryptocurrency"),
-            (152, "Do nothing"),
-            (151.62, "Do nothing")
+            (80, "Sell all your cryptocurrency"),
+            (155, "Buy more cryptocurrency"),
+            (95, "Do nothing"),
+            (105, "Do nothing")
         ]
     )
     def test_main(self, prediction_rate: int | float, result: str) -> None:
         with mock.patch("app.main.get_exchange_rate_prediction") as rate:
             rate.return_value = prediction_rate
-            assert cryptocurrency_action(144.4) == result
-            rate.assert_called_once_with(144.4)
+            assert cryptocurrency_action(100) == result
+            rate.assert_called_once_with(100)
