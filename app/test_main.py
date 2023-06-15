7f5447a3c94997003a2e3cccd9f8621fb4459732
YOUR_NAME <YOUR_EMAIL>
2023-06-15 16:13:04 +0300

    Tests cryptocurrency (Second commit):

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,21 @@
+from typing import Callable
 from unittest import mock
 
 from app.main import cryptocurrency_action
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action(
-        mock_get_exchange_rate_prediction: mock.Mock
+def test_should_return_do_nothing_105(
+        mocked_get_exchange_rate_prediction: Callable
 ) -> None:
-    mock_get_exchange_rate_prediction.return_value = 105
+    mocked_get_exchange_rate_prediction.return_value = 105
+    assert cryptocurrency_action(100) == "Do nothing"
+
+    mocked_get_exchange_rate_prediction.return_value = 95
+    assert cryptocurrency_action(100) == "Do nothing"
+
+    mocked_get_exchange_rate_prediction.return_value = 106
     assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
-    mock_get_exchange_rate_prediction.return_value = 95
+    mocked_get_exchange_rate_prediction.return_value = 94
     assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
-
-    mock_get_exchange_rate_prediction.return_value = 100
-    assert cryptocurrency_action(100) == "Do nothing"
