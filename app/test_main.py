ad76330756943776b36f60eca6cd4172369c9324
Denys Raboshchuk <kontroler444@gmail.com>
2023-06-21 16:59:06 +0300

    Done!

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,34 @@
-# write your code here
+import pytest
+
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+@pytest.fixture()
+def mocked_get_exchange_rate_prediction() -> mock:
+    with mock.patch("app.main.get_exchange_rate_prediction") as mocked_func:
+        yield mocked_func
+
+
+def test_with_buy_more(mocked_get_exchange_rate_prediction: mock) -> None:
+    mocked_get_exchange_rate_prediction.return_value = 1.5
+    assert cryptocurrency_action(1) == "Buy more cryptocurrency"
+
+
+def test_with_sell_all(mocked_get_exchange_rate_prediction: mock) -> None:
+    mocked_get_exchange_rate_prediction.return_value = 0.5
+    assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
+
+
+def test_do_nothing(mocked_get_exchange_rate_prediction: mock) -> None:
+    mocked_get_exchange_rate_prediction.return_value = 1.05
+    assert cryptocurrency_action(1) == "Do nothing"
+
+
+def test_extreme_digital_value(
+        mocked_get_exchange_rate_prediction: mock
+) -> None:
+
+    mocked_get_exchange_rate_prediction.return_value = 0.95
+    assert cryptocurrency_action(1) == "Do nothing"
