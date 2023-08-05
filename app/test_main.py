2016874295604c18a3f74a5234e57c890afa2782
Nazarii Schcur <nazae530@gmail.com>
2023-08-05 22:14:12 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+import pytest
+from app.main import cryptocurrency_action
+
+# Test cases for cryptocurrency_action function
+
+
+@pytest.mark.parametrize("current_rate, expected_action", [
+    (100, "Buy more cryptocurrency"),  # Predicted rate > 5% increase
+    (95, "Sell all your cryptocurrency"),  # Predicted rate > 5% decrease
+    (98, "Do nothing"),  # Predicted rate < 5% decrease
+    (100, "Do nothing"),  # Predicted rate is equal to current rate
+])
+def test_cryptocurrency_action(
+        current_rate: int,
+        expected_action: float
+) -> None:
+    assert cryptocurrency_action(current_rate) == expected_action
