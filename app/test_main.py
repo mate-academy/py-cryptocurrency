4441bd459de59d16f5f0316a1f2f3cdb8f55e1c8
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-08-18 20:34:19 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,38 @@
-# write your code here
+import app.main as main
+import pytest
+from typing import Callable
+
+
+@pytest.mark.parametrize(
+    "mock_function, expected_action",
+    [
+        pytest.param(
+            lambda rate: rate / 0.5, "Buy more cryptocurrency", id="Buy action"
+        ),
+        pytest.param(
+            lambda rate: rate * 0.5,
+            "Sell all your cryptocurrency",
+            id="Sell action",
+        ),
+        pytest.param(
+            lambda rate: rate + rate * 0.05,
+            "Do nothing",
+            id="Do nothing action when rate is 5% higher",
+        ),
+        pytest.param(
+            lambda rate: round((rate * 0.95), 2),
+            "Do nothing",
+            id="Do nothing action when rate is 5% lower",
+        ),
+    ],
+)
+def test_cryptocurrency_action_with_parametrized_mocks(
+    mock_function: Callable,
+    expected_action: str,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """
+    Test that cryptocurrency_action returns the expected action
+    """
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", mock_function)
+    assert main.cryptocurrency_action(10) == expected_action
