b059571f422c8655110b2527541a1124817a5cea
Andrii Ishchenko <iscenkoa@gmail.com>
2023-08-20 10:29:05 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -16,6 +16,11 @@ from app.main import cryptocurrency_action
         (100, 94, "Sell all your cryptocurrency"),
     ]
 )
-def test_get_exchange_rate_prediction(current_rate, predicted_rate, prediction):
-    with mock.patch("app.main.get_exchange_rate_prediction", return_value=predicted_rate):
-        assert cryptocurrency_action(current_rate) == prediction
+def test_get_exchange_rate_prediction(
+        current_rate: int,
+        predicted_rate: int,
+        prediction: str) -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction",
+                    return_value=predicted_rate):
+        assert (cryptocurrency_action(current_rate)
+                == prediction)
