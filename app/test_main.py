494f0fafd66fe84756d2a1be0b1a4c3d5c78c3eb
Vasya Turok <turokvasil0@gmail.com>
2023-08-25 02:53:09 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action(
+        mock_get_exchange_rate_prediction: float
+) -> None:
+    test_data = [
+        [106, "Buy more cryptocurrency"],
+        [94, "Sell all your cryptocurrency"],
+        [105, "Do nothing"],
+        [95, "Do nothing"],
+        [102, "Do nothing"]
+    ]
+    for (
+        mock_get_exchange_rate_prediction.return_value,
+        expected_result
+    ) in test_data:
+
+        assert cryptocurrency_action(100) == expected_result
