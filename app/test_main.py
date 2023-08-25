7b8ee5ce14e784cc2aa24d42833914307a1626a4
jinjo <vitaly.tyagunov@gmail.com>
2023-08-25 09:45:10 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,16 @@
-# write your code here
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action(mock_get_exchange_rate: mock) -> None:
+    mock_get_exchange_rate.side_effect = [95, 100, 105, 94, 106]
+    expected_results = [
+        "Do nothing",
+        "Do nothing",
+        "Do nothing",
+        "Sell all your cryptocurrency",
+        "Buy more cryptocurrency"
+    ]
+    for result in expected_results:
+        assert cryptocurrency_action(100) == result
