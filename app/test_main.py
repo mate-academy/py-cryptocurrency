8e1a17658ff6c98dda4b98169257509aed046d10
jinjo <vitaly.tyagunov@gmail.com>
2023-08-25 10:48:59 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,16 +1,24 @@
 from unittest import mock
 from app.main import cryptocurrency_action
+import pytest
 
 
-@mock.patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action(mock_get_exchange_rate: mock) -> None:
-    mock_get_exchange_rate.side_effect = [95, 100, 105, 94, 106]
-    expected_results = [
-        "Do nothing",
-        "Do nothing",
-        "Do nothing",
-        "Sell all your cryptocurrency",
-        "Buy more cryptocurrency"
+@pytest.mark.parametrize(
+    "current_rate, prediction_rate, expected_result",
+    [
+        (100, 95, "Do nothing"),
+        (100, 100, "Do nothing"),
+        (100, 105, "Do nothing"),
+        (100, 94, "Sell all your cryptocurrency"),
+        (100, 106, "Buy more cryptocurrency")
     ]
-    for result in expected_results:
-        assert cryptocurrency_action(100) == result
+)
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action(
+        mock_get_exchange_rate: mock,
+        current_rate: int,
+        prediction_rate: int,
+        expected_result: int,
+) -> None:
+    mock_get_exchange_rate.return_value = prediction_rate
+    assert cryptocurrency_action(current_rate) == expected_result
