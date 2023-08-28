c821c15496087f88dbf094a16707bd11ae6d55c2
Bohdan Kolomiiets <kb907406@gmail.com>
2023-08-28 21:47:21 +0300

    Solution(updated)

--- app/test_main.py
+++ app/test_main.py
@@ -6,25 +6,35 @@ from typing import Callable
 
 @pytest.fixture()
 def mock_prediction() -> Callable:
-    with mock.patch("app.main.get_exchange_rate_prediction") as mocked_prediction:
+    with mock.patch(
+            "app.main.get_exchange_rate_prediction"
+    ) as mocked_prediction:
         yield mocked_prediction
 
 
-def test_cryptocurrency_action_with_better_rate(mock_prediction) -> None:
+def test_cryptocurrency_action_with_better_rate(
+        mock_prediction: Callable
+) -> None:
     mock_prediction.return_value = 1
     assert cryptocurrency_action(1.5) == "Sell all your cryptocurrency"
 
 
-def test_cryptocurrency_action_with_bad_rate(mock_prediction) -> None:
+def test_cryptocurrency_action_with_bad_rate(
+        mock_prediction: Callable
+) -> None:
     mock_prediction.return_value = 1.5
     assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
-def test_cryptocurrency_action_first_exception(mock_prediction) -> None:
+def test_cryptocurrency_action_first_exception(
+        mock_prediction: Callable
+) -> None:
     mock_prediction.return_value = 0.95
     assert cryptocurrency_action(1) == "Do nothing"
 
 
-def test_cryptocurrency_action_second_exception(mock_prediction) -> None:
+def test_cryptocurrency_action_second_exception(
+        mock_prediction: Callable
+) -> None:
     mock_prediction.return_value = 1.05
     assert cryptocurrency_action(1) == "Do nothing"
