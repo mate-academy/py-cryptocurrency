778e7d9bb011e14e3c32a256ffb897b212402af4
Ostap <ostapbojko04@gmail.com>
2023-09-01 16:24:15 +0300

    py-crypto2

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,5 @@
 from unittest.mock import patch
-
 import pytest
-
 from app.main import cryptocurrency_action
 
 
@@ -11,22 +9,23 @@ def mock_exchange_rate_prediction() -> None:
         yield mock
 
 
-def test_cryptocurrency_action_buy(
-        mock_exchange_rate_prediction: any) -> None:
-    mock_exchange_rate_prediction.return_value = 1.06 * 100
+def test_cryptocurrency_action_buy(mock_exchange_rate_prediction: str) -> None:
+    mock_exchange_rate_prediction.return_value = 1.05 * 100
     result = cryptocurrency_action(100)
-    assert result == "Buy more cryptocurrency"
+    assert result == "Do nothing"
 
 
-def test_cryptocurrency_action_buy2(
-        mock_exchange_rate_prediction: any) -> None:
-    mock_exchange_rate_prediction.return_value = 3.01 * 100
+def test_cryptocurrency_action_sell(
+        mock_exchange_rate_prediction: str
+) -> None:
+    mock_exchange_rate_prediction.return_value = 0.95 * 100
     result = cryptocurrency_action(100)
-    assert result == "Buy more cryptocurrency"
+    assert result == "Do nothing"
 
 
-def test_cryptocurrency_action_buy3(
-        mock_exchange_rate_prediction: any) -> None:
-    mock_exchange_rate_prediction.return_value = 3.04 * 100
+def test_cryptocurrency_action_do_nothing(
+        mock_exchange_rate_prediction: str
+) -> None:
+    mock_exchange_rate_prediction.return_value = 1.02 * 100
     result = cryptocurrency_action(100)
-    assert result == "Buy more cryptocurrency"
+    assert result == "Do nothing"
