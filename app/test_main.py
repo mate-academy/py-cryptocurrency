3ff419d369d9c53d0e93865ae35a909d39480f26
Kosta Stepanjuk <stepanjukkosta@qmail.com>
2023-09-09 13:56:07 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_funct_value_if_exchange_rate_higher_current_rate(
+    mock_rate: mock.MagicMock
+) -> None:
+    mock_rate.return_value = 1.051
+
+    assert cryptocurrency_action(1) == "Buy more cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_funct_value_if_exchange_rate_lower_current_rate(
+    mock_rate: mock.MagicMock
+) -> None:
+    mock_rate.return_value = 0.949
+
+    assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_funct_value_if_difference_not_much(
+    mock_rate: mock.MagicMock
+) -> None:
+    mock_rate.return_value = 1
+
+    assert cryptocurrency_action(1) == "Do nothing"
