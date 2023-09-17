4b5ba69515f8c9925ab4341729c2695be1084a5d
Ruslan <rusipbox@gmail.com>
2023-09-17 17:49:11 +0300

    Test(py-cryptocurrency)

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,22 @@
-import pytest
-from app import main
-from typing import Any
+from app.main import get_exchange_rate_prediction, cryptocurrency_action
 
 
-def rate_105_buy_cryptocurrency(current_rate: int or float) -> str:
-    prediction_rate = main.get_exchange_rate_prediction(current_rate)
-    if prediction_rate / current_rate >= 1.05:
-        return "Buy more cryptocurrency"
-    if prediction_rate / current_rate < 0.95:
-        return "Sell all your cryptocurrency"
-    return "Do nothing"
+def test_rate_105_buy_cryptocurrency():
+    current_rate = 1.0
+    prediction_rate = get_exchange_rate_prediction(current_rate)
+    result = cryptocurrency_action(current_rate)
+    if prediction_rate / current_rate > 1.05:
+        assert result == "Buy more cryptocurrency"
+    elif prediction_rate / current_rate < 0.95:
+        assert result == "Sell all your cryptocurrency"
+    else:
+        assert result == "Do nothing"
 
 
-def test_rate_105_percent_do_nothing(monkeypatch: Any) -> None:
-    monkeypatch.setattr(
-        main,
-        "cryptocurrency_action",
-        rate_105_buy_cryptocurrency
-    )
-    test_result = pytest.main(["app/test_main.py"])
-    assert test_result == 0, (
+def test_rate_105_percent_do_nothing():
+    current_rate = 1.0
+    prediction_rate = get_exchange_rate_prediction(current_rate)
+    assert prediction_rate / current_rate != 1.05, (
         "You should not buy cryptocurrency when "
         "prediction_rate / current_rate == 1.05"
     )
