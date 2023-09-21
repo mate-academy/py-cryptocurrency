b7965670d94d20ae686342427f65a17976b96caf
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-21 19:03:33 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,25 @@
-import unittest
 from app.main import cryptocurrency_action
 
 
-class TestCryptocurrencyAction(unittest.TestCase):
-
-    def test_buy_more_cryptocurrency(self):
-        action = cryptocurrency_action(1000)
-        self.assertEqual(action, "Buy more cryptocurrency")
-
-    def test_sell_all_cryptocurrency(self):
-        action = cryptocurrency_action(1000)
-        self.assertEqual(action, "Sell all your cryptocurrency")
-
-    def test_do_nothing(self):
-        action = cryptocurrency_action(1000)
-        self.assertEqual(action, "Do nothing")
-
-    def test_edge_case_current_rate_zero(self):
-        action = cryptocurrency_action(0)
-        self.assertEqual(action, "Do nothing")
+def test_buy_more_cryptocurrency():
+    # Передбачення обмінного курсу більше поточного на більше 5%.
+    action = cryptocurrency_action(1000)
+    assert action == "Buy more cryptocurrency"
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_sell_all_cryptocurrency():
+    # Передбачення обмінного курсу менше поточного на більше 5%.
+    action = cryptocurrency_action(1000)
+    assert action == "Sell all your cryptocurrency"
+
+
+def test_do_nothing():
+    # Передбачення обмінного курсу змінюється менше ніж на 5% відносно поточного.
+    action = cryptocurrency_action(1000)
+    assert action == "Do nothing"
+
+
+def test_edge_case_current_rate_zero():
+    # Перевірка для крайнього випадку, коли поточний курс дорівнює нулю.
+    action = cryptocurrency_action(0)
+    assert action == "Do nothing"
