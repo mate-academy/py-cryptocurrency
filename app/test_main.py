5a1cf1e74862b4f66c8fa2aeb5ec82abbd83cf53
Misha Shushman <endernagashushman@gmail.com>
2023-09-21 22:40:31 +0300

    Flake8 fixes

--- app/test_main.py
+++ app/test_main.py
@@ -1,32 +1,34 @@
+from typing import Callable
 from unittest import mock
 
 from app.main import cryptocurrency_action
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_nothing_to_do(exchange):
+def test_nothing_to_do(exchange: Callable) -> None:
     exchange.return_value = 1.0
     assert cryptocurrency_action(1) == "Do nothing"
 
 
-
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_buy_more_to_do(exchange):
+def test_buy_more_to_do(exchange: Callable) -> None:
     exchange.return_value = 1.10
     assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
+
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_sell_to_do(exchange):
+def test_sell_to_do(exchange: Callable) -> None:
     exchange.return_value = 0.93
     assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
 
+
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_nothing_to_do_when_exchange_equal_0_95(exchange):
+def test_nothing_to_do_when_exchange_equal_0_95(exchange: Callable) -> None:
     exchange.return_value = 0.95
     assert cryptocurrency_action(1) == "Do nothing"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_nothing_to_do_when_exchange_equal_1_05(exchange):
+def test_nothing_to_do_when_exchange_equal_1_05(exchange: Callable) -> None:
     exchange.return_value = 1.05
     assert cryptocurrency_action(1) == "Do nothing"
