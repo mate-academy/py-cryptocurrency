e0534f0f046404bbbd92119619d19352767c1dea
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-24 17:47:01 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,33 @@
+import unittest
+from unittest.mock import Mock
 from app.main import cryptocurrency_action
 
 
-def test_buy_more_cryptocurrency() -> None:
-    action = cryptocurrency_action(1000)
-    assert action == "Buy more cryptocurrency"
+class TestCryptocurrencyAction(unittest.TestCase):
+    def setUp(self):
+        self.mock_get_exchange_rate_prediction = Mock()
+
+    def test_buy_more_cryptocurrency(self) -> None:
+        self.mock_get_exchange_rate_prediction.return_value = 1.1
+
+        action = cryptocurrency_action(1.0)
+
+        self.assertEqual(action, "Buy more cryptocurrency")
+
+    def test_sell_all_cryptocurrency(self) -> None:
+        self.mock_get_exchange_rate_prediction.return_value = 0.9
+
+        action = cryptocurrency_action(1.0)
+
+        self.assertEqual(action, "Sell all your cryptocurrency")
+
+    def test_do_nothing(self) -> None:
+        self.mock_get_exchange_rate_prediction.return_value = 1.03
+
+        action = cryptocurrency_action(1.0)
+
+        self.assertEqual(action, "Do nothing")
 
 
-def test_sell_all_cryptocurrency() -> None:
-    action = cryptocurrency_action(1000)
-    assert action == "Sell all your cryptocurrency"
-
-
-def test_do_nothing() -> None:
-    action = cryptocurrency_action(1000)
-    assert action == "Do nothing"
-
-
-def test_edge_case_current_rate_zero() -> None:
-    action = cryptocurrency_action(0)
-    assert action == "Do nothing"
+if __name__ == "__main__":
+    unittest.main()
