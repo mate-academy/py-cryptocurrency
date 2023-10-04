bd218ef0166ec8447aec9ed949bb21d9cd30c01a
Даня Пономаренко <you@example.com>
2023-10-04 13:01:52 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,45 @@
-# write your code here
+from unittest import mock
+
+import pytest
+
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def tester(func: mock) -> None:
+    func.return_value = 1
+    cryptocurrency_action(2)
+
+    func.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "value,ans",
+    [
+        (
+            4,
+            "Buy more cryptocurrency"
+        ),
+        (
+            1,
+            "Do nothing"
+        ),
+        (
+            0.5,
+            "Sell all your cryptocurrency"
+
+        ),
+        (
+            1.05,
+            "Do nothing"
+        ),
+        (
+            0.95,
+            "Do nothing"
+        )
+    ]
+)
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_sell(func: mock, value: int, ans: str) -> None:
+    func.return_value = value
+    assert cryptocurrency_action(1) == ans
