44ea22e51c79d66fc8e0e2030db09f9b214e9956
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-19 17:44:58 +0300

    s1

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,7 @@ from unittest import mock
 
 
 @mock.patch("main.get_exchange_rate_prediction")
-def test_function_has_called(mocked_exchange):
+def test_function_has_called(mocked_exchange: mock) -> None:
     mocked_exchange.return_value(1.1)
     cryptocurrency_action(100)
     mocked_exchange.assert_called_once_with("Buy more cryptocurrency")
