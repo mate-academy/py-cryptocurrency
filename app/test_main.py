79858fb897cffba0b513e22aeac8fa5ab4aa0ce7
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-19 17:42:44 +0300

    test sol

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,10 @@
-# write your code here
+from app.main import cryptocurrency_action
+
+from unittest import mock
+
+
+@mock.patch("main.get_exchange_rate_prediction")
+def test_function_has_called(mocked_exchange):
+    mocked_exchange.return_value(1.1)
+    cryptocurrency_action(100)
+    mocked_exchange.assert_called_once_with("Buy more cryptocurrency")
