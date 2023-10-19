caece19bb1973f50bbb50de5dfe43cd486a45a0e
Vladyslav Sabo <lebwa1337@gmail.com>
2023-10-19 20:50:59 +0300

    finale

--- app/test_main.py
+++ app/test_main.py
@@ -2,34 +2,25 @@ from unittest import mock
 from app.main import cryptocurrency_action
 
 
-@mock.patch("app.main.random.choice", return_value="increase")
-@mock.patch("app.main.random.random", return_value=0.8)
-def test_if_random_choose_increase_and_0_dot_8(
-        mocked_rate: float,
-        mocked_choice: str) -> None:
+@mock.patch("app.main.get_exchange_rate_prediction", return_value=6.25)
+def test_if_exchange_rate_eq_6_25(
+        mocked_rate: float) -> None:
     assert cryptocurrency_action(5) == "Buy more cryptocurrency"
 
 
-@mock.patch("app.main.random.choice", return_value="decrease")
-@mock.patch("app.main.random.random", return_value=0.8)
-def test_if_random_choose_decrease_and_0_dot_8(
-        mocked_rate: float,
-        mocked_choice: str) -> None:
+@mock.patch("app.main.get_exchange_rate_prediction", return_value=4)
+def test_if_exchange_rate_eq_4(
+        mocked_rate: float) -> None:
     assert cryptocurrency_action(5) == "Sell all your cryptocurrency"
 
 
-@mock.patch("app.main.random.choice", return_value="increase")
-@mock.patch("app.main.random.random", return_value=0.99)
-def test_if_random_choose_increase_and_0_dot_99(
-        mocked_rate: float,
-        mocked_choice: int) -> None:
+@mock.patch("app.main.get_exchange_rate_prediction", return_value=5.25)
+def test_if_exchange_rate_eq_5_25(
+        mocked_rate: float) -> None:
     assert cryptocurrency_action(5) == "Do nothing"
 
 
-@mock.patch("app.main.prediction_rate / current_rate", return_value=1.05)
-@mock.patch("app.main.random.choice", return_value="decrease")
-@mock.patch("app.main.random.random", return_value=0.8)
-def test_if_random_choose_increase_and_1_dot_05(
-        mocked_rate: float,
-        mocked_choice: str) -> None:
+@mock.patch("app.main.get_exchange_rate_prediction", return_value=4.75)
+def test_if_exchange_rate_eq_4_75(
+        mocked_rate: float) -> None:
     assert cryptocurrency_action(5) == "Do nothing"
