78aba88c9242a82453f2398fb45e2f4f9a647819
Vladyslav Sabo <lebwa1337@gmail.com>
2023-10-19 20:37:41 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,35 @@
-# write your code here
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.random.choice", return_value="increase")
+@mock.patch("app.main.random.random", return_value=0.8)
+def test_if_random_choose_increase_and_0_dot_8(
+        mocked_rate: float,
+        mocked_choice: str) -> None:
+    assert cryptocurrency_action(5) == "Buy more cryptocurrency"
+
+
+@mock.patch("app.main.random.choice", return_value="decrease")
+@mock.patch("app.main.random.random", return_value=0.8)
+def test_if_random_choose_decrease_and_0_dot_8(
+        mocked_rate: float,
+        mocked_choice: str) -> None:
+    assert cryptocurrency_action(5) == "Sell all your cryptocurrency"
+
+
+@mock.patch("app.main.random.choice", return_value="increase")
+@mock.patch("app.main.random.random", return_value=0.99)
+def test_if_random_choose_increase_and_0_dot_99(
+        mocked_rate: float,
+        mocked_choice: str) -> None:
+    assert cryptocurrency_action(5) == "Do nothing"
+
+
+@mock.patch("app.main.prediction_rate / current_rate", return_value=1.05)
+@mock.patch("app.main.random.choice", return_value="decrease")
+@mock.patch("app.main.random.random", return_value=0.8)
+def test_if_random_choose_increase_and_1_dot_05(
+        mocked_rate: float,
+        mocked_choice: str) -> None:
+    assert cryptocurrency_action(5) == "Do nothing"
