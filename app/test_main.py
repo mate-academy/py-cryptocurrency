b73025d3469539de45fdb82d8198906552028782
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-10-21 18:45:06 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,10 +4,10 @@ from app.main import cryptocurrency_action
 
 
 @pytest.mark.parametrize("current_rate, prediction_rate, expected_action", [
-    (4.7, 9.3, "Buy more cryptocurrency"),
-    (1.0, 1.3, "Do nothing"),
-    (15.0, 7.1, "Sell all your cryptocurrency"),
-    (2.0, 2.75, "Do nothing")
+    (3.1, 5.2, "Buy more cryptocurrency"),
+    (5.1, 3.2, "Sell all your cryptocurrency"),
+    (5.0, 5.25, "Do nothing"),
+    (5.0, 4.75, "Do nothing")
 ])
 def test_cryptocurrency_action(current_rate: float,
                                prediction_rate: float,
