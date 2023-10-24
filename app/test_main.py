8b909e5e443ee2e5e84bc9a591eb2a6f1cdd9866
Dmytro Prokhach <dprokhach@gmail.com>
2023-10-24 20:06:29 +0300

    Done

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+import pytest
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    "mocked_course, result",
+    [
+        (3, "Buy more cryptocurrency"),
+        (1, "Sell all your cryptocurrency"),
+        (2.1, "Do nothing"),
+        (1.9, "Do nothing")
+    ]
+)
+def test_cryptocurrency_action(mocked_course: int, result: str) -> None:
+    with patch("app.main.get_exchange_rate_prediction",
+               return_value=mocked_course):
+        assert cryptocurrency_action(2) == result
