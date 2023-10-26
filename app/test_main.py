3b9e0e1a1a304f73c319e46e4321848f84d7b514
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-10-26 19:26:56 +0300

    Refact code

--- app/test_main.py
+++ app/test_main.py
@@ -1,27 +1,19 @@
-# write your code here
-from unittest import mock
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import pytest
 
 from app.main import cryptocurrency_action
 
-
-@mock.patch("app.main.get_exchange_rate_prediction")
 @pytest.mark.parametrize(
-    "current_rate, predict_rate, result",
+    "mocked, result",
     [
-        (1, 1, "Do nothing"),
-        (1, 0.93, "Sell all your cryptocurrency"),
-        (1, 1.01, "Do nothing"),
-        (1, 1.07, "Buy more cryptocurrency")
+        (3, "Buy more cryptocurrency"),
+        (1, "Sell all your cryptocurrency"),
+        (2.1, "Do nothing"),
+        (1, 9, "Do nothing")
+
     ]
 )
-def test_cryptocurrency_action(
-        mock_get_exchange_rate_prediction: Mock,
-        current_rate: int | float,
-        predict_rate: int | float,
-        result: str
-) -> None:
-    mock_get_exchange_rate_prediction.return_value = predict_rate
-    assert cryptocurrency_action(current_rate) == result
+def test_get_currency_action(mocked: Mock, result: str):
+    with patch('app.main.get_currency_action', return_value=mocked):
+        assert cryptocurrency_action(2) == result
