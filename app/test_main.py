210b9b67e07d38e2eee8384b932a218fea477fc2
Your_Name Your_Surname <your@email>
2023-10-31 10:48:51 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from app.main import cryptocurrency_action
+
+from unittest.mock import patch
+
+
+@patch(
+    "app.main.get_exchange_rate_prediction",
+    return_value=0.95
+)
+def test_should_return_buy_more_cryptocurrency(
+get_exchange_rate_prediction_mock: callable
+) -> None:
+    assert cryptocurrency_action(1) == "Do nothing"
+
+
+@patch(
+    "app.main.get_exchange_rate_prediction",
+    return_value=1.05
+)
+def test_should_return_sell_cryptocurrency(
+get_exchange_rate_prediction_mock: callable
+) -> None:
+    assert cryptocurrency_action(1) == "Do nothing"
