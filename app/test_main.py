d06dc4394f8264b672d813c7dd1872708f85d223
Frezworx <frezworx@gmail.com>
2023-11-01 16:03:53 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,38 @@
-from typing import Any, Union
+from typing import Any
+from unittest import TestCase, mock
 from app.main import cryptocurrency_action
 
-import pytest
 
+class TestCryptocurrencyAction(TestCase):
 
-@pytest.mark.parametrize(
-    "current_rate,prediction_rate,expected",
-    [
-        (1, 1.05, "Buy more cryptocurrency"),
-        (1, 0.95, "Sell all your cryptocurrency"),
-        (1, 1, "Do nothing"),
-    ],
-)
-def test_cryptocurrency_action(
-        monkeypatch: Any,
-        current_rate: int | float,
-        prediction_rate: int | float,
-        expected: str
-) -> None:
-    def mock_get_exchange_rate_prediction(
-            exchange_rate: Union[int, float]
-    ) -> float:
-        return prediction_rate
+    @mock.patch("app.main.get_exchange_rate_prediction")
+    def test_cryptocurrency_action(
+            self,
+            mock_get_exchange_rate_prediction: Any
+    ) -> None:
+        test_cases = [
+            {
+                "current_rate": 1,
+                "prediction_rate": 1.06,
+                "expected": "Buy more cryptocurrency",
+            },
+            {
+                "current_rate": 1,
+                "prediction_rate": 0.94,
+                "expected": "Sell all your cryptocurrency",
+            },
+            {
+                "current_rate": 1,
+                "prediction_rate": 1,
+                "expected": "Do nothing",
+            },
+        ]
 
-    monkeypatch.setattr(
-        "app.main.get_exchange_rate_prediction",
-        mock_get_exchange_rate_prediction
-    )
-
-    assert cryptocurrency_action(current_rate) == expected
+        for case in test_cases:
+            mock_get_exchange_rate_prediction.return_value = (
+                case.get("prediction_rate")
+            )
+            self.assertEqual(
+                cryptocurrency_action(case.get("current_rate")),
+                case.get("expected")
+            )
