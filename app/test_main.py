21872c19316464943e816713c0674fe3404c70b2
Illya Maznitskiy <comercaleuros@gmail.com>
2023-11-15 20:18:14 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -4,14 +4,22 @@ from app.main import cryptocurrency_action
 
 
 @patch("app.main.get_exchange_rate_prediction")
-@pytest.mark.parametrize("current_rate, predicted_rate, expected_action", [
-    (100, 105.1, "Buy more cryptocurrency"),
-    (100, 94.9, "Sell all your cryptocurrency"),
-    (100, 100.0, "Do nothing"),  # Edge case: Predicted rate exactly the same
-    (100, 105.0, "Do nothing"),  # Edge case: Predicted rate 5% higher
-    (100, 95.0, "Do nothing"),   # Edge case: Predicted rate 5% lower
-])
-def test_cryptocurrency_action(mock_get_exchange_rate_prediction, current_rate, predicted_rate, expected_action):
+@pytest.mark.parametrize(
+    "current_rate, predicted_rate, expected_action",
+    [
+        (100, 105.1, "Buy more cryptocurrency"),
+        (100, 94.9, "Sell all your cryptocurrency"),
+        (100, 100.0, "Do nothing"),
+        (100, 105.0, "Do nothing"),
+        (100, 95.0, "Do nothing"),
+    ]
+)
+def test_cryptocurrency_action(
+    mock_get_exchange_rate_prediction: patch,
+        current_rate: int,
+        predicted_rate: int,
+        expected_action: str
+) -> None:
     mock_get_exchange_rate_prediction.return_value = predicted_rate
     result = cryptocurrency_action(current_rate)
     assert result == expected_action
