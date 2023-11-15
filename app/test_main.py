2185c2a141cb103012ff377b17623937e0c9049c
Illya Maznitskiy <comercaleuros@gmail.com>
2023-11-15 20:14:10 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,18 @@
+import pytest
 from unittest.mock import patch
-
 from app.main import cryptocurrency_action
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action(mock_get_exchange_rate_prediction: any) -> None:
-    test_cases = [
-        (100, 105.1, "Buy more cryptocurrency"),
-        (100, 94.9, "Sell all your cryptocurrency"),
-        (100, 100.1, "Do nothing"),
-    ]
-
-    for current_rate, predicted_rate, expected_action in test_cases:
-
-        mock_get_exchange_rate_prediction.return_value = predicted_rate
-        result = cryptocurrency_action(current_rate)
-
-        assert result == expected_action
-
-        mock_get_exchange_rate_prediction.assert_called_with(current_rate)
+@pytest.mark.parametrize("current_rate, predicted_rate, expected_action", [
+    (100, 105.1, "Buy more cryptocurrency"),
+    (100, 94.9, "Sell all your cryptocurrency"),
+    (100, 100.0, "Do nothing"),  # Edge case: Predicted rate exactly the same
+    (100, 105.0, "Do nothing"),  # Edge case: Predicted rate 5% higher
+    (100, 95.0, "Do nothing"),   # Edge case: Predicted rate 5% lower
+])
+def test_cryptocurrency_action(mock_get_exchange_rate_prediction, current_rate, predicted_rate, expected_action):
+    mock_get_exchange_rate_prediction.return_value = predicted_rate
+    result = cryptocurrency_action(current_rate)
+    assert result == expected_action
+    mock_get_exchange_rate_prediction.assert_called_with(current_rate)
