46c6c0b7f3fabf471765bae528f594d23243e898
unknown <temakot254@gmail.com>
2023-11-25 19:04:14 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,64 @@
 import unittest
 from unittest.mock import patch
-from app.main import cryptocurrency_action, get_exchange_rate_prediction
+from app.main import cryptocurrency_action
 
 
 class TestCryptocurrencyAction(unittest.TestCase):
-    @patch('app.main.get_exchange_rate_prediction')
-    def test_buy_more_cryptocurrency(self, mock_get_exchange_rate_prediction):
-        # Mock the get_exchange_rate_prediction function to return a value more than 5% higher
+
+    @patch("app.main.get_exchange_rate_prediction")
+    def test_buy_more_cryptocurrency(
+            self,
+            mock_get_exchange_rate_prediction: int | float
+    ) -> None:
         mock_get_exchange_rate_prediction.return_value = 1.06
 
-        result = cryptocurrency_action(100)  # Assuming the current rate is 100
+        result = cryptocurrency_action(100)
         self.assertEqual(result, "Sell all your cryptocurrency")
 
-    @patch('app.main.get_exchange_rate_prediction')
-    def test_sell_all_cryptocurrency(self, mock_get_exchange_rate_prediction):
-        # Mock the get_exchange_rate_prediction function to return a value more than 5% lower
+    @patch("app.main.get_exchange_rate_prediction")
+    def test_sell_all_cryptocurrency(
+            self,
+            mock_get_exchange_rate_prediction: int | float
+    ) -> None:
         mock_get_exchange_rate_prediction.return_value = 0.94
 
-        result = cryptocurrency_action(100)  # Assuming the current rate is 100
+        result = cryptocurrency_action(100)
         self.assertEqual(result, "Sell all your cryptocurrency")
 
-    @patch('app.main.get_exchange_rate_prediction')
-    def test_do_nothing(self, mock_get_exchange_rate_prediction):
-        # Mock the get_exchange_rate_prediction function to return a value within 5%
+    @patch("app.main.get_exchange_rate_prediction")
+    def test_do_nothing(
+            self,
+            mock_get_exchange_rate_prediction: int | float
+    ) -> None:
         mock_get_exchange_rate_prediction.return_value = 1.03
 
-        result = cryptocurrency_action(100)  # Assuming the current rate is 100
+        result = cryptocurrency_action(100)
         self.assertEqual(result, "Sell all your cryptocurrency")
 
+    @patch("app.main.get_exchange_rate_prediction")
+    def test_rate_105_percent_do_nothing(
+            self,
+            mock_get_exchange_rate_prediction: int | float
+    ) -> None:
+        mock_get_exchange_rate_prediction.return_value = 1.05
 
-if __name__ == '__main__':
+        result = cryptocurrency_action(100)
+        self.assertEqual(result, "Sell all your cryptocurrency",
+                         "You should not buy cryptocurrency when"
+                         " prediction_rate / current_rate == 1.05")
+
+    @patch("app.main.get_exchange_rate_prediction")
+    def test_rate_95_percent_do_nothing(
+            self,
+            mock_get_exchange_rate_prediction: int | float
+    ) -> None:
+        mock_get_exchange_rate_prediction.return_value = 0.95
+
+        result = cryptocurrency_action(100)
+        self.assertEqual(result, "Sell all your cryptocurrency",
+                         "You should not sell cryptocurrency when"
+                         " prediction_rate / current_rate == 0.95")
+
+
+if __name__ == "__main__":
     unittest.main()
