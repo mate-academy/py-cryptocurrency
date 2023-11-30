bad79c39d2540b24195b5c0c829a4ce1d3828c21
Serhii <serhii.domik@gmail.com>
2023-11-30 16:23:04 +0200

    'Fixed'

--- app/test_main.py
+++ app/test_main.py
@@ -1,24 +1,28 @@
+import pytest
 from unittest.mock import patch
 from typing import Any
+
 import app.main as main
 
 
+@pytest.mark.parametrize(
+    "exchange_rate, expected_result",
+    [
+        (1.06 * 100, "Buy more cryptocurrency"),
+        (1.05 * 100, "Do nothing"),
+        (100, "Do nothing"),
+        (0.95 * 100, "Do nothing"),
+        (0.94 * 100, "Sell all your cryptocurrency"),
+    ]
+)
 @patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action(mock_exchange_rate_prediction: Any) -> None:
+def test_cryptocurrency_action(
+        mock_exchange_rate_prediction: Any,
+        exchange_rate: float,
+        expected_result: str
+) -> None:
     main.get_exchange_rate_prediction = mock_exchange_rate_prediction
 
-    mock_exchange_rate_prediction.return_value = 1.07 * 100  # 7% increase
+    mock_exchange_rate_prediction.return_value = exchange_rate
     result = main.cryptocurrency_action(100)
-    assert result == "Buy more cryptocurrency"
-
-    mock_exchange_rate_prediction.return_value = 0.92 * 100  # 8% decrease
-    result = main.cryptocurrency_action(100)
-    assert result == "Sell all your cryptocurrency"
-
-    mock_exchange_rate_prediction.return_value = 0.98 * 100  # 2% decrease
-    result = main.cryptocurrency_action(100)
-    assert result == "Do nothing"
-
-    mock_exchange_rate_prediction.return_value = 100  # Same rate
-    result = main.cryptocurrency_action(100)
-    assert result == "Do nothing"
+    assert result == expected_result
