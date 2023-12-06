d49c2c7717b7ab710b4e6980fb5406cfdafbf61b
Vlad <mr.darmstadtium@gmail.com>
2023-12-06 19:35:23 +0100

    some flake8

--- app/test_main.py
+++ app/test_main.py
@@ -5,8 +5,8 @@ from app.main import cryptocurrency_action
 
 @mock.patch("app.main.get_exchange_rate_prediction", return_value=1.07)
 def test_cryptocurrency_should_buy_more_cryptocurrency(
-        mocked_exchange) -> None:
-    assert(
+        mocked_exchange: any) -> None:
+    assert (
         cryptocurrency_action(1) == "Buy more cryptocurrency"
     )
     mocked_exchange.assert_called_once_with(1)
@@ -14,24 +14,26 @@ def test_cryptocurrency_should_buy_more_cryptocurrency(
 
 @mock.patch("app.main.get_exchange_rate_prediction", return_value=0.93)
 def test_cryptocurrency_should_sell_all_your_cryptocurrency(
-        mocked_exchange) -> None:
-    assert(
+        mocked_exchange: any) -> None:
+    assert (
         cryptocurrency_action(1) == "Sell all your cryptocurrency"
     )
     mocked_exchange.assert_called_once_with(1)
 
 
 @mock.patch("app.main.get_exchange_rate_prediction", return_value=1.05)
-def test_cryptocurrency_should_do_nothing_1(mocked_exchange) -> None:
-    assert(
+def test_cryptocurrency_should_do_nothing_1(
+        mocked_exchange: any) -> None:
+    assert (
         cryptocurrency_action(1) == "Do nothing"
     )
     mocked_exchange.assert_called_once_with(1)
 
 
 @mock.patch("app.main.get_exchange_rate_prediction", return_value=0.95)
-def test_cryptocurrency_should_do_nothing_2(mocked_exchange) -> None:
-    assert(
+def test_cryptocurrency_should_do_nothing_2(
+        mocked_exchange: any) -> None:
+    assert (
         cryptocurrency_action(1) == "Do nothing"
     )
     mocked_exchange.assert_called_once_with(1)
