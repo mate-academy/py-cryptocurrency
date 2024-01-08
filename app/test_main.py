cb11cfc4f39a544efb9a1ca6e1988b8092d3d8b6
Юрий <you@exampleabc1234567892777@gmail.com>
2024-01-08 16:30:10 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+def test_cryptocurrency_action_buy() -> None:
+    with patch(
+            "app.main.get_exchange_rate_prediction"
+    ) as mock_get_exchange_rate_prediction:
+        mock_get_exchange_rate_prediction.return_value = 1.06
+        result = cryptocurrency_action(100)
+        assert result == "Buy more cryptocurrency"
+
+
+def test_cryptocurrency_action_sell() -> None:
+    with patch(
+            "app.main.get_exchange_rate_prediction"
+    ) as mock_get_exchange_rate_prediction:
+        mock_get_exchange_rate_prediction.return_value = 0.94
+        result = cryptocurrency_action(100)
+        assert result == "Sell all your cryptocurrency"
+
+
+def test_cryptocurrency_action_do_nothing() -> None:
+    with patch(
+            "app.main.get_exchange_rate_prediction"
+    ) as mock_get_exchange_rate_prediction:
+        mock_get_exchange_rate_prediction.return_value = 1.03
+        result = cryptocurrency_action(100)
+        assert result == "Do nothing"
