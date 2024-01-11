ee1ab2176cdd94ef264304391a51cb6f667a17a0
Юрий <you@exampleabc1234567892777@gmail.com>
2024-01-11 11:33:39 +0200

    'Solution3'

--- app/test_main.py
+++ app/test_main.py
@@ -7,8 +7,7 @@ def test_cryptocurrency_action_buy() -> None:
             "app.main.get_exchange_rate_prediction"
     ) as mock_get_exchange_rate_prediction:
         mock_get_exchange_rate_prediction.return_value = 1.06
-        result = cryptocurrency_action(100)
-        assert result == "Sell all your cryptocurrency"
+        result = cryptocurrency_action(1)
         assert result == "Buy more cryptocurrency"
 
 
@@ -17,7 +16,7 @@ def test_cryptocurrency_action_sell() -> None:
             "app.main.get_exchange_rate_prediction"
     ) as mock_get_exchange_rate_prediction:
         mock_get_exchange_rate_prediction.return_value = 0.94
-        result = cryptocurrency_action(100)
+        result = cryptocurrency_action(1)
         assert result == "Sell all your cryptocurrency"
 
 
@@ -26,6 +25,23 @@ def test_cryptocurrency_action_do_nothing() -> None:
             "app.main.get_exchange_rate_prediction"
     ) as mock_get_exchange_rate_prediction:
         mock_get_exchange_rate_prediction.return_value = 1.03
-        result = cryptocurrency_action(100)
-        assert result == "Sell all your cryptocurrency"
+        result = cryptocurrency_action(1)
+        assert result == "Do nothing"
+
+
+def test_cryptocurrency_action_boundary_0_95() -> None:
+    with patch(
+            "app.main.get_exchange_rate_prediction"
+    ) as mock_get_exchange_rate_prediction:
+        mock_get_exchange_rate_prediction.return_value = 0.95
+        result = cryptocurrency_action(1)
+        assert result == "Do nothing"
+
+
+def test_cryptocurrency_action_boundary_1_05() -> None:
+    with patch(
+            "app.main.get_exchange_rate_prediction"
+    ) as mock_get_exchange_rate_prediction:
+        mock_get_exchange_rate_prediction.return_value = 1.05
+        result = cryptocurrency_action(1)
         assert result == "Do nothing"
