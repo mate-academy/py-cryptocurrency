4a7ace76c04be7cf9ed5f03ad1d44d737ee07c0a
Yevhen <evgeniy.playid@icloud.com>
2024-01-30 17:50:56 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -4,24 +4,24 @@ from app.main import cryptocurrency_action
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_action_is_buy(mock_get_exchange_rate_prediction):
+def test_action_is_buy(mock_get_exchange_rate_prediction: callable) -> None:
     mock_get_exchange_rate_prediction.return_value = 1.06
     assert cryptocurrency_action(1.0) == "Buy more cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_action_is_sell(mock_get_exchange_rate_prediction):
+def test_action_is_sell(mock_get_exchange_rate_prediction: callable) -> None:
     mock_get_exchange_rate_prediction.return_value = 0.94
     assert cryptocurrency_action(1.0) == "Sell all your cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_action_is_do_nothing_when_plus_5(mock_get_exchange_rate_prediction):
+def test_action_is_do_nothing_when_plus_5(mock_get_exchange_rate_prediction: callable) -> None:
     mock_get_exchange_rate_prediction.return_value = 1.05
     assert cryptocurrency_action(1.0) == "Do nothing"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_action_is_do_nothing_when_minus_5(mock_get_exchange_rate_prediction):
+def test_action_is_do_nothing_when_minus_5(mock_get_exchange_rate_prediction: callable) -> None:
     mock_get_exchange_rate_prediction.return_value = 0.95
     assert cryptocurrency_action(1.0) == "Do nothing"
