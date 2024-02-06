1ae11bf007f570146ce7b1e2601a63b75bd1eb41
sergey <38cerg12021979@jmal.com>
2024-02-06 18:35:34 +0200

    06_02_24

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+def test_cryptocurrency_action_buy_more() -> None:
+    get_exchange_rate_prediction = mock.MagicMock()
+    cryptocurrency_action(1.00)
+    get_exchange_rate_prediction.return_value = 1.06
+
+    assert cryptocurrency_action(1.00) == "Buy more cryptocurrency"
+
+
+def test_cryptocurrency_action_sell_all() -> None:
+    get_exchange_rate_prediction = mock.MagicMock()
+    cryptocurrency_action(1.00)
+    get_exchange_rate_prediction.return_value = 0.95
+
+    assert cryptocurrency_action(1.00) == "Sell all your cryptocurrency"
