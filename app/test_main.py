c3ab7fb8257294d6c65311356d088d0df4198eb6
mkuchuman <mkuchuman1@gmail.com>
2024-02-09 11:59:42 +0100

    Solution with problems

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.main import cryptocurrency_action
+from unittest import mock
+import pytest
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+@pytest.mark.parametrize(
+    "current_rate, predicted_rate, message",
+    [
+        (1, 1.06, "Buy more cryptocurrency"),
+        (1, 0.94, "Sell all your cryptocurrency"),
+        (1, 1.01, "Do nothing"),
+    ]
+)
+def test_cannot_access_if_only_valid_url(mocked_func: mock,
+                                         current_rate: int,
+                                         predicted_rate: int,
+                                         message: str) -> None:
+    mocked_func.return_value = predicted_rate
+    assert cryptocurrency_action(current_rate) == message
