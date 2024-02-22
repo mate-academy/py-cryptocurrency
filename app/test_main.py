01329160d786bfe9cb67152392da8caf38594822
volry <v.v.ryzhuk@gmail.com>
2024-02-22 20:57:33 +0200

    final solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,15 +6,27 @@ from unittest.mock import MagicMock
 # Test when the predicted exchange rate is more than 5% higher
 @patch("app.main.get_exchange_rate_prediction")
 def test_buy(mock_get_exchange_rate_prediction: MagicMock) -> None:
-    mock_get_exchange_rate_prediction.return_value = 106
+    mock_get_exchange_rate_prediction.return_value = 110
     assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_buy_3(mock_get_exchange_rate_prediction: MagicMock) -> None:
+    mock_get_exchange_rate_prediction.return_value = 105
+    assert cryptocurrency_action(100) == "Do nothing"
 
 
 # Test when the predicted exchange rate is more than 5% lower
 @patch("app.main.get_exchange_rate_prediction")
-def test_sell(mock_get_exchange_rate_prediction: MagicMock) -> None:
-    mock_get_exchange_rate_prediction.return_value = 94
+def test_sell_2(mock_get_exchange_rate_prediction: MagicMock) -> None:
+    mock_get_exchange_rate_prediction.return_value = 90
     assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_sell(mock_get_exchange_rate_prediction: MagicMock) -> None:
+    mock_get_exchange_rate_prediction.return_value = 95
+    assert cryptocurrency_action(100) == "Do nothing"
 
 
 # Test when the difference in predicted exchange rate is within 5%
