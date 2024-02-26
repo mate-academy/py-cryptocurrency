7e1715a42e4a212dfa943d3bbec0c2c810fce4f8
Shcherbyna Svitlana <sw.shcherbina@gmail.com>
2024-02-26 12:02:29 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,6 +2,7 @@ from unittest import mock
 
 from app.main import cryptocurrency_action
 
+
 def test_should_by_more() -> None:
     with (mock.patch("app.main.get_exchange_rate_prediction")
           as mocked_get_exchange_rate_prediction):
@@ -9,12 +10,15 @@ def test_should_by_more() -> None:
 
         assert cryptocurrency_action(51938.40) == "Buy more cryptocurrency"
 
+
 def test_should_sell() -> None:
     with (mock.patch("app.main.get_exchange_rate_prediction")
           as mocked_get_exchange_rate_prediction):
         mocked_get_exchange_rate_prediction.return_value = 41550.72
 
-        assert cryptocurrency_action(51938.40) == "Sell all your cryptocurrency"
+        assert cryptocurrency_action(51938.40) == ("Sell "
+                                                   "all your cryptocurrency")
+
 
 def test_should_do_nothing_with_rate_105() -> None:
     with (mock.patch("app.main.get_exchange_rate_prediction")
@@ -22,6 +26,7 @@ def test_should_do_nothing_with_rate_105() -> None:
         mocked_get_exchange_rate_prediction.return_value = 54535.32
 
         assert cryptocurrency_action(51938.40) == "Do nothing"
+
 
 def test_should_do_nothing_with_rate_95() -> None:
     with (mock.patch("app.main.get_exchange_rate_prediction")
