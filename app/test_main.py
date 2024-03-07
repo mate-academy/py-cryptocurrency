2aa10698f7225cf0291ded2f3c08c32c5d0b4bdb
donbas777 <renary34@gmail.com>
2024-03-07 18:28:58 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -4,34 +4,37 @@ from app.main import cryptocurrency_action
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_called_functions(
-        get_exchange_rate_prediction: mock.Mock
+        mocked_get_exchange_rate_prediction: mock.Mock
 ) -> None:
-    cryptocurrency_action(1)
-    get_exchange_rate_prediction.assert_called_once()
+    current_rate = 100
+    mocked_get_exchange_rate_prediction.return_value = current_rate
+    cryptocurrency_action(current_rate)
+    mocked_get_exchange_rate_prediction.assert_called_once_with(current_rate)
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_if_percen_is_more_than_5(
-        get_exchange_rate_prediction: mock.Mock
+        mocked_get_exchange_rate_prediction: mock.Mock
 ) -> None:
-    cryptocurrency_action(1)
-    get_exchange_rate_prediction.return_value = 1.06
-    assert cryptocurrency_action(1) == "Buy more cryptocurrency"
+    mocked_get_exchange_rate_prediction.return_value = 106
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_if_percen_is_less_than_5(
-        get_exchange_rate_prediction: mock.Mock
+        mocked_get_exchange_rate_prediction: mock.Mock
 ) -> None:
-    cryptocurrency_action(1)
-    get_exchange_rate_prediction.return_value = 0.89
-    assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
+    mocked_get_exchange_rate_prediction.return_value = 89
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_if_percen_is_neutral(
-        get_exchange_rate_prediction: mock.Mock
+        mocked_get_exchange_rate_prediction: mock.Mock
 ) -> None:
-    cryptocurrency_action(1)
-    get_exchange_rate_prediction.return_value = 0.99
-    assert cryptocurrency_action(1) == "Do nothing"
+    mocked_get_exchange_rate_prediction.return_value = 99
+    assert cryptocurrency_action(100) == "Do nothing"
+    mocked_get_exchange_rate_prediction.return_value = 105
+    assert cryptocurrency_action(100) == "Do nothing"
+    mocked_get_exchange_rate_prediction.return_value = 95
+    assert cryptocurrency_action(100) == "Do nothing"
