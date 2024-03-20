baf6068c4d54db439dbda15c02f213bb15fc41e0
Olga <olgahrytsiuk777@gmail.com>
2024-03-20 21:22:28 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,43 +1,27 @@
+import pytest
+from unittest.mock import patch
 from app.main import cryptocurrency_action
-from unittest import mock
+from unittest.mock import MagicMock
 
 
-def test_much_money() -> None:
-    with (mock.patch("app.main.get_exchange_rate_prediction")
-          as mock_get_exchange_rate_prediction):
-        mock_get_exchange_rate_prediction.return_value = 54600.48
+@patch("app.main.get_exchange_rate_prediction")
+@pytest.mark.parametrize(
+    "current_rate, exchange_rate, expected_result",
+    [
+        (100, 105.1, "Buy more cryptocurrency"),
+        (100, 94.9, "Sell all your cryptocurrency"),
+        (100, 100.0, "Do nothing"),
+        (100, 105.0, "Do nothing"),
+        (100, 95.0, "Do nothing"),
+    ]
+)
+def test_cryptocurrency_action(
+    mock_get_exchange_rate_prediction: MagicMock,
+    current_rate: int,
+    exchange_rate: float,
+    expected_result: str
+) -> None:
 
-        assert cryptocurrency_action(51938.40) == "Buy more cryptocurrency"
-
-
-def test_few_money() -> None:
-    with (mock.patch("app.main.get_exchange_rate_prediction")
-          as mock_get_exchange_rate_prediction):
-        mock_get_exchange_rate_prediction.return_value = 41550.72
-
-        assert cryptocurrency_action(51938.40) == ("Sell"
-                                                   " all your cryptocurrency")
-
-
-def test_no_change_money() -> None:
-    with (mock.patch("app.main.get_exchange_rate_prediction")
-          as mock_get_exchange_rate_prediction):
-        mock_get_exchange_rate_prediction.return_value = 54535.32
-
-        assert cryptocurrency_action(51938.40) == "Do nothing"
-
-
-def test_nothing_095() -> None:
-    with (mock.patch("app.main.get_exchange_rate_prediction")
-          as mock_get_exchange_rate_prediction):
-        mock_get_exchange_rate_prediction.return_value = 1
-
-        assert cryptocurrency_action(0.95) == "Do nothing"
-
-
-def test_nothing_2() -> None:
-    with (mock.patch("app.main.get_exchange_rate_prediction")
-          as mock_get_exchange_rate_prediction):
-        mock_get_exchange_rate_prediction.return_value = 1.9
-
-        assert cryptocurrency_action(2) == "Do nothing"
+    mock_get_exchange_rate_prediction.return_value = exchange_rate
+    result = cryptocurrency_action(current_rate)
+    assert result == expected_result
