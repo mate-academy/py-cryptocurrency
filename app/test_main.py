2d1fc837ac0ccbffeacc23efead7c18cfef2d5c5
Alexander <sasacihun@gmail.com>
2024-03-25 11:43:48 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from unittest import mock
+
+import pytest
+
+from app.main import cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    "predicted_rate,current_rate,expected_res",
+    [
+        (1.06, 1, "Buy more cryptocurrency"),
+        (0.94, 1, "Sell all your cryptocurrency"),
+        (1.05, 1, "Do nothing"),
+        (0.95, 1, "Do nothing"),
+        (1, 1, "Do nothing"),
+    ]
+)
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action(
+        mocked_get_exchange_rate_prediction,
+        predicted_rate,
+        current_rate,
+        expected_res,
+):
+    mocked_get_exchange_rate_prediction.return_value = predicted_rate
+    assert cryptocurrency_action(current_rate) == expected_res
