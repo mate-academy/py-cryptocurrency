56314b88b326b9a89e158f0d0713d69b38be668c
Misha <156539295+mgoryn@users.noreply.github.com>
2024-04-01 14:48:13 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,40 @@
 import pytest
-from app.restore_names import restore_names
+from unittest.mock import patch
+from typing import Union
+from app.main import cryptocurrency_action
 
 
 @pytest.mark.parametrize(
-    "first_name, last_name, full_name, expected_first_name", [
-        (None, "Holy", "Jack Holy", "Jack"),
-        ("", "Adams", "Mike Adams", ""),
-        ("John", "Doe", "John Doe", "John"),
-    ])
-def test_restore_names(
-        first_name: str,
-        last_name: str,
-        full_name: str,
-        expected_first_name: str) -> None:
-    users = [{"first_name": first_name,
-              "last_name": last_name,
-              "full_name": full_name
-              }]
-    restore_names(users)
-    assert users[0]["first_name"] == expected_first_name
+    "current_rate, rate_prediction, expected",
+    [
+        pytest.param(
+            100, 105,
+            "Do nothing", id="exchange rate exactly 5% higher, do nothing"
+        ),
+        pytest.param(
+            100, 95,
+            "Do nothing", id="exchange rate exactly 5% lower, do nothing"
+        ),
+        pytest.param(
+            100,
+            94,
+            "Sell all your cryptocurrency",
+            id="exchange rate more than 5% lower, sell",
+        ),
+        pytest.param(
+            100,
+            106,
+            "Buy more cryptocurrency",
+            id="exchange rate more than 5% higher, buy",
+        ),
+    ],
+)
+@patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action(
+    mock_get_exchange_rate_prediction: patch,
+    current_rate: Union[int, float],
+    rate_prediction: Union[int, float],
+    expected: str,
+) -> None:
+    mock_get_exchange_rate_prediction.return_value = rate_prediction
+    assert cryptocurrency_action(current_rate) == expected
