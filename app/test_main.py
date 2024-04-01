9a2b0b5b0667a46b911213e8c9ef37c02fbd9dd4
Misha <156539295+mgoryn@users.noreply.github.com>
2024-04-01 14:39:21 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
 import pytest
-from unittest import mock
-from app.main import cryptocurrency_action
+from app.restore_names import restore_names
 
 
 @pytest.mark.parametrize(
-    "exchange_rate, current_rate, expected_result",
-    [(82, 7, "Buy more cryptocurrency"),
-     (1, 4, "Sell all your cryptocurrency"),
-     (45, 46, "Do nothing"),
-     (85, 85.6, "Do nothing")]
-)
-@mock.patch("app.main.get_exchange_rate_prediction")
-def test_exchange_rate(
-        mock_get_exchange_rate_prediction: pytest.param,
-        exchange_rate: int | float,
-        current_rate: int | float,
-        expected_result: str
-) -> None:
-    mock_get_exchange_rate_prediction.return_value = exchange_rate
-    assert cryptocurrency_action(current_rate) == expected_result
+    "first_name, last_name, full_name, expected_first_name", [
+        (None, "Holy", "Jack Holy", "Jack"),
+        ("", "Adams", "Mike Adams", ""),
+        ("John", "Doe", "John Doe", "John"),
+    ])
+def test_restore_names(
+        first_name: str,
+        last_name: str,
+        full_name: str,
+        expected_first_name: str) -> None:
+    users = [{"first_name": first_name,
+              "last_name": last_name,
+              "full_name": full_name
+              }]
+    restore_names(users)
+    assert users[0]["first_name"] == expected_first_name
