84c5d62520b3a3359db77592cf9ffb8d6eaee3fc
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-04-04 13:41:53 +0300

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -14,6 +14,16 @@ def test_cryptocurrency_buy_more(
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_do_nothing_with_95(
+        mock_rate_prediction: callable
+) -> None:
+    current_rate = 1.00
+    mock_rate_prediction.return_value = 0.95
+    result = cryptocurrency_action(current_rate)
+    assert result == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
 def test_cryptocurrency_sell_all(
         mock_rate_prediction: callable
 ) -> None:
@@ -28,6 +38,6 @@ def test_cryptocurrency_do_nothing(
         mock_rate_prediction: callable
 ) -> None:
     current_rate = 1.00
-    mock_rate_prediction.return_value = 1.00
+    mock_rate_prediction.return_value = 1.05
     result = cryptocurrency_action(current_rate)
     assert result == "Do nothing"
