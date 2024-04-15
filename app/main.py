7548501852af6607c16df6296c690e3cefcf54ea
Pavlo <pavlo@MacBook-Pro-de-Pavlo.local>
2024-04-15 23:16:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -15,3 +15,8 @@ def cryptocurrency_action(current_rate: Union[int, float]) -> str:
     if prediction_rate / current_rate < 0.95:
         return "Sell all your cryptocurrency"
     return "Do nothing"
+
+#
+if __name__ == "__main__":
+    print(round(30 / random.random(), 2) / 30)
+#     print(cryptocurrency_action(1)
--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+import pytest
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    "current_rate, predicted_rate, expected_action",
+    [
+        (100, 106, "Buy more cryptocurrency"),
+        (100, 94, "Sell all your cryptocurrency"),
+        (100, 105, "Do nothing"),
+        (100, 96, "Do nothing"),
+        (100, 95, "Do nothing")
+    ]
+)
+@mock.patch('app.main.get_exchange_rate_prediction')
+def test_cryptocurrency_action(mock_get_rate, current_rate, predicted_rate,
+                               expected_action) -> None:
+    mock_get_rate.return_value = predicted_rate
+    result = cryptocurrency_action(current_rate)
+    assert result == expected_action
