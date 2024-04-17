b25342448c2ed4e39ce4303b083eb385e4d5606c
Maksym Hekalo <maximgekalo@gmail.com>
2024-04-17 22:00:13 +0300

    test test_cryptocurrency_action_logic

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+from unittest import mock
+import pytest
+from app.main import cryptocurrency_action
+from typing import Union
+
+
+@pytest.mark.parametrize(
+    "current_rate, prediction_rate, expected_status",
+    [
+        pytest.param(100, 110, "Buy more cryptocurrency"),
+        pytest.param(120, 110, "Sell all your cryptocurrency"),
+        pytest.param(100, 105, "Do nothing"),
+        pytest.param(105, 100, "Do nothing"),
+        pytest.param(100, 95, "Do nothing"),
+
+    ]
+)
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_logic(
+        mocked_rate_prediction: object,
+        current_rate: Union[int, float],
+        prediction_rate: Union[int, float],
+        expected_status: str,
+
+) -> None:
+    mocked_rate_prediction.return_value = prediction_rate
+    assert cryptocurrency_action(current_rate) == expected_status
