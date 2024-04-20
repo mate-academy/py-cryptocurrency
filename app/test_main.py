98d1f224471af19da172ba85c3ec23df29a2f360
Volkov <volkovyegorandriiovich@gmail.com>
2024-04-20 19:26:23 +0300

    test v1.1

--- app/test_main.py
+++ app/test_main.py
@@ -6,37 +6,32 @@ from app.main import cryptocurrency_action
 
 
 @pytest.mark.parametrize(
-    "c_rate,func,answer",
+    "func,answer",
     [
         pytest.param(
-            1,
             1.06,
             "Buy more cryptocurrency",
             id="should buy"
         ),
         pytest.param(
-            1,
             0.94,
             "Sell all your cryptocurrency",
             id="should sell",
         ),
         pytest.param(
-            1,
             0.95,
             "Do nothing",
             id="should hold"
         ),
         pytest.param(
-            1,
             1.05,
             "Do nothing",
             id="should hold"
         )
     ]
 )
-def test(c_rate,
-         func,
-         answer):
+def test(func: int,
+         answer: str) -> None:
     with mock.patch("app.main.get_exchange_rate_prediction",
                     return_value=func):
-        assert cryptocurrency_action(c_rate) == answer
+        assert cryptocurrency_action(1) == answer
