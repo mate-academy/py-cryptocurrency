8d596438788681ef9fb079b8f0730fa42e1aef94
AndrewwSS <clash2clans1one@gmail.com>
2024-05-28 15:14:03 +0300

    fix flake8

--- app/test_main.py
+++ app/test_main.py
@@ -2,22 +2,27 @@ from unittest.mock import patch
 from app.main import cryptocurrency_action
 
 
-@patch('app.main.get_exchange_rate_prediction')
-def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction):
+@patch("app.main.get_exchange_rate_prediction")
+def test_buy_more_cryptocurrency(
+        mock_get_exchange_rate_prediction: object
+) -> None:
     current_rate = 100
     mock_get_exchange_rate_prediction.return_value = 106  # More than 5% higher
     assert cryptocurrency_action(current_rate) == "Buy more cryptocurrency"
 
 
-@patch('app.main.get_exchange_rate_prediction')
-def test_sell_all_cryptocurrency(mock_get_exchange_rate_prediction):
+@patch("app.main.get_exchange_rate_prediction")
+def test_sell_all_cryptocurrency(
+        mock_get_exchange_rate_prediction: object
+) -> None:
     current_rate = 100
     mock_get_exchange_rate_prediction.return_value = 94  # More than 5% lower
-    assert cryptocurrency_action(current_rate) == "Sell all your cryptocurrency"
+    assert (cryptocurrency_action(current_rate)
+            == "Sell all your cryptocurrency")
 
 
-@patch('app.main.get_exchange_rate_prediction')
-def test_do_nothing(mock_get_exchange_rate_prediction):
+@patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing(mock_get_exchange_rate_prediction: object) -> None:
     current_rate = 100
     mock_get_exchange_rate_prediction.return_value = 102  # Within 5%
     assert cryptocurrency_action(current_rate) == "Do nothing"
@@ -30,4 +35,3 @@ def test_do_nothing(mock_get_exchange_rate_prediction):
 
     mock_get_exchange_rate_prediction.return_value = 95  # Exactly 5% lower
     assert cryptocurrency_action(current_rate) == "Do nothing"
-
