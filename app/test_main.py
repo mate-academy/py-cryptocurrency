dd56f10eda18b047bf69644916263742214ac731
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-06-03 17:28:51 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,46 @@
-# write your code here
+import unittest
+from unittest.mock import patch
+
+from app.main import cryptocurrency_action
+
+
+class TestCryptocurrencyAction(unittest.TestCase):
+    @patch("app.main.get_exchange_rate_prediction")
+    def test_buy_more_cryptocurrency(
+            self,
+            mock_get_exchange_rate_prediction: float
+    ) -> None:
+
+        mock_get_exchange_rate_prediction.return_value = 1.05
+        current_rate = 1.0
+        result = cryptocurrency_action(current_rate)
+
+        self.assertEqual(result, "Buy more cryptocurrency")
+
+    @patch("app.main.get_exchange_rate_prediction")
+    def test_sell_all_cryptocurrency(
+            self,
+            mock_get_exchange_rate_prediction: float
+    ) -> None:
+
+        mock_get_exchange_rate_prediction.return_value = 0.95
+        current_rate = 1.0
+        result = cryptocurrency_action(current_rate)
+
+        self.assertEqual(result, "Sell all your cryptocurrency")
+
+    @patch("app.main.get_exchange_rate_prediction")
+    def test_do_nothing(
+            self,
+            mock_get_exchange_rate_prediction: float
+    ) -> None:
+
+        mock_get_exchange_rate_prediction.return_value = 1.0
+        current_rate = 1.0
+        result = cryptocurrency_action(current_rate)
+
+        self.assertEqual(result, "Do nothing")
+
+
+if __name__ == "__main__":
+    unittest.main()
