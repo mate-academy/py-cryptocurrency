9ca98849dfe44a2e21f536a6a411f1772ae5bc66
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-06-03 18:41:15 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,41 +1,31 @@
 import unittest
 from unittest.mock import patch
-
 from app.main import cryptocurrency_action
 
 
 class TestCryptocurrencyAction(unittest.TestCase):
     @patch("app.main.get_exchange_rate_prediction")
-    def test_buy_more_cryptocurrency(
-            self,
-            mock_get_exchange_rate_prediction: float
-    ) -> None:
+    def test_buy_more_cryptocurrency(self, mock_get_exchange_rate_prediction):
+        mock_get_exchange_rate_prediction.return_value = 1.06
 
-        mock_get_exchange_rate_prediction.return_value = 1.05
         current_rate = 1.0
         result = cryptocurrency_action(current_rate)
 
         self.assertEqual(result, "Buy more cryptocurrency")
 
     @patch("app.main.get_exchange_rate_prediction")
-    def test_sell_all_cryptocurrency(
-            self,
-            mock_get_exchange_rate_prediction: float
-    ) -> None:
+    def test_sell_all_cryptocurrency(self, mock_get_exchange_rate_prediction):
+        mock_get_exchange_rate_prediction.return_value = 0.94
 
-        mock_get_exchange_rate_prediction.return_value = 0.95
         current_rate = 1.0
         result = cryptocurrency_action(current_rate)
 
         self.assertEqual(result, "Sell all your cryptocurrency")
 
     @patch("app.main.get_exchange_rate_prediction")
-    def test_do_nothing(
-            self,
-            mock_get_exchange_rate_prediction: float
-    ) -> None:
+    def test_do_nothing(self, mock_get_exchange_rate_prediction):
+        mock_get_exchange_rate_prediction.return_value = 1.02
 
-        mock_get_exchange_rate_prediction.return_value = 1.0
         current_rate = 1.0
         result = cryptocurrency_action(current_rate)
 
