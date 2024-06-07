ca631b88b59b2c4dcca4a9b609bcbbe7fe1ac012
jpdreamthug <lentorilp@gmail.com>
2024-06-07 16:27:44 +0300

    fix soltion

--- app/test_main.py
+++ app/test_main.py
@@ -7,27 +7,19 @@ from app.main import cryptocurrency_action
 @pytest.mark.parametrize(
     "current_rate,prediction_rate,expected",
     [
-        pytest.param(10, 15.0, "Buy more cryptocurrency"),
-        pytest.param(15.5, 10, "Sell all your cryptocurrency"),
-        pytest.param(15, 15.1, "Do nothing"),
-        pytest.param(0, 15, "Do nothing")
-    ],
-    ids=[
-        "Predict higher, buy",
-        "Predict lower, sell",
-        "Predict similar, hold",
-        "Current rate zero"
+        pytest.param(10, 100, "Buy more cryptocurrency"),
+        pytest.param(100, 50, "Sell all your cryptocurrency"),
+        pytest.param(100, 99, "Do nothing"),
+        pytest.param(100, 101, "Do nothing")
     ]
+
 )
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_cryptocurrency_action(
         mocked_rate: mock.Mock,
         current_rate: int | float,
         prediction_rate: int | float,
-        expected: str) -> None:
-    if current_rate == 0:
-        with pytest.raises(ZeroDivisionError):
-            cryptocurrency_action(current_rate)
-    else:
-        mocked_rate.return_value = prediction_rate
-        assert cryptocurrency_action(current_rate) == expected
+        expected: str
+) -> None:
+    mocked_rate.return_value = prediction_rate
+    assert cryptocurrency_action(current_rate) == expected
