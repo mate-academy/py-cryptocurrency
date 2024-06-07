d113356567f9470703660745699de468bdc67cd1
lilarin <80650911+lilarin@users.noreply.github.com>
2024-06-07 13:55:17 +0300

    Solution refinement

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,19 @@
 from __future__ import annotations
-from app.main import cryptocurrency_action
 from unittest import mock
 import pytest
+from app.main import cryptocurrency_action
 
 
 @pytest.mark.parametrize(
-    "prediction_rate,current_rate,expected_result",
+    "current_rate,prediction_rate,expected_result",
     [
         pytest.param(
-            3.05, 2, "Buy more cryptocurrency",
-            id="Rate rise"
+            3.05, 2, "Sell all your cryptocurrency",
+            id="Rate fall"
         ),
         pytest.param(
-            1.25, 5, "Sell all your cryptocurrency",
-            id="Rate fall"
+            1.25, 5, "Buy more cryptocurrency",
+            id="Rate rise"
         ),
         pytest.param(
             49.99, 52, "Do nothing",
@@ -22,15 +22,14 @@ import pytest
         pytest.param(
             52, 49.99, "Do nothing",
             id="Close values"
-        )
+        ),
     ]
 )
 @mock.patch("app.main.get_exchange_rate_prediction")
-@mock.patch("app.main.cryptocurrency_action")
 def test_cryptocurrency_action(
-        mocked_rate_prediction: mock.MagicMock,
-        prediction_rate: int | float,
+        mocked_rate_prediction: callable,
         current_rate: int | float,
+        prediction_rate: int | float,
         expected_result: str
 ) -> None:
     mocked_rate_prediction.return_value = prediction_rate
