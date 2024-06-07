755c8363672916115fa59a9d9185c97af666675f
lilarin <80650911+lilarin@users.noreply.github.com>
2024-06-07 14:03:35 +0300

    Solution refinement (try to copy solution bc mine crashes on github)

--- app/test_main.py
+++ app/test_main.py
@@ -1,24 +1,22 @@
 from __future__ import annotations
 from unittest import mock
+import app.main
 import pytest
-from app.main import cryptocurrency_action
 
 
 @pytest.mark.parametrize(
-    "current_rate,rate_prediction,expected_action",
-    [
-        (1, 2.2, "Buy more cryptocurrency"),
-        (2.2, 1, "Sell all your cryptocurrency"),
-        (2.2, 2.1, "Do nothing"),
-        (2.1, 2, "Do nothing"),
+    "rate, result", [
+        (0.5, "Sell all your cryptocurrency"),
+        (1.5, "Buy more cryptocurrency"),
+        (0.95, "Do nothing"),
+        (1.05, "Do nothing"),
     ]
 )
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_cryptocurrency_action(
-        mocked_exchange_rate_prediction: mock.MagicMock,
-        current_rate: int | float,
-        rate_prediction: int | float,
-        expected_action: str
+        mocked_rate_prediction: mock.MagicMock,
+        rate: int | float,
+        result: str
 ) -> None:
-    mocked_exchange_rate_prediction.return_value = rate_prediction
-    assert cryptocurrency_action(current_rate) == expected_action
+    mocked_rate_prediction.return_value = rate
+    assert app.main.cryptocurrency_action(1) == result
