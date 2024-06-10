c608727639780ff49678cda0e637426716c09df2
Ivan <ivan.seeker6@gmail.com>
2024-06-10 17:14:00 +0300

    sol 1

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,46 @@
 import pytest
-from unittest.mock import patch
 from app.main import cryptocurrency_action
+from typing import Callable
 
 
-def test_buy_more_cryptocurrency() -> None:
-    with patch("app.main.get_exchange_rate_prediction", return_value=105.1):
-        assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+def mock_get_exchange_rate_prediction(rate: float) -> float:
+    return rate
 
 
-def test_sell_all_cryptocurrency() -> None:
-    with patch("app.main.get_exchange_rate_prediction", return_value=94.9):
-        assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+def test_rate_95_percent_do_nothing(monkeypatch: Callable) -> None:
+    def mock_get_exchange_rate_prediction(rate: float) -> float:
+        return rate * 0.95
+
+    monkeypatch.setattr("app.main.get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction)
+    assert cryptocurrency_action(100) == "Do nothing"
 
 
-def test_do_nothing() -> None:
-    with patch("app.main.get_exchange_rate_prediction", return_value=100):
-        assert cryptocurrency_action(100) == "Do nothing"
-    with patch("app.main.get_exchange_rate_prediction", return_value=104.9):
-        assert cryptocurrency_action(100) == "Do nothing"
-    with patch("app.main.get_exchange_rate_prediction", return_value=95.1):
-        assert cryptocurrency_action(100) == "Do nothing"
+def test_rate_105_percent_do_nothing(monkeypatch: Callable) -> None:
+    def mock_get_exchange_rate_prediction(rate: float) -> float:
+        return rate * 1.05
+
+    monkeypatch.setattr("app.main.get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction)
+    assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_rate_more_than_105_percent_buy(monkeypatch: Callable) -> None:
+    def mock_get_exchange_rate_prediction(rate: float) -> float:
+        return rate * 1.06
+
+    monkeypatch.setattr("app.main.get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction)
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+def test_rate_less_than_95_percent_sell(monkeypatch: Callable) -> None:
+    def mock_get_exchange_rate_prediction(rate: float) -> float:
+        return rate * 0.94
+
+    monkeypatch.setattr("app.main.get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction)
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
 
 
 if __name__ == "__main__":
