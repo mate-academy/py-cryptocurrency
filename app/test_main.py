bffaa6410c5788e2794dd2a1ab78a1f11de6dfb5
Ivan <ivan.seeker6@gmail.com>
2024-06-10 17:02:50 +0300

    sol 1

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+import pytest
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+def test_buy_more_cryptocurrency() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=105.1):
+        assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+def test_sell_all_cryptocurrency() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=94.9):
+        assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+def test_do_nothing() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=100):
+        assert cryptocurrency_action(100) == "Do nothing"
+    with patch("app.main.get_exchange_rate_prediction", return_value=104.9):
+        assert cryptocurrency_action(100) == "Do nothing"
+    with patch("app.main.get_exchange_rate_prediction", return_value=95.1):
+        assert cryptocurrency_action(100) == "Do nothing"
+
+
+if __name__ == "__main__":
+    pytest
