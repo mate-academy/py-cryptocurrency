ad7cabc6c748ebf607545ffaff021d6d2d69a3f9
Serveladik <vladyslav.sabadakh@litslink.com>
2024-06-24 20:46:18 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,35 @@
-# write your code here
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction) -> None:
+    mock_get_exchange_rate_prediction.return_value = 1.06
+    assert cryptocurrency_action(1.0) == "Buy more cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_sell_all_cryptocurrency(mock_get_exchange_rate_prediction) -> None:
+    mock_get_exchange_rate_prediction.return_value = 0.94
+    assert cryptocurrency_action(1.0) == "Sell all your cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing(mock_get_exchange_rate_prediction) -> None:
+    mock_get_exchange_rate_prediction.return_value = 0.95
+    assert cryptocurrency_action(1.0) == "Do nothing", \
+        ("You should not sell cryptocurrency when prediction_rate "
+         "/ current_rate == 0.95")
+
+    mock_get_exchange_rate_prediction.return_value = 1.05
+    assert cryptocurrency_action(1.0) == "Do nothing", \
+        ("You should not buy cryptocurrency when prediction_rate "
+         "/ current_rate == 1.05")
+
+    mock_get_exchange_rate_prediction.return_value = 0.951
+    assert cryptocurrency_action(1.0) == "Do nothing", \
+        "You should not sell cryptocurrency within ±5% range"
+
+    mock_get_exchange_rate_prediction.return_value = 1.049
+    assert cryptocurrency_action(1.0) == "Do nothing", \
+        "You should not buy cryptocurrency within ±5% range"
