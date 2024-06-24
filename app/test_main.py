301794f27d2a958467837c5b2d7d7c25b77b3468
Serveladik <vladyslav.sabadakh@litslink.com>
2024-06-24 20:54:15 +0300

    Solution3

--- app/test_main.py
+++ app/test_main.py
@@ -3,13 +3,15 @@ from unittest.mock import patch, Mock
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction: Mock) -> None:
+def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction: Mock)\
+        -> None:
     mock_get_exchange_rate_prediction.return_value = 1.06
     assert cryptocurrency_action(1.0) == "Buy more cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_sell_all_cryptocurrency(mock_get_exchange_rate_prediction: Mock) -> None:
+def test_sell_all_cryptocurrency(mock_get_exchange_rate_prediction: Mock)\
+        -> None:
     mock_get_exchange_rate_prediction.return_value = 0.94
     assert cryptocurrency_action(1.0) == "Sell all your cryptocurrency"
 
