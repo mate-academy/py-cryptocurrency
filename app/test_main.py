024d58b468bdb3f99d0b07231f4e2e7c0bda438a
Serveladik <vladyslav.sabadakh@litslink.com>
2024-06-24 20:48:52 +0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
-from unittest.mock import patch
 from app.main import cryptocurrency_action
+from unittest.mock import patch, Mock
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction) -> None:
+def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction: Mock) -> None:
     mock_get_exchange_rate_prediction.return_value = 1.06
     assert cryptocurrency_action(1.0) == "Buy more cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_sell_all_cryptocurrency(mock_get_exchange_rate_prediction) -> None:
+def test_sell_all_cryptocurrency(mock_get_exchange_rate_prediction: Mock) -> None:
     mock_get_exchange_rate_prediction.return_value = 0.94
     assert cryptocurrency_action(1.0) == "Sell all your cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_do_nothing(mock_get_exchange_rate_prediction) -> None:
+def test_do_nothing(mock_get_exchange_rate_prediction: Mock) -> None:
     mock_get_exchange_rate_prediction.return_value = 0.95
     assert cryptocurrency_action(1.0) == "Do nothing", \
         ("You should not sell cryptocurrency when prediction_rate "
