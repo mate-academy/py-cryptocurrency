23a412c55b796b848d085b0d196657086e561e19
MRGL <Sonemongoo@gmail.com>
2024-06-27 02:22:04 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -5,19 +5,21 @@ from typing import Any
 
 
 @pytest.mark.parametrize(
-    "possible_rate, expected_result",
+    "prediction_rate,current_rate,expected_result",
     [
-        (120, "Buy more cryptocurrency"),
-        (80, "Sell all your cryptocurrency"),
-        (103, "Do nothing"),
-        (97, "Do nothing")
+        (11, 10, "Buy more cryptocurrency"),
+        (9, 10, "Sell all your cryptocurrency"),
+        (10, 10, "Do nothing"),
+        (9.5, 10, "Do nothing"),
+        (10.5, 10, "Do nothing")
     ]
 )
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_cryptocurrency_action(
-        get_exchange_rate_prediction: Any,
-        possible_rate: int,
-        expected_result: str
+    get_exchange_rate_prediction: Any,
+    prediction_rate: int | float,
+    current_rate: int | float,
+    expected_result: str,
 ) -> None:
-    get_exchange_rate_prediction.return_value = possible_rate
-    assert cryptocurrency_action(100) == expected_result
+    get_exchange_rate_prediction.return_value = prediction_rate
+    assert cryptocurrency_action(current_rate) == expected_result
