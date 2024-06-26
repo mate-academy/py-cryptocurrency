e910628c5f8951f5723e5d8334459ad1e29dccf4
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-26 17:19:42 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,23 +1,25 @@
-import pytest
 from app.main import cryptocurrency_action
+from unittest import mock
+import pytest
+from typing import Any
 
 
-# Mock function
-def mock_get_exchange_rate_prediction(exchange_rate):
-    if exchange_rate == 100:
-        return 110
-    if exchange_rate == 200:
-        return 180
-    if exchange_rate == 300:
-        return 310
-    return exchange_rate
-
-
-@pytest.mark.parametrize("current_rate, expected_action", [
-    (100, "Buy more cryptocurrency"),
-    (200, "Sell all your cryptocurrency"),
-    (300, "Do nothing"),
-])
-def test_cryptocurrency_action(monkeypatch, current_rate, expected_action):
-    monkeypatch.setattr('app.main.get_exchange_rate_prediction', mock_get_exchange_rate_prediction)
-    assert cryptocurrency_action(current_rate) == expected_action
+@pytest.mark.parametrize(
+    "prediction_rate,current_rate,expected_result",
+    [
+        (11, 10, "Buy more cryptocurrency"),
+        (9, 10, "Sell all your cryptocurrency"),
+        (10, 10, "Do nothing"),
+        (9.5, 10, "Do nothing"),
+        (10.5, 10, "Do nothing")
+    ]
+)
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action(
+    get_exchange_rate_prediction: Any,
+    prediction_rate: int | float,
+    current_rate: int | float,
+    expected_result: str,
+) -> None:
+    get_exchange_rate_prediction.return_value = prediction_rate
+    assert cryptocurrency_action(current_rate) == expected_result
