8b41c956055b9b08e79e3ca634108fd6e22f9c80
Danylo Minkov <minkov215@gmail.com>
2024-06-27 19:21:44 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -4,21 +4,31 @@ from app.main import cryptocurrency_action
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_buy_more_cryptocurrency(
-        mock_get_exchange_rate_prediction: None) -> None:
+        mock_get_exchange_rate_prediction: mock.Mock) -> None:
     mock_get_exchange_rate_prediction.return_value = 106
     assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_sell_all_cryptocurrency(
-        mock_get_exchange_rate_prediction: None) -> None:
+        mock_get_exchange_rate_prediction: mock.Mock) -> None:
     mock_get_exchange_rate_prediction.return_value = 94
     assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_do_nothing(mock_get_exchange_rate_prediction: None) -> None:
+def test_do_nothing(
+        mock_get_exchange_rate_prediction: mock.Mock) -> None:
     mock_get_exchange_rate_prediction.return_value = 104
     assert cryptocurrency_action(100) == "Do nothing"
     mock_get_exchange_rate_prediction.return_value = 96
+    assert cryptocurrency_action(100) == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing_on_threshold(
+        mock_get_exchange_rate_prediction: mock.Mock) -> None:
+    mock_get_exchange_rate_prediction.return_value = 105
+    assert cryptocurrency_action(100) == "Do nothing"
+    mock_get_exchange_rate_prediction.return_value = 95
     assert cryptocurrency_action(100) == "Do nothing"
