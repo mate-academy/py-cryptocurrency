c44622c28880f73ef5d6954a5248be9a9c57bd56
Ppllupold <kotnazar2005@gmail.com>
2024-06-30 17:45:03 +0300

    dfgh

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+def test_crypto_currency_good_prediction() -> None:
+    with mock.patch("app.main.get_exchange_ra"
+                    "te_prediction", return_value=5):
+        assert cryptocurrency_action(2) == "Buy more cryptocurrency"
+
+
+def test_crypto_currency_bad_prediction() -> None:
+    with mock.patch("app.main.get_exchange_"
+                    "rate_prediction", return_value=5):
+        assert cryptocurrency_action(6) == "Sell all your cryptocurrency"
+
+
+def test_crypto_currency_do_nothing() -> None:
+    with mock.patch("app.main.get_exchange"
+                    "_rate_prediction", return_value=5):
+        assert cryptocurrency_action(5) == "Do nothing"
