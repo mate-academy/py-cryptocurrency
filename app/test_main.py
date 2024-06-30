48213d1c97de44023ef79184811af930e04f1078
Ppllupold <kotnazar2005@gmail.com>
2024-06-30 21:30:06 +0300

    dfgh

--- app/test_main.py
+++ app/test_main.py
@@ -1,30 +1,30 @@
-import unittest
+from decimal import Decimal
+
+import pytest
 from unittest import mock
-from parameterized import parameterized
-import app.main
+from app.main import cryptocurrency_action, get_exchange_rate_prediction
 
 
-class TestCryptocurrencyAction(unittest.TestCase):
-
-    @parameterized.expand([
-        (100, 107, "Buy more cryptocurrency"),
-        (100, 94, "Sell all your cryptocurrency"),
-        (100, 100, "Do nothing"),
-        (100, 104.99, "Do nothing"),
-        (100, 95.01, "Do nothing"),
-        (3, 3.15, "Do nothing"),
-        (1, 0.95, "Do nothing")
-    ])
-    @mock.patch("app.main.get_exchange_rate_prediction")
-    def test_cryptocurrency_action(self, current_rate: float,
-                                   predicted_rate: None, expected_action: None,
-                                   mock_get_exchange_rate_prediction:
-                                   None) -> None:
-        mock_get_exchange_rate_prediction.return_value = predicted_rate
-
-        result = app.main.cryptocurrency_action(current_rate)
-        self.assertEqual(result, expected_action)
+@pytest.mark.parametrize(
+    "predicted, current, expected",
+    [
+        (5, 2, "Buy more cryptocurrency"),
+        (1, 2, "Sell all your cryptocurrency"),
+        (1, 1, "Do nothing"),
+        (1.05, 1, "Do nothing"),
+        (3.15, 3, "Do nothing"),
+    ]
+)
+def test_cryptocurrency_action_works_ok(predicted: int, current: int, expected: str):
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=predicted):
+        assert cryptocurrency_action(current) == expected
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_rate_95_percent_do_nothing():
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=0.95):
+        assert cryptocurrency_action(1) == "Do nothing"
+
+
+def test_rate_105_percent_do_nothing():
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=1.05):
+        assert cryptocurrency_action(1) == "Do nothing"
