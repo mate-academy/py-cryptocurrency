2774bbf46556d4c6fca3649617fbc402723f3668
DSahalatyi <dsahalatyi@gmail.com>
2024-07-31 20:09:44 +0300

    fix linter

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from unittest import mock
 
 import pytest
@@ -6,7 +7,7 @@ from app.main import cryptocurrency_action
 
 
 @pytest.fixture
-def mock_rate_prediction():
+def mock_rate_prediction() -> Callable:
     with mock.patch("app.main.get_exchange_rate_prediction") as mocked_rate:
         yield mocked_rate
 
@@ -17,16 +18,15 @@ def mock_rate_prediction():
         (2, 1, "Buy more cryptocurrency"),
         (21, 20, "Do nothing"),
         (1, 2, "Sell all your cryptocurrency"),
-        (19, 20, "Do nothing"),
-        (1, 1, "Do nothing")
+        (19, 20, "Do nothing")
     ]
 )
 def test_return_correct_prediction(
-        mock_rate_prediction,
-        rate_prediction,
-        current_rate,
-        expected_result
-):
+        mock_rate_prediction: Callable,
+        rate_prediction: int | float,
+        current_rate: int | float,
+        expected_result: str
+) -> None:
     mocked_rate = mock_rate_prediction
     mocked_rate.return_value = rate_prediction
 
