9c1f88d6b086430c7a7c613579f4a2a76763fd09
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-08-10 00:01:13 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -10,7 +10,7 @@ def test_advice_sell(
         mocked_get_exchange_rate_prediction: float,
         mocked_random_choice: str) -> None:
     mocked_random_choice.return_value = "decrease"
-    mocked_get_exchange_rate_prediction.return_value = 3
+    mocked_get_exchange_rate_prediction.return_value = 0.9 * 5
     assert cryptocurrency_action(5) == "Sell all your cryptocurrency"
 
 
@@ -21,8 +21,18 @@ def test_advice_do_nothing(
         mocked_random_choice: str
 ) -> None:
     mocked_random_choice.return_value = "increase"
-    mocked_get_exchange_rate_prediction.return_value = 89.5
-    assert cryptocurrency_action(85.5) == "Do nothing"
+    mocked_get_exchange_rate_prediction.return_value = 1.05 * 85
+    assert cryptocurrency_action(85) == "Do nothing"
+
+@mock.patch("app.main.random.choice")
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing(
+        mocked_get_exchange_rate_prediction: float,
+        mocked_random_choice: str
+) -> None:
+    mocked_random_choice.return_value = "increase"
+    mocked_get_exchange_rate_prediction.return_value = 0.95 * 85
+    assert cryptocurrency_action(85) == "Do nothing"
 
 
 @mock.patch("app.main.random.choice")
@@ -32,5 +42,5 @@ def test_advice_buy(
         mocked_random_choice: str
 ) -> None:
     mocked_random_choice.return_value = "increase"
-    mocked_get_exchange_rate_prediction.return_value = 100.65
+    mocked_get_exchange_rate_prediction.return_value = 1.1 * 85
     assert cryptocurrency_action(85) == "Buy more cryptocurrency"
