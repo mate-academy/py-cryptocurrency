9e85c7d23be3b93a1419e9d9f4c1795dd0d2249c
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-08-09 17:18:51 +0300

    solving3

--- app/test_main.py
+++ app/test_main.py
@@ -4,31 +4,39 @@ from app.main import cryptocurrency_action
 
 @patch("app.main.get_exchange_rate_prediction")
 def test_buy_more_crypto(
-        mock_get_rate: MagicMock
+        mock_get_exchange_rate_prediction: MagicMock
 ) -> None:
-    mock_get_rate.return_value = 105.01
+    mock_get_exchange_rate_prediction.return_value = 105.1
     assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_sell_crypto(
-        mock_get_rate: MagicMock
+def test_sell_all_crypto(
+        mock_get_exchange_rate_prediction: MagicMock
 ) -> None:
-    mock_get_rate.return_value = 94.99
+    mock_get_exchange_rate_prediction.return_value = 94.9
     assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_do_nothing_low(
-mock_get_rate: MagicMock
+def test_do_nothing(
+        mock_get_exchange_rate_prediction: MagicMock
 ) -> None:
-    mock_get_rate.return_value = 95.0
+    mock_get_exchange_rate_prediction.return_value = 100.0
     assert cryptocurrency_action(100) == "Do nothing"
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_do_nothing_high(
-mock_get_rate: MagicMock
+def test_rate_95_percent(
+        mock_get_exchange_rate_prediction: MagicMock
 ) -> None:
-    mock_get_rate.return_value = 105.0
+    mock_get_exchange_rate_prediction.return_value = 95.0
+    assert cryptocurrency_action(100) == "Do nothing"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_rate_105_percent(
+        mock_get_exchange_rate_prediction: MagicMock
+) -> None:
+    mock_get_exchange_rate_prediction.return_value = 105.0
     assert cryptocurrency_action(100) == "Do nothing"
