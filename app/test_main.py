eb4614926227a84288b0611eec4f18efc6abe8b7
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-09 20:19:53 +0300

    first

--- app/test_main.py
+++ app/test_main.py
@@ -5,19 +5,22 @@ from app.main import cryptocurrency_action
 def test_buy_more_cryptocurrency() -> None:
     current_rate = 100
     prediction_rate = 106
-    with mock.patch('app.main.get_exchange_rate_prediction', return_value=prediction_rate):
+    with mock.patch("app.main.get_exchange_rate_prediction",
+                    return_value=prediction_rate):
         assert cryptocurrency_action(current_rate) == "Buy more cryptocurrency"
 
 
 def test_sell_all_cryptocurrency() -> None:
     current_rate = 100
     prediction_rate = 94
-    with mock.patch('app.main.get_exchange_rate_prediction', return_value=prediction_rate):
+    with mock.patch("app.main.get_exchange_rate_prediction",
+                    return_value=prediction_rate):
         assert cryptocurrency_action(current_rate) == prediction_rate
 
 
 def test_do_nothing() -> None:
     current_rate = 100
     prediction_rate = 96
-    with mock.patch('app.main.get_exchange_rate_prediction', return_value=prediction_rate):
+    with mock.patch("app.main.get_exchange_rate_prediction",
+                    return_value=prediction_rate):
         assert cryptocurrency_action(current_rate) == prediction_rate
