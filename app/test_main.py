5e7a13d759958b04acd6c5a31722d7d74e7e911e
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-08-09 23:45:26 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,36 @@
-# write your code here
+from unittest import mock
+
+
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.random.choice")
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_advice_sell(
+        mocked_get_exchange_rate_prediction: float,
+        mocked_random_choice: str) -> None:
+    mocked_random_choice.return_value = "decrease"
+    mocked_get_exchange_rate_prediction.return_value = 3
+    assert cryptocurrency_action(5) == "Sell all your cryptocurrency"
+
+
+@mock.patch("app.main.random.choice")
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_advice_do_nothing(
+        mocked_get_exchange_rate_prediction: float,
+        mocked_random_choice: str
+) -> None:
+    mocked_random_choice.return_value = "increase"
+    mocked_get_exchange_rate_prediction.return_value = 89.5
+    assert cryptocurrency_action(85.5) == "Do nothing"
+
+
+@mock.patch("app.main.random.choice")
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_advice_buy(
+        mocked_get_exchange_rate_prediction: float,
+        mocked_random_choice: str
+) -> None:
+    mocked_random_choice.return_value = "increase"
+    mocked_get_exchange_rate_prediction.return_value = 100.65
+    assert cryptocurrency_action(85) == "Buy more cryptocurrency"
