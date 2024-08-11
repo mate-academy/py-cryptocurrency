edf3879b710dfc4ad79eab1f31d3021000f1b0ef
Maxim Turenko <turenkomaksim099@gmail.com>
2024-08-11 09:53:39 +0300

    implement(fixed)

--- app/test_main.py
+++ app/test_main.py
@@ -2,23 +2,23 @@ from unittest.mock import patch
 from app.main import cryptocurrency_action
 
 
-@patch("main.get_exchange_rate_prediction")
+@patch("app.main.get_exchange_rate_prediction")
 def test_buy_more_cryptocurrency(
         mock_get_exchange_rate_prediction: int
 ) -> None:
-    mock_get_exchange_rate_prediction.return_value = 110
+    mock_get_exchange_rate_prediction.return_value = 105.1
     assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
-@patch("main.get_exchange_rate_prediction")
+@patch("app.main.get_exchange_rate_prediction")
 def test_sell_all_cryptocurrency(
         mock_get_exchange_rate_prediction: int
 ) -> None:
-    mock_get_exchange_rate_prediction.return_value = 90
+    mock_get_exchange_rate_prediction.return_value = 94.9
     assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
 
 
-@patch("main.get_exchange_rate_prediction")
+@patch("app.main.get_exchange_rate_prediction")
 def test_do_nothing(
         mock_get_exchange_rate_prediction: int
 ) -> None:
@@ -26,12 +26,12 @@ def test_do_nothing(
     assert cryptocurrency_action(100) == "Do nothing"
 
 
-@patch("main.get_exchange_rate_prediction")
+@patch("app.main.get_exchange_rate_prediction")
 def test_do_nothing_close_to_threshold(
         mock_get_exchange_rate_prediction: int
 ) -> None:
-    mock_get_exchange_rate_prediction.return_value = 104.9
+    mock_get_exchange_rate_prediction.return_value = 105.0
     assert cryptocurrency_action(100) == "Do nothing"
 
-    mock_get_exchange_rate_prediction.return_value = 95.1
+    mock_get_exchange_rate_prediction.return_value = 95.0
     assert cryptocurrency_action(100) == "Do nothing"
