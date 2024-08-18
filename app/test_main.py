5220d7ae8d76619330ead08775f9e654c70208b1
FalseCoreCentury <simonov0101k@gmail.com>
2024-08-18 21:59:31 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,17 +4,17 @@ from app.main import cryptocurrency_action
 
 
 @pytest.mark.parametrize(
-    "predict_value,current_value,expected",
+    "current_value,predict_value,expected",
     [
-        (1.06, 1.0, "Buy more cryptocurrency"),
-        (0.89, 1.0, "Sell all your cryptocurrency"),
-        (0.96, 1.0, "Do nothing"),
-        (1.02, 1.0, "Do nothing"),
+        (1.0, 1.06, "Buy more cryptocurrency"),
+        (1.0, 0.89, "Sell all your cryptocurrency"),
+        (1.0, 0.96, "Do nothing"),
+        (1.0, 1.02, "Do nothing"),
     ],
 )
 def test_funk_cryptocurrency_action(
-        predict_value: float,
         current_value: float,
+        predict_value: float,
         expected: str
 ) -> None:
     with mock.patch("app.main.get_exchange_rate_prediction") as mock_val:
