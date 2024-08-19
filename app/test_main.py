f1c1c628b0989483226da6d5eed98ea40b156572
user <graban2253@gmail.com>
2024-08-19 23:22:36 +0300

    Finished

--- app/test_main.py
+++ app/test_main.py
@@ -16,10 +16,11 @@ from app.main import cryptocurrency_action
     ]
 )
 @patch("app.main.get_exchange_rate_prediction")
-def test_can_exchange_rate_prediction(mock_get_exchange_rate: MagicMock,
+def test_can_exchange_rate_prediction(mock_get_exchange_rate_prediction:
+MagicMock,
                                       current_rate: float,
                                       predicted_rate: float,
                                       expected_action: str) -> None:
-    mock_get_exchange_rate.return_value = predicted_rate
+    mock_get_exchange_rate_prediction.return_value = predicted_rate
     result: str = cryptocurrency_action(current_rate)
     assert result == expected_action
