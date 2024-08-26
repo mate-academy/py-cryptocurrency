5d0e2bb4a5958f97d612a1e2c2114d7391f6f14a
Oleksandr <aav162807@gmail.com>
2024-08-26 18:00:33 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+import pytest
+from app import main
+
+
+def test_rate_105_percent_do_nothing(monkeypatch):
+    def mock_get_exchange_rate_prediction(current_rate):
+        return current_rate * 1.05
+
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", mock_get_exchange_rate_prediction)
+    result = main.cryptocurrency_action(100)
+
+    assert result == "Do nothing", (
+        "You should not buy cryptocurrency when prediction_rate / current_rate == 1.05"
+    )
+
+
+def test_rate_95_percent_do_nothing(monkeypatch):
+    def mock_get_exchange_rate_prediction(current_rate):
+        return current_rate * 0.95
+
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", mock_get_exchange_rate_prediction)
+    result = main.cryptocurrency_action(100)
+
+    assert result == "Do nothing", (
+        "You should not sell cryptocurrency when prediction_rate / current_rate == 0.95"
+    )
