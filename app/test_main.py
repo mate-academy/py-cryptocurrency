91a535638f3074fa0d618010b91cedfa8b8e6edc
Illia Khomutov <ilyakhomutov613@gmail.com>
2024-08-29 06:24:29 +0300

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,5 @@
-import pytest
-from typing import Any
 from unittest import mock
+import pytest
 
 from app.main import cryptocurrency_action
 
@@ -10,18 +9,17 @@ from app.main import cryptocurrency_action
     [
         (5, 11, "Buy more cryptocurrency"),
         (5, 2, "Sell all your cryptocurrency"),
-        (1.05, 1, "Do nothing"),
-        (1.1, 1.05, "Do nothing")
+        (1, 1.05, "Do nothing"),
+        (1, 0.95, "Do nothing")
 
     ]
 )
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_cryptocurrency_action(
-        rate_prediction_mock: Any,
+        rate_prediction_mock: object,
         current_rate: int | float,
         prediction_rate: int | float,
         result: str
 ) -> None:
     rate_prediction_mock.return_value = prediction_rate
     assert cryptocurrency_action(current_rate) == result
-    rate_prediction_mock.assert_called_once_with(current_rate)
