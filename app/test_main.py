3c98171caec61598cfe9121cb189c57f79435956
Vitalii <dumavb777@gmail.com>
2024-08-30 16:46:21 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+from unittest.mock import patch
+
+import pytest
+
+from app.main import cryptocurrency_action
+
+
+@pytest.fixture()
+def get_exchange_rate_prediction() -> None:
+    with patch("app.main.get_exchange_rate_prediction") as get_exchange_rate_prediction:
+        yield get_exchange_rate_prediction
+
+
+def test_should_return_sell_all_if_high(get_exchange_rate_prediction) -> None:
+    get_exchange_rate_prediction.return_value = 1.05
+    assert cryptocurrency_action(1) == "Do nothing"
+
+
+def test_should_return_do_nothing(get_exchange_rate_prediction) -> None:
+    get_exchange_rate_prediction.return_value = 0.95
+    assert cryptocurrency_action(1) == "Do nothing"
