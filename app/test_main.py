96d53ea2d0b7a850efbf282eeda29ed8986a5021
Arthur <avers.target@gmail.com>
2024-09-02 19:51:48 +0200

    Refactor test_cryptocurrency_action function and add unit tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+import pytest
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    "current_rate,predicted_rate,expected",
+    [
+        (100, 100, "Do nothing"),
+        (100, 106, "Buy more cryptocurrency"),
+        (100, 94, "Sell all your cryptocurrency"),
+        (100, 105, "Do nothing"),
+        (100, 95, "Do nothing"),
+    ],
+)
+def test_cryptocurrency_action(current_rate, predicted_rate, expected):
+    with mock.patch(
+        "app.main.get_exchange_rate_prediction", return_value=predicted_rate
+    ):
+        assert cryptocurrency_action(current_rate) == expected
