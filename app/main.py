4ab8466572fe10721009ca518e7254bd3a820e01
Rudge0 <maksim.pariychuk07@gmail.com>
2024-09-03 21:17:08 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ def get_exchange_rate_prediction(exchange_rate: Union[int, float]) -> float:
 
 def cryptocurrency_action(current_rate: Union[int, float]) -> str:
     prediction_rate = get_exchange_rate_prediction(current_rate)
+    print(prediction_rate)
     if prediction_rate / current_rate > 1.05:
         return "Buy more cryptocurrency"
     if prediction_rate / current_rate < 0.95:
--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,24 @@
-# write your code here
+from unittest import mock
+from app.main import cryptocurrency_action
+import pytest
+
+
+@pytest.mark.parametrize(
+    "prediction_return,current_rate,expectation",
+    [
+        (0.6, 1, "Sell all your cryptocurrency"),
+        (1.6, 1, "Buy more cryptocurrency"),
+        (1.05, 1, "Do nothing"),
+        (0.95, 1, "Do nothing")
+    ]
+)
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_should_buy_more(
+        mocked_prediction: float,
+        prediction_return: float | int,
+        current_rate: float | int,
+        expectation: str
+) -> None:
+
+    mocked_prediction.return_value = prediction_return
+    assert cryptocurrency_action(1) == expectation
