6433c65a4d0658508e5f7d73cb7c50e78f986580
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-09-21 09:44:18 +0300

    add solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,50 @@
-# write your code here
+from unittest import mock
+from typing import Callable
+
+
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_buy_more_crypto(
+        mock_get_prediction: Callable
+) -> None:
+    mock_get_prediction.return_value = 170
+    result = cryptocurrency_action(100)
+    assert result == "Buy more cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_sell_all_crypto(
+    mock_get_prediction: Callable
+) -> None:
+    mock_get_prediction.return_value = 90
+    result = cryptocurrency_action(150)
+    assert result == "Sell all your cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing(
+    mock_get_prediction: Callable
+) -> None:
+    mock_get_prediction.return_value = 101
+    result = cryptocurrency_action(100)
+    assert result == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_rate_105_percent_do_nothing(
+        mock_get_prediction: Callable
+) -> None:
+    mock_get_prediction.return_value = 105
+    result = cryptocurrency_action(100)
+    assert result == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_rate_95_percent_do_nothing(
+        mock_get_prediction: Callable
+) -> None:
+    mock_get_prediction.return_value = 95
+    result = cryptocurrency_action(100)
+    assert result == "Do nothing"
