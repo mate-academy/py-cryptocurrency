5ad2c53e20289ea2028d6423c9aec5c4087c9a11
user <nikitaskrypets@gmail.com>
2024-10-06 13:52:43 +0300

    created tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+import pytest
+from unittest import mock
+from app.main import get_exchange_rate_prediction, cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    ("mock_return_value", "current_rate", "expected"),
+    [
+        (15, 20, "Sell all your cryptocurrency"),
+        (19, 20, "Do nothing"),
+        (20, 15, "Buy more cryptocurrency"),
+        (20, 19, "Do nothing"),
+    ]
+)
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action(
+        get_exchange_rate_prediction: mock.MagicMock,
+        mock_return_value: int,
+        current_rate: int | float,
+        expected: str,
+) -> None:
+    get_exchange_rate_prediction.return_value = mock_return_value
+    assert cryptocurrency_action(current_rate) == expected
