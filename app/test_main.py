f0206e4cdcf0d6ef983f8d9095d38426bcc5f432
yura <pollo7964132@gmail.com>
2024-10-14 19:14:27 +0300

    added test cases

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,35 @@
-from unittest import mock
+import pytest
 
+from unittest import mock
 from app.main import cryptocurrency_action
 
 
+@pytest.mark.parametrize(
+    "current_rate,exchange_rate,expected_result",
+    [
+        pytest.param(
+            1, 1, "Do nothing",
+            id="No change predicted, do nothing"),
+        pytest.param(
+            1, 1.05, "Do nothing",
+            id="Small change predicted, do nothing"),
+        pytest.param(
+            1, 0.95, "Do nothing",
+            id="Small change predicted, do nothing"),
+        pytest.param(
+            1, 1.06, "Buy more cryptocurrency",
+            id="predicted rate is high, Buy more cryptocurrency"),
+        pytest.param(
+            1, 0.5, "Sell all your cryptocurrency",
+            id="predicted rate is low, Sell all your cryptocurrency")
+    ]
+)
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_cryptocurrency_action_if_inner_function_call(
-        mocked_get_exchange_rate_prediction: mock.MagicMock
+        mocked_get_exchange_rate_prediction: mock.MagicMock,
+        current_rate: int | float,
+        exchange_rate: int | float,
+        expected_result: str
 ) -> None:
-    cryptocurrency_action(2)
-    mocked_get_exchange_rate_prediction.assert_called_once_with(2)
+    mocked_get_exchange_rate_prediction.return_value = exchange_rate
+    assert cryptocurrency_action(current_rate) == expected_result
