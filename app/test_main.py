6b81071b90062205cef1885f0cc1ebba5eba92ba
yura <pollo7964132@gmail.com>
2024-10-14 18:18:59 +0300

    implemented one test which cheks func calling

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,12 @@
-# write your code here
+from unittest import mock
+
+import app.main
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_if_inner_function_call(
+        mocked_get_exchange_rate_prediction: mock.MagicMock
+) -> None:
+    cryptocurrency_action(2)
+    mocked_get_exchange_rate_prediction.assert_called_once_with(2)
