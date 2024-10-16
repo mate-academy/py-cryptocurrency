041dc571788078e7b59ad299578df72b8bc6c37e
Ivan Hutsuliak <ivan.guculyak@gmail.com>
2024-10-17 00:58:35 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,32 +1,32 @@
 from app.main import cryptocurrency_action
 from unittest import mock
+import pytest
 
 
-CURRENT_RATE = 1.2
-
-
-def test_cryptocurrency_action_buy_more() -> None:
+@pytest.mark.parametrize(
+    "current_rate, predication_rate, action",
+    [
+        (100, 106, "Buy more cryptocurrency"),
+        (100, 94, "Sell all your cryptocurrency"),
+        (100, 98, "Do nothing"),
+        (100, 105, "Do nothing"),
+        (100, 95, "Do nothing")
+    ],
+    ids=[
+        "buy more cryptocurrency",
+        "sell all your cryptocurrency",
+        "do nothing",
+        "do nothing",
+        "do nothing"
+    ]
+)
+def test_cryptocurrency_action(
+    predication_rate: int | float,
+    current_rate: int | float,
+    action: str
+) -> None:
     with mock.patch(
         "app.main.get_exchange_rate_prediction"
     ) as mocked_prediction:
-        mocked_prediction.return_value = 1.5
-        assert cryptocurrency_action(CURRENT_RATE) == "Buy more cryptocurrency"
-
-
-def test_cryptocurrency_action_sell_all() -> None:
-    with mock.patch(
-        "app.main.get_exchange_rate_prediction"
-    ) as mocked_prediction:
-        mocked_prediction.return_value = 0.8
-        assert (
-            cryptocurrency_action(CURRENT_RATE)
-            == "Sell all your cryptocurrency"
-        )
-
-
-def test_cryptocurrency_action_do_nothing() -> None:
-    with mock.patch(
-        "app.main.get_exchange_rate_prediction"
-    ) as mocked_prediction:
-        mocked_prediction.return_value = 0.9
-        assert cryptocurrency_action(CURRENT_RATE) == "Do nothing"
+        mocked_prediction.return_value = predication_rate
+        assert cryptocurrency_action(current_rate) == action
