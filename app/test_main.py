581e51b2eba5ad5783847848d35ca4c5fc3440e9
roman <skyfoxwork@gmail.com>
2024-10-17 19:14:21 +0300

    Solution-py-cryptocurrency

--- app/test_main.py
+++ app/test_main.py
@@ -6,7 +6,5 @@ from app.main import cryptocurrency_action
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_cryptocurrency_action(mocked_prediction: Any) -> None:
-    # mocket_result = mocked_prediction.return_value = 1
-    # cryptocurrency_action(mocket_result)
     cryptocurrency_action(1)
     mocked_prediction.assert_called_once()
