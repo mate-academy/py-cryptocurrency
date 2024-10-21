570089d03d637b5ec75612694fa806bec2358db4
Serhii <serhijkrasa64uk5@gmail.com>
2024-10-21 16:59:19 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,36 @@
-# write your code here
+from unittest import mock
+from typing import Union
+
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_good_prediction_rate(mocked_get_exchange_rate_prediction:
+                              Union[int, float]) -> None:
+    mocked_get_exchange_rate_prediction.return_value = 1.10
+    result = cryptocurrency_action(1.00)
+    assert (result == "Buy more cryptocurrency")
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_bad_prediction_rate(mocked_get_exchange_rate_prediction:
+                             Union[int, float]) -> None:
+    mocked_get_exchange_rate_prediction.return_value = 0.90
+    result = cryptocurrency_action(1.00)
+    assert (result == "Sell all your cryptocurrency")
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_no_actions_lower_bound(mocked_get_exchange_rate_prediction:
+                                Union[int, float]) -> None:
+    mocked_get_exchange_rate_prediction.return_value = 0.95
+    result = cryptocurrency_action(1.00)
+    assert result == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_no_actions_upper_bound(mocked_get_exchange_rate_prediction:
+                                Union[int, float]) -> None:
+    mocked_get_exchange_rate_prediction.return_value = 1.05
+    result = cryptocurrency_action(1.00)
+    assert result == "Do nothing"
