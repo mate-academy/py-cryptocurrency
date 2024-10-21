24ef1fb0d44acee68e677fe3b4ba87e73a7ba2f2
Vadym Sulim <ohnooq1@gmail.com>
2024-10-21 16:56:32 +0300

    solution 3

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,7 @@ from app.main import cryptocurrency_action
 @patch("app.main.get_exchange_rate_prediction")
 def test_buy_more(mock_get_prediction: Mock) -> None:
     current_rate = 100
-    mock_get_prediction.return_value = 105
+    mock_get_prediction.return_value = 106
     res = cryptocurrency_action(current_rate)
     assert res == "Buy more cryptocurrency"
 
@@ -13,7 +13,7 @@ def test_buy_more(mock_get_prediction: Mock) -> None:
 @patch("app.main.get_exchange_rate_prediction")
 def test_sell_all(mock_get_prediction: Mock) -> None:
     current_rate = 100
-    mock_get_prediction.return_value = 95
+    mock_get_prediction.return_value = 94
     res = cryptocurrency_action(current_rate)
     assert res == "Sell all your cryptocurrency"
 
@@ -21,6 +21,14 @@ def test_sell_all(mock_get_prediction: Mock) -> None:
 @patch("app.main.get_exchange_rate_prediction")
 def test_do_nothing(mock_get_prediction: Mock) -> None:
     current_rate = 100
-    mock_get_prediction.return_value = 103
+    mock_get_prediction.return_value = 105
+    res = cryptocurrency_action(current_rate)
+    assert res == "Do nothing"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_rate_95_percent_do_nothing(mock_get_prediction: Mock) -> None:
+    current_rate = 100
+    mock_get_prediction.return_value = 95
     res = cryptocurrency_action(current_rate)
     assert res == "Do nothing"
