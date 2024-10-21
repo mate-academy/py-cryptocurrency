d100fcd1c85ba5ce88f06e317ead8f6960b4976a
Artem_Shatylo <alukard32244@gmail.com>
2024-10-21 17:57:41 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,5 @@
 import pytest
-from unittest.mock import Mock
+from unittest import mock
 from app.main import cryptocurrency_action
 
 
@@ -7,19 +7,23 @@ from app.main import cryptocurrency_action
     "current_rate, prediction_rate, expected_action",
     [
         (1, 1, "Do nothing"),
-        (1, 1.06, "Buy more cryptocurrency"),
-        (1, 0.92, "Sell all your cryptocurrency"),
+        (1, 1.05, "Do nothing"),
+        (1, 0.95, "Do nothing"),
+        (1, 156, "Buy more cryptocurrency"),
+        (1, 0.72, "Sell all your cryptocurrency"),
+        # (100, 106, "Buy more cryptocurrency"),
+        # (100, 105, "Do nothing"),
+        # (100, 95, "Do nothing"),
+        # (100, 94, "Sell all your cryptocurrency"),
     ],
 )
+@mock.patch("app.main.get_exchange_rate_prediction")
 def test_cryptocurrency_action(
+    mock_func: callable,
     current_rate: float,
     prediction_rate: float,
     expected_action: str,
-    monkeypatch: pytest.MonkeyPatch
 ) -> None:
-    monkeypatch.setattr(
-        "app.main.get_exchange_rate_prediction",
-        Mock(return_value=prediction_rate)
-    )
+    mock_func.return_value = prediction_rate
 
     assert cryptocurrency_action(current_rate) == expected_action
