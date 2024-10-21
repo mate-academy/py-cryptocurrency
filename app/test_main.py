84c2d1ae9d5afe01440535c58803302d5281192d
mregorlite <egor.infinity@gmail.com>
2024-10-21 14:47:32 +0300

    Solution fix

--- app/test_main.py
+++ app/test_main.py
@@ -2,8 +2,8 @@ from app import main
 from app.main import cryptocurrency_action
 
 
-def test_cryptocurrency_action_equal_buy(monkeypatch) -> None:
-    def noting(current_rate):
+def test_cryptocurrency_action_equal_buy(monkeypatch: None) -> None:
+    def noting(current_rate: int | float) -> int | float:
         return current_rate * 1.05
     monkeypatch.setattr(
         main, "get_exchange_rate_prediction", noting
@@ -12,9 +12,8 @@ def test_cryptocurrency_action_equal_buy(monkeypatch) -> None:
     assert test == "Do nothing"
 
 
-
-def test_cryptocurrency_action_equal_sell(monkeypatch) -> None:
-    def noting(current_rate):
+def test_cryptocurrency_action_equal_sell(monkeypatch: None) -> None:
+    def noting(current_rate: int | float) -> int | float:
         return current_rate * 0.95
     monkeypatch.setattr(
         main, "get_exchange_rate_prediction", noting
