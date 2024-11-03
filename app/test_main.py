cdc5f2be2fa2665185b05000cdcd26a24688557a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2024-11-03 22:27:40 +0200

    Solution 4.0

--- app/test_main.py
+++ app/test_main.py
@@ -7,21 +7,19 @@ from app.main import cryptocurrency_action
 @pytest.mark.parametrize(
     "current_rate,expected_result",
     (
-        (12.5, "Buy more cryptocurrency"),
-        (0.3, "Sell all your cryptocurrency")
+        (1.06, "Buy more cryptocurrency"),
+        (0.94, "Sell all your cryptocurrency"),
+        (1.05, "Do nothing"),
+        (0.95, "Do nothing")
     )
 )
-def test_cryptocurrency_action(
+def test_get_exchange_rate_prediction(
     current_rate: int,
     expected_result: str
 ) -> None:
-    assert cryptocurrency_action(current_rate) == expected_result
-
-
-@mock.patch("app.main.get_exchange_rate_prediction")
-def test_get_exchange_rate_prediction(
-    mocked_get_exchange_rate_prediction: mock.MagicMock
-) -> None:
-    mocked_get_exchange_rate_prediction.return_value = 10.0
-    cryptocurrency_action(5.5)
-    mocked_get_exchange_rate_prediction.assert_called_once_with(5.5)
+    with (mock.patch("app.main.get_exchange_rate_prediction")
+          as mocked_get_exchange_rate_prediction
+      ):
+        # mocked_get_exchange_rate_prediction.return_value = current_rate
+        assert cryptocurrency_action(current_rate) == expected_result
+        mocked_get_exchange_rate_prediction.assert_called_once_with(current_rate)
