7b514355e4b178f479b3d38f84f532e27f036a42
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2024-11-03 16:27:04 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,14 @@
-# write your code here
+import pytest
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_get_exchange_rate_prediction(
+    mocked_get_exchange_rate_prediction: mock.MagicMock
+) -> None:
+    mocked_get_exchange_rate_prediction.return_value = 10.0
+    cryptocurrency_action(5.5)
+    mocked_get_exchange_rate_prediction.assert_called_once()
+    pass
