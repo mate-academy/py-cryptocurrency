fd2a2bad34c9aee30ce68d5fbe0dc6f867a95332
Evgeniya <didovichevgeniya@gmail.com>
2024-11-11 16:49:52 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,44 @@
-# write your code here
+import pytest
+from app.main import get_exchange_rate_prediction, cryptocurrency_action
+
+
+def mock_get_exchange_rate_prediction(current_rate: float) -> callable:
+    def mock() -> float:
+        return current_rate
+    return mock
+
+
+def test_buy_more_cryptocurrency(monkeypatch: pytest.MonkeyPatch
+                                 ) -> None:
+    current_rate = 100
+    monkeypatch.setattr("app.get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction(106))
+    result = cryptocurrency_action(current_rate)
+    assert result == "Buy more cryptocurrency"
+
+
+def test_buy_sell_cryptocurrency(monkeypatch: pytest.MonkeyPatch
+                                 ) -> None:
+    current_rate = 100
+    monkeypatch.setattr("app.get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction(94))
+    result = cryptocurrency_action(current_rate)
+    assert result == "Sell all your cryptocurrency"
+
+
+def test_buy_difference_cryptocurrency1(monkeypatch: pytest.MonkeyPatch
+                                        ) -> None:
+    current_rate = 100
+    monkeypatch.setattr("app.get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction(104))
+    result = cryptocurrency_action(current_rate)
+    assert result == "Do nothing"
+
+
+def test_buy_difference_cryptocurrency2(monkeypatch: pytest.MonkeyPatch
+                                        ) -> None:
+    current_rate = 100
+    monkeypatch.setattr("app.get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction(96))
+    result = cryptocurrency_action(current_rate)
+    assert result == "Do nothing"
