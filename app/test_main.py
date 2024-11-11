c5ce6a237b466a3bcabb9344c5dbba304415e782
Evgeniya <didovichevgeniya@gmail.com>
2024-11-11 17:49:07 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -8,8 +8,8 @@ from app.main import cryptocurrency_action
     "current_rate, prediction_rate, expected_result",
     [
         (1.0, 1.06, "Buy more cryptocurrency"),
-        (1.06, 1.0, "Sell all your cryptocurrency"),
-        (1.06, 1.06, "Do nothing"),
+        (1.0, 0.94, "Sell all your cryptocurrency"),
+        (1.0, 1.05, "Do nothing"),
         (1.0, 0.95, "Do nothing")
     ]
 )
@@ -17,8 +17,8 @@ def test_buy_cryptocurrency(current_rate: Union[int, float],
                             prediction_rate: Union[int, float],
                             expected_result: str) -> None:
 
-    with (mock.patch("app.main_get_exchange_rate_prediction")
-          as mock_exchange_rate):
-        mock_exchange_rate.return_value = prediction_rate
+    with (mock.patch("app.main.get_exchange_rate_prediction")
+          as mock_get_exchange_rate):
+        mock_get_exchange_rate.return_value = prediction_rate
         result = cryptocurrency_action(current_rate)
         assert result == expected_result
