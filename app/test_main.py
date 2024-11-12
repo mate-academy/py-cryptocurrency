c9f7ae2f98cca91d0e80b20a51f7b4bf173ab68c
fanni <fmalevych@zoolatech.com>
2024-11-12 14:57:15 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,33 @@
-# write your code here
+import pytest
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+@pytest.fixture()
+def mocked_exchange() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") as mock_exc_rate:
+        yield mock_exc_rate
+
+
+@pytest.mark.parametrize(
+    "current_rate, predicted_rate, expected",
+    [
+        pytest.param(100, 90, "Sell all your cryptocurrency",
+                     id="Should sell if prediction more than 5 percent lower"),
+        pytest.param(100, 120, "Buy more cryptocurrency",
+                     id="Should buy if prediction more than 5 percent higher"),
+        pytest.param(100, 105, "Do nothing",
+                     id="Should return 'Do nothing' if not much diff higher"),
+        pytest.param(100, 95, "Do nothing",
+                     id="Should return 'Do nothing' if not much diff lower")
+    ]
+)
+def test_cryptocurrency_action_returns_correct_values(
+        mocked_exchange: callable,
+        current_rate: int | float,
+        predicted_rate: int | float,
+        expected: str
+) -> None:
+    mocked_exchange.return_value = predicted_rate
+    assert cryptocurrency_action(current_rate) == expected
