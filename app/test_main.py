61ee6143aa56e9b74f1adfe71573c1b41f2faaad
88victory88 <victory.otrok@gmail.com>
2024-11-24 13:17:08 +0800

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,31 @@
-# write your code here
+import unittest
+from unittest.mock import patch, MagicMock
+
+from app.main import cryptocurrency_action
+
+
+class TestCryptocurrency(unittest.TestCase):
+
+    @patch("app.main.get_exchange_rate_prediction")
+    def test_do_nothing_lower_value_cryptocurrency_action(
+            self,
+            mock_get_exchange_rate_prediction: MagicMock) -> None:
+        mock_get_exchange_rate_prediction.return_value = 95
+
+        result = cryptocurrency_action(100)
+
+        expected = "Do nothing"
+
+        self.assertEqual(result, expected)
+
+    @patch("app.main.get_exchange_rate_prediction")
+    def test_do_nothing_upper_value_cryptocurrency_action(
+            self,
+            mock_get_exchange_rate_prediction: MagicMock) -> None:
+        mock_get_exchange_rate_prediction.return_value = 105
+
+        result = cryptocurrency_action(100)
+
+        expected = "Do nothing"
+
+        self.assertEqual(result, expected)
