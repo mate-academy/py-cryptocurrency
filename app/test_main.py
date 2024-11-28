e4d5c95e1f15ac576cc5795fe6882e4a77aaad12
Oleh <sholeg2005@gmail.com>
2024-11-28 22:21:14 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,8 @@
-# write your code here
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_crypto_action(mocked_prediction: mock.Mock):
+    cryptocurrency_action(5)
+    mocked_prediction.assert_called()
