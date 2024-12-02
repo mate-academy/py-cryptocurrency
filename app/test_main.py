63e6a6f63fdffda50cd32f0ee7bf1cbb72832c30
Serhiy <sergeynechaenko@ukr.net>
2024-12-02 21:46:12 +0200

    added edge tests for edge values

--- app/test_main.py
+++ app/test_main.py
@@ -5,20 +5,34 @@ from app.main import cryptocurrency_action
 
 def test_should_return_the_less_rate_from_the_current() -> None:
     with mock.patch("app.main.get_exchange_rate_prediction",
-                    return_value=10.4):
-        result = cryptocurrency_action(9.05)
-        assert result == "Sell all your cryptocurrency"
+                    return_value=12):
+        result = cryptocurrency_action(10)
+        assert result == "Buy more cryptocurrency"
 
 
 def test_should_return_the_higher_rate_from_the_current() -> None:
     with mock.patch("app.main.get_exchange_rate_prediction",
-                    return_value=10.6):
-        result = cryptocurrency_action(11.85)
-        assert result == "Buy more cryptocurrency"
+                    return_value=8):
+        result = cryptocurrency_action(10)
+        assert result == "Sell all your cryptocurrency"
 
 
 def test_should_return_an_equal_rate_from_current() -> None:
     with mock.patch("app.main.get_exchange_rate_prediction",
                     return_value=10.2):
+        result = cryptocurrency_action(10)
+        assert result == "Do nothing"
+
+
+def test_should_return_an_equal_rate_from_current2() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction",
+                    return_value=10.5):
+        result = cryptocurrency_action(10)
+        assert result == "Do nothing"
+
+
+def test_should_return_an_equal_rate_from_current3() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction",
+                    return_value=9.5):
         result = cryptocurrency_action(10)
         assert result == "Do nothing"
