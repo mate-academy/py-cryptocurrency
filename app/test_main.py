85e473812b285e1bb9313134bdec2bc33d54c033
Serhiy <sergeynechaenko@ukr.net>
2024-12-02 21:06:19 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+def test_should_return_the_higher_rate_from_the_current() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=6):
+        result = cryptocurrency_action(1.05)
+        assert result == "Buy more cryptocurrency"
+
+
+def test_should_return_the_less_rate_from_the_current() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=4):
+        result = cryptocurrency_action(0.95)
+        assert result == "Sell all your cryptocurrency"
+
+
+def test_should_return_an_equal_rate_from_current() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=5):
+        result = cryptocurrency_action(1)
+        assert result == "Do nothing"
