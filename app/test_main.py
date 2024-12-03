b166339c67f309df642a557cc0c94846d168562d
01https <Make772@ukr.net>
2024-12-03 11:09:56 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -7,9 +7,18 @@ from app.main import cryptocurrency_action
 def test_cryptocurrency_action_return_correct_result_buy(
         mock_exchange: Callable) -> None:
     mock_exchange.return_value = 100
-    current_rate = 95
+    current_rate = 80
     result = cryptocurrency_action(current_rate)
     assert result == "Buy more cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_return_correct_result_buy(
+        mock_exchange: Callable) -> None:
+    mock_exchange.return_value = 100
+    current_rate = 95
+    result = cryptocurrency_action(current_rate)
+    assert result == "Do nothing"
 
 
 @patch("app.main.get_exchange_rate_prediction")
