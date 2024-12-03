d4afba9e6c01e99bbbfc6421dc7d6c3df271e6ec
Question <make772@ukr.net>
2024-12-03 10:55:55 +0200

    result

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+from typing import Callable
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_return_correct_result_buy(
+        mock_exchange: Callable) -> None:
+    mock_exchange.return_value = 100
+    current_rate = 95
+    result = cryptocurrency_action(current_rate)
+    assert result == "Buy more cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_return_correct_result_sell(
+        mock_exchange: Callable) -> None:
+    mock_exchange.return_value = 85
+    current_rate = 100
+    result = cryptocurrency_action(current_rate)
+    assert result == "Sell all your cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_return_correct_result_nothing(
+        mock_exchange: Callable) -> None:
+    mock_exchange.return_value = 95
+    current_rate = 100
+    result = cryptocurrency_action(current_rate)
+    assert result == "Do nothing"
