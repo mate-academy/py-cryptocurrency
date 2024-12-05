c0062c1836b7490cff721870008b7a3f1eb09e50
Beliar24 <blackdid7@gmail.com>
2024-12-05 23:45:44 +0200

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -19,3 +19,13 @@ def test_buy_more_crypto(mock_value: int,
     with patch("app.main.get_exchange_rate_prediction") as rate:
         rate.return_value = mock_value
         assert cryptocurrency_action(expected_value) == message
+
+
+def test_rate_95_percent_do_nothing():
+    with patch('app.main.get_exchange_rate_prediction', return_value=95):  # Пример значения для 0.95 границы
+        assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_rate_105_percent_do_nothing():
+    with patch('app.main.get_exchange_rate_prediction', return_value=105):  # Пример значения для 1.05 границы
+        assert cryptocurrency_action(100) == "Do nothing"
