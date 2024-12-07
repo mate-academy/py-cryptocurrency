8a491aeee0415b527881b720d3238dcaa332aee4
Глеб Дубинин <gleb1395@gmail.com>
2024-12-07 21:41:00 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -14,14 +14,16 @@ def test_get_exchange_rate_prediction_buy_more_cryptocurrency(
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_rate_exactly_105_do_nothing(mock_get_exchange_rate_prediction: mock.Mock) -> None:
+def test_rate_exactly_105_do_nothing(
+        mock_get_exchange_rate_prediction: mock.Mock) -> None:
     mock_get_exchange_rate_prediction.return_value = 105
     result = cryptocurrency_action(100)
     assert result == "Do nothing"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_rate_exactly_95_do_nothing(mock_get_exchange_rate_prediction: mock.Mock) -> None:
+def test_rate_exactly_95_do_nothing(
+        mock_get_exchange_rate_prediction: mock.Mock) -> None:
     mock_get_exchange_rate_prediction.return_value = 95
     result = cryptocurrency_action(100)
     assert result == "Do nothing"
