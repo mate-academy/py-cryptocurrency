1ede83379f154e49d415ffd0346f47286c311e21
Глеб Дубинин <gleb1395@gmail.com>
2024-12-07 17:15:51 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,8 @@ from app.main import cryptocurrency_action
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_get_exchange_rate_prediction_positive(mock_get_exchange_rate_prediction):
+def test_get_exchange_rate_prediction_positive(
+        mock_get_exchange_rate_prediction: mock.Mock) -> None:
     mock_get_exchange_rate_prediction.return_value = 120
     result = cryptocurrency_action(100)
     assert result == "Buy more cryptocurrency"
@@ -13,7 +14,8 @@ def test_get_exchange_rate_prediction_positive(mock_get_exchange_rate_prediction
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_get_exchange_rate_prediction_negative(mock_get_exchange_rate_prediction):
+def test_get_exchange_rate_prediction_negative(
+        mock_get_exchange_rate_prediction: mock.Mock) -> None:
     mock_get_exchange_rate_prediction.return_value = 80
     result = cryptocurrency_action(100)
     assert result == "Sell all your cryptocurrency"
@@ -21,7 +23,8 @@ def test_get_exchange_rate_prediction_negative(mock_get_exchange_rate_prediction
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_get_exchange_rate_prediction_same_value(mock_get_exchange_rate_prediction):
+def test_get_exchange_rate_prediction_same_value(
+        mock_get_exchange_rate_prediction: mock.Mock) -> None:
     mock_get_exchange_rate_prediction.return_value = 101.12
     result = cryptocurrency_action(100)
     assert result == "Do nothing"
