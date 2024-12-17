8b0eebf0804d308112781635394516d965c3cbde
Mariana Lishchynska <lishchynska.m@gmail.com>
2024-12-17 12:58:31 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from unittest import mock
+from app.main import get_exchange_rate_prediction
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_get_exchange_rate_prediction_to_buy_more(
+        mock_get_exchange_rate_prediction) -> None:
+    mock_get_exchange_rate_prediction.return_value = 150
+    mock_get_exchange_rate_prediction.assert_called_with(50)
+    assert mock_get_exchange_rate_prediction.return_value == 150
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_get_exchange_rate_prediction_to_sell(
+        mock_get_exchange_rate_prediction) -> None:
+    mock_get_exchange_rate_prediction.assert_called_with(200)
+    assert mock_get_exchange_rate_prediction.return_value == 80
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_get_exchange_rate_prediction_to_do_nothing(
+        mock_get_exchange_rate_prediction) -> None:
+    mock_get_exchange_rate_prediction.return_value = 105
+    mock_get_exchange_rate_prediction.assert_called_with(100)
+    assert mock_get_exchange_rate_prediction.return_value == 105
