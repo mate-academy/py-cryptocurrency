81ba1a93997fee6cdce419ed9bf579550a2b2590
Psychox1k <success01001011@gmail.com>
2024-12-17 20:41:47 +0100

    solution v2

--- app/test_main.py
+++ app/test_main.py
@@ -2,8 +2,8 @@ from unittest import mock
 from app.main import cryptocurrency_action
 
 
-@mock.patch('app.main.get_exchange_rate_prediction')
-def test_cryptocurrency_action(mock_get_prediction: mock.Mock):
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action(mock_get_prediction: mock.Mock) -> None:
 
     mock_get_prediction.return_value = 1.11
     assert cryptocurrency_action(1.05) == "Buy more cryptocurrency"
