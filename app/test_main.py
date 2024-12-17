7ec72425a33ce580a634872ac369a4701bd50d3f
Mariana Lishchynska <lishchynska.m@gmail.com>
2024-12-17 13:01:47 +0200

    'Fixed'

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,7 @@ from app.main import get_exchange_rate_prediction
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_get_exchange_rate_prediction_to_buy_more(
-        mock_get_exchange_rate_prediction) -> None:
+        mock_get_exchange_rate_prediction: mock.MagicMock) -> None:
     mock_get_exchange_rate_prediction.return_value = 150
     mock_get_exchange_rate_prediction.assert_called_with(50)
     assert mock_get_exchange_rate_prediction.return_value == 150
@@ -12,14 +12,14 @@ def test_get_exchange_rate_prediction_to_buy_more(
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_get_exchange_rate_prediction_to_sell(
-        mock_get_exchange_rate_prediction) -> None:
+        mock_get_exchange_rate_prediction: mock.MagicMock) -> None:
     mock_get_exchange_rate_prediction.assert_called_with(200)
     assert mock_get_exchange_rate_prediction.return_value == 80
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_get_exchange_rate_prediction_to_do_nothing(
-        mock_get_exchange_rate_prediction) -> None:
+        mock_get_exchange_rate_prediction: mock.MagicMock) -> None:
     mock_get_exchange_rate_prediction.return_value = 105
     mock_get_exchange_rate_prediction.assert_called_with(100)
     assert mock_get_exchange_rate_prediction.return_value == 105
