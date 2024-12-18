9bb9e86eebe4d4b5be434c91f8fe55f1d8dfddcc
Julia Alexeeva <julia4406@gmail.com>
2024-12-18 17:59:12 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,7 +6,7 @@ def test_buy_cryptocurrency_action(monkeypatch: MonkeyPatch) -> None:
     rate = 1
     monkeypatch.setattr(
         "app.main.get_exchange_rate_prediction",
-        lambda predicted_rate: predicted_rate * 1.05
+        lambda predicted_rate: predicted_rate * 1.06
     )
     assert cryptocurrency_action(rate) == "Buy more cryptocurrency"
 
@@ -15,7 +15,7 @@ def test_sell_cryptocurrency_action(monkeypatch: MonkeyPatch) -> None:
     rate = 1
     monkeypatch.setattr(
         "app.main.get_exchange_rate_prediction",
-        lambda predicted_rate: predicted_rate * 0.95
+        lambda predicted_rate: predicted_rate * 0.94
     )
     assert cryptocurrency_action(rate) == "Sell all your cryptocurrency"
 
@@ -24,12 +24,12 @@ def test_do_nothing_cryptocurrency_action(monkeypatch: MonkeyPatch) -> None:
     rate = 1
     monkeypatch.setattr(
         "app.main.get_exchange_rate_prediction",
-        lambda predicted_rate: predicted_rate * 0.96
+        lambda predicted_rate: predicted_rate * 0.95
     )
     assert cryptocurrency_action(rate) == "Do nothing"
 
     monkeypatch.setattr(
         "app.main.get_exchange_rate_prediction",
-        lambda predicted_rate: predicted_rate * 1.04
+        lambda predicted_rate: predicted_rate * 1.05
     )
     assert cryptocurrency_action(rate) == "Do nothing"
