70a404aea6bbbabce62957d87867df237354fc21
Arsenii Aristov <arseniyaristov07@gmail.com>
2024-12-20 19:45:51 +0200

    Solution_currency

--- app/test_main.py
+++ app/test_main.py
@@ -5,19 +5,20 @@ from app.main import cryptocurrency_action
 @pytest.mark.parametrize(
     "current_rate, predicted_rate, expected",
     [
-        (100, 106, "Buy more cryptocurrency"),
-        (100, 94, "Sell all your cryptocurrency"),
-        (100, 102, "Do nothing"),
-        (100, 105, "Do nothing")
+        (1, 1.06, "Buy more cryptocurrency"),
+        (1, 0.94, "Sell all your cryptocurrency"),
+        (1, 1.01, "Do nothing"),
+        (1, 1.05, "Do nothing"),
+        (1, 0.95, "Do nothing"),
     ],
 )
 
 def test_crypto(
-    current_rate: int,
-    predicted_rate: int,
-    expected: int
+    current_rate: float,
+    predicted_rate: float,
+    expected: str
 ) -> None:
-    with (patch("app.main.get_exchange_rate_prediction",
-               return_value = predicted_rate)):
+    with patch("app.main.get_exchange_rate_prediction",
+               return_value = predicted_rate):
         result = cryptocurrency_action(current_rate)
         assert result == expected, f"expected '{expected}' but got '{result}'."
