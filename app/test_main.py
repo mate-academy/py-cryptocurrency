da58aecf37135280f0d460e45ab54d53f9212d7f
Dell <gyrkalomas@gmail.com>
2024-12-21 13:37:21 +0300

    error

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,11 @@
-# write your code here
+import pytest
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+class TestClass:
+    @patch("app.main.get_exchange_rate_prediction")
+    def test_first_func(mock_get_exchange_rate_prediction):
+        mock_get_exchange_rate_prediction.return_value = 110
+        result = cryptocurrency_action(100)
+        assert result == "Buy more cryptocurrency"
