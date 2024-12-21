9fc716d7c5ba9b8420f0a369f479158d1ccfbfb5
Dell <gyrkalomas@gmail.com>
2024-12-21 20:00:16 +0300

    error my tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,22 @@
-# write your code here
+import pytest
+from app.main import cryptocurrency_action
+from unittest.mock import patch
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_sell_all_cryptocurrency(mock_get_exchange_rate_prediction):
+    mock_get_exchange_rate_prediction.return_value = 94
+    result = cryptocurrency_action(100)
+    assert result == "Sell all your cryptocurrency"
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction):
+    mock_get_exchange_rate_prediction.return_value = 100
+    result = cryptocurrency_action(106)
+    assert result == "Buy more cryptocurrency"
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing(mock_get_exchange_rate_prediction):
+    mock_get_exchange_rate_prediction.return_value = 100
+    result = cryptocurrency_action(100)
+    assert result == "Do nothing"
