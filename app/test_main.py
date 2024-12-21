899e90bb4d2e5d54a1cafa8be45ca13db20b9ffd
Koziakov Hlib <hlibs.work@gmail.com>
2024-12-21 23:08:09 +0200

    Added additional tests

--- app/test_main.py
+++ app/test_main.py
@@ -27,3 +27,19 @@ def test_cryptocurrency_action_do_nothing() -> None:
 
         result = cryptocurrency_action(5)
         assert result == "Do nothing"
+
+
+def test_cryptocurrency_action_exact_105() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") as mock_get_prediction:
+        mock_get_prediction.return_value = 5.25  # Ratio == 1.05
+
+        result = cryptocurrency_action(5)
+        assert result == "Buy more cryptocurrency"
+
+
+def test_cryptocurrency_action_exact_95() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction") as mock_get_prediction:
+        mock_get_prediction.return_value = 4.75  # Ratio == 0.95
+
+        result = cryptocurrency_action(5)
+        assert result == "Sell all your cryptocurrency"
