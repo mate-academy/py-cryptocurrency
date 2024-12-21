86cd898958d38c7837cc863f94dce76159a71eab
Koziakov Hlib <hlibs.work@gmail.com>
2024-12-21 23:11:25 +0200

    Fixed length for flake

--- app/test_main.py
+++ app/test_main.py
@@ -30,7 +30,8 @@ def test_cryptocurrency_action_do_nothing() -> None:
 
 
 def test_cryptocurrency_action_exact_105() -> None:
-    with mock.patch("app.main.get_exchange_rate_prediction") as mock_get_prediction:
+    with mock.patch("app.main.get_exchange_rate_prediction")\
+            as mock_get_prediction:
         mock_get_prediction.return_value = 5.25  # Ratio == 1.05
 
         result = cryptocurrency_action(5)
@@ -38,7 +39,8 @@ def test_cryptocurrency_action_exact_105() -> None:
 
 
 def test_cryptocurrency_action_exact_95() -> None:
-    with mock.patch("app.main.get_exchange_rate_prediction") as mock_get_prediction:
+    with mock.patch("app.main.get_exchange_rate_prediction")\
+            as mock_get_prediction:
         mock_get_prediction.return_value = 4.75  # Ratio == 0.95
 
         result = cryptocurrency_action(5)
