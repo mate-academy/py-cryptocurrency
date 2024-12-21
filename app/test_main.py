aa13ba30e52a4cacc4cb6c781cf2e6916f7a92c1
Dell <gyrkalomas@gmail.com>
2024-12-21 21:08:39 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,18 +5,27 @@ from unittest.mock import patch
 
 @patch("app.main.get_exchange_rate_prediction")
 def test_sell_all_cryptocurrency(mock_get_exchange_rate_prediction):
-    mock_get_exchange_rate_prediction.return_value = 94
+    mock_get_exchange_rate_prediction.return_value = 94  # prediction_rate / current_rate < 0.95
     result = cryptocurrency_action(100)
     assert result == "Sell all your cryptocurrency"
 
-@patch("app.main.get_exchange_rate_prediction")
-def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction):
-    mock_get_exchange_rate_prediction.return_value = 100
-    result = cryptocurrency_action(106)
-    assert result == "Buy more cryptocurrency"
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_do_nothing(mock_get_exchange_rate_prediction):
-    mock_get_exchange_rate_prediction.return_value = 100
+def test_buy_more_cryptocurrency(mock_get_exchange_rate_prediction):
+    mock_get_exchange_rate_prediction.return_value = 106  # prediction_rate / current_rate > 1.05
+    result = cryptocurrency_action(100)
+    assert result == "Buy more cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing_upper_boundary(mock_get_exchange_rate_prediction):
+    mock_get_exchange_rate_prediction.return_value = 105  # prediction_rate / current_rate == 1.05
+    result = cryptocurrency_action(100)
+    assert result == "Do nothing"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing_lower_boundary(mock_get_exchange_rate_prediction):
+    mock_get_exchange_rate_prediction.return_value = 95  # prediction_rate / current_rate == 0.95
     result = cryptocurrency_action(100)
     assert result == "Do nothing"
