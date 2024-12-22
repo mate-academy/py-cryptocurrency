66e354c203d4d05a303469d34495f81ef1dde0a7
Arsenii Aristov <arseniyaristov07@gmail.com>
2024-12-22 13:52:49 +0200

    Solution_currency

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,27 @@
-from unittest import mock
-
 import pytest
+from unittest.mock import patch
 from app.main import cryptocurrency_action
 
 
-def test_cryptocurrency_action() -> None:
-    with (mock.patch("app.main.get_exchange_rate_prediction")
-          as mock_get_exchange_rate_prediction):
-
-        mock_get_exchange_rate_prediction.return_value = 105.1
-        assert cryptocurrency_action(100) == "Buy more crypto"
-
-        mock_get_exchange_rate_prediction.return_value = 94.9
-        assert cryptocurrency_action(100) == "Sell all your crypto"
-
-        mock_get_exchange_rate_prediction.return_value = 105.0
-        assert cryptocurrency_action(100) == "Do nothing"
-
-        mock_get_exchange_rate_prediction.return_value = 95.0
-        assert cryptocurrency_action(100) == "Do nothing"
-
-        mock_get_exchange_rate_prediction.return_value = 104.9
-        assert cryptocurrency_action(100) == "Do nothing"
-
-        mock_get_exchange_rate_prediction.return_value = 95.1
-        assert cryptocurrency_action(100) == "Do nothing"
+def test_rate_95_percent_do_nothing():
+    with patch("app.main.get_exchange_rate_prediction", return_value=95.0):
+        assert cryptocurrency_action(100) == "Do nothing", (
+            "You should not sell cryptocurrency when prediction_rate / current_rate == 0.95"
+        )
 
 
-if __name__ == "__main__":
-    pytest.main()
+def test_rate_105_percent_do_nothing():
+    with patch("app.main.get_exchange_rate_prediction", return_value=105.0):
+        assert cryptocurrency_action(100) == "Do nothing", (
+            "You should not buy cryptocurrency when prediction_rate / current_rate == 1.05"
+        )
+
+
+def test_buy_more_crypto():
+    with patch("app.main.get_exchange_rate_prediction", return_value=105.1):
+        assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+def test_sell_all_crypto():
+    with patch("app.main.get_exchange_rate_prediction", return_value=94.9):
+        assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
