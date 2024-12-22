e6d6a04fed972b8ff12d2972f3b67c87fc7b3383
Maks Vakulenko <maksimvakulenkooo@gmail.com>
2024-12-22 14:57:01 +0100

    solution2

--- app/test_main.py
+++ app/test_main.py
@@ -5,11 +5,12 @@ from app.main import cryptocurrency_action
 
 @pytest.mark.parametrize("current_rate, predicted_rate, expected",
                          [
-                             (100, 10, "Buy more cryptocurrency"),
-                             (100, 90, "Sell all your cryptocurrency"),
-                             (100, 102, "Do nothing")
+                             (1, 2, "Buy more cryptocurrency"),
+                             (1, 0.5, "Sell all your cryptocurrency"),
+                             (1, 0.95, "Do nothing"),
+                             (1, 1.05, "Do nothing")
                          ])
-@patch("app.maim.get_exchange_rate_prediction")
+@patch("app.main.get_exchange_rate_prediction")
 def test_cryptocurrency_action(mock_prediction: MagicMock,
                                current_rate: float,
                                predicted_rate: float,
