e4b991579cf149d14a111cd3fa8487de2990f5a0
Mazurkav <aleshkamazurkevich@gmail.com>
2025-01-04 17:35:23 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,10 +1,10 @@
 from unittest.mock import patch
 import pytest
 from typing import Callable
-from main import cryptocurrency_action
+from app.main import cryptocurrency_action
 
 
-@patch("main.get_exchange_rate_prediction")
+@patch("app.main.get_exchange_rate_prediction")
 def test_buy_more_cryptocurrency(
     mock_get_exchange_rate_prediction: Callable
 ) -> None:
@@ -13,7 +13,7 @@ def test_buy_more_cryptocurrency(
     assert result == "Buy more cryptocurrency"
 
 
-@patch("main.get_exchange_rate_prediction")
+@patch("app.main.get_exchange_rate_prediction")
 def test_sell_all_cryptocurrency(
     mock_get_exchange_rate_prediction: Callable
 ) -> None:
@@ -22,7 +22,7 @@ def test_sell_all_cryptocurrency(
     assert result == "Sell all your cryptocurrency"
 
 
-@patch("main.get_exchange_rate_prediction")
+@patch("app.main.get_exchange_rate_prediction")
 def test_do_nothing(
     mock_get_exchange_rate_prediction: Callable
 ) -> None:
