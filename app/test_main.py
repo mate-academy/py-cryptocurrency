16f6491c3507de3dcaa7932f6e975deecf9b40e1
Bohdan <bogdannew@ukr.net>
2025-01-05 14:54:59 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,5 @@
-# write your code here
+def get_exchange_rate_prediction(exchange_rate: Union[int, float]) -> float:
+    if random.choice(["increase", "decrease"]) == "increase":
+        return round(exchange_rate / random.random(), 2)
+    return round(exchange_rate * random.random(), 2)
+
