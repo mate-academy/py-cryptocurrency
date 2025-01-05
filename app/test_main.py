a0c46b7d58a9a41629e861371659f4bd4d9888da
Bohdan <bogdannew@ukr.net>
2025-01-05 22:37:32 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,25 @@
-def get_exchange_rate_prediction(exchange_rate: Union[int, float]) -> float:
-    if random.choice(["increase", "decrease"]) == "increase":
-        return round(exchange_rate / random.random(), 2)
-    return round(exchange_rate * random.random(), 2)
+import pytest
 
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    "current_rate, predict_rate, action",
+    [
+        (100, 110, "Buy more cryptocurrency"),
+        (110, 100, "Sell all your cryptocurrency"),
+        (101, 100, "Do nothing"),
+        (100, 95, "Do nothing"),
+        (100, 105, "Do nothing"),
+    ]
+)
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_action(
+        mock_rate: mock,
+        current_rate: int,
+        predict_rate: int,
+        action: str
+) -> None:
+    mock_rate.return_value = predict_rate
+    assert cryptocurrency_action(current_rate) == action
