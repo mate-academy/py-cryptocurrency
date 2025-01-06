d41e4a79757187b1fb05c5d29d65accfb6246e29
JustVov4uk <volodabudzan4@gmail.com>
2025-01-06 16:30:35 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+from unittest.mock import patch
+
+
+from app.main import cryptocurrency_action
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_if_more_than_5_higher(mock_exchange_rate: bool)\
+        -> None:
+    mock_exchange_rate.return_value = 16
+
+    result = cryptocurrency_action(10)
+    assert result == "Buy more cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_if_more_than_5_lower_than_the_current(mock_exchange_rate: bool)\
+        -> None:
+    mock_exchange_rate.return_value = 10
+
+    result = cryptocurrency_action(19)
+    assert result == "Sell all your cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_if_the_difference_is_small(mock_exchange_rate: bool) -> None:
+    mock_exchange_rate.return_value = 10
+
+    result = cryptocurrency_action(10)
+    assert result == "Do nothing"
