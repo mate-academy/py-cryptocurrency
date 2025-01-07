72a0862b1dfa8af07954459bec7493406760f718
shafran <shafran.andriy@gmail.com>
2025-01-07 19:25:53 +0200

    Created all tests

--- app/test_main.py
+++ app/test_main.py
@@ -8,15 +8,17 @@ from app.main import cryptocurrency_action
 @pytest.mark.parametrize(
     "mocked_prediction_rate, current_rate, expected_action",
     [
-        pytest.param(10.5, 10, "Buy more cryptocurrency"),
+        pytest.param(11.5, 10, "Buy more cryptocurrency"),
         pytest.param(9.0, 10, "Sell all your cryptocurrency"),
-        pytest.param(10.3, 10, "Do nothing"),
+        pytest.param(10.5, 10, "Do nothing"),
+        pytest.param(9.5, 10, "Do nothing"),
         pytest.param(11.025, 10.5, "Do nothing"),
     ],
     ids=[
         "exchange_rate > 5% higher",
         "exchange_rate > 5% lower",
         "exchange_rate is within ±5%",
+        "exchange_rate 5%",
         "exchange_rate is 5%"
     ]
 )
