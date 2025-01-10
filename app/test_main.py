4d7608b40de1b002bc5f9969a6c2d2661560a913
Liza Shulika <liza.shulika2009@gmail.com>
2025-01-10 19:10:07 +0200

    cryptocurrency

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
 # write your code here
+import pytest
+
+
+from app.main import cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    "current_rate, projected_course, expected_result",
+    [
+        pytest.param(100, 150, "Buy more cryptocurrency",
+                     id="Rate increased by more than 5%"),
+        pytest.param(50, 25, "Sell all your cryptocurrency",
+                     id="The rate fell by more than 5%"),
+        pytest.param(40, 41, "Do nothing",
+                     id="nothing"),
+    ]
+)
+def test_cryptocurrency_action(mocker, current_rate: int,
+                               projected_course: int,
+                               expected_result: str) -> None:
+    mocker.patch("app.main.get_exchange_rate_prediction",
+                 return_value=projected_course)
+    result = cryptocurrency_action(current_rate)
+    assert result == expected_result
