52e8b3a07bbdc27cb12346de1ee928c535244f8f
Vlad <vladk4996@gmail.com>
2025-01-13 19:38:18 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,7 +3,7 @@ from app.main import cryptocurrency_action
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_buy_more(mock_get_prediction):
+def test_buy_more(mock_get_prediction: int) -> None:
     current_rate = 100
     mock_get_prediction.return_value = 106
     result = cryptocurrency_action(current_rate)
@@ -11,7 +11,7 @@ def test_buy_more(mock_get_prediction):
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_sell_all(mock_get_prediction):
+def test_sell_all(mock_get_prediction: int) -> None:
     current_rate = 100
     mock_get_prediction.return_value = 94
     result = cryptocurrency_action(current_rate)
@@ -19,7 +19,7 @@ def test_sell_all(mock_get_prediction):
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_do_nothing(mock_get_prediction):
+def test_do_nothing(mock_get_prediction: int) -> None:
     current_rate = 100
     mock_get_prediction.return_value = 101
     result = cryptocurrency_action(current_rate)
@@ -27,7 +27,7 @@ def test_do_nothing(mock_get_prediction):
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_boundary_min(mock_get_prediction):
+def test_boundary_min(mock_get_prediction: int) -> None:
     current_rate = 100
     mock_get_prediction.return_value = 95
     result = cryptocurrency_action(current_rate)
@@ -35,7 +35,7 @@ def test_boundary_min(mock_get_prediction):
 
 
 @patch("app.main.get_exchange_rate_prediction")
-def test_boundary_max(mock_get_prediction):
+def test_boundary_max(mock_get_prediction: int) -> None:
     current_rate = 100
     mock_get_prediction.return_value = 105
     result = cryptocurrency_action(current_rate)
