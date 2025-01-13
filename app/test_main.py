d33fbd8ebe623b63ea08a6b5b2acfd08a261d0c2
Yasha Lapin <yasha.lapin@gmail.com>
2025-01-13 13:45:41 +0200

    Cryptocurrency

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+def test_cryptocurrency_action_when_rate_increases_more_than_5_percent() \
+        -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=110):
+        current_rate = 100
+        result = cryptocurrency_action(current_rate)
+        assert result == "Buy more cryptocurrency"
+
+
+def test_cryptocurrency_action_when_rate_decreases_more_than_5_percent() \
+        -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=95):
+        current_rate = 100
+        result = cryptocurrency_action(current_rate)
+        assert result == "Do nothing"
+
+
+def test_cryptocurrency_action_when_rate_changes_less_than_5_percent() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=105):
+        current_rate = 100
+        result = cryptocurrency_action(current_rate)
+        assert result == "Do nothing"
