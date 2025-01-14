d8ddec7d51804fa0309a84213f9732d0200f0085
Vlad <vladlubeka147@gmail.com>
2025-01-14 21:27:39 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,32 @@
-# write your code here
+import pytest
+from app import main
+
+
+def test_rate_95_percent_do_nothing(monkeypatch: pytest.MonkeyPatch) -> None:
+    """Test that no action is taken when prediction rate is exactly 95% of the
+    current rate."""
+
+    def mock_get_exchange_rate_prediction(_: float) -> float:
+        return 95  # Mock prediction rate exactly 95% of the current rate
+
+    monkeypatch.setattr(main, "get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction)
+
+    result = main.cryptocurrency_action(100)  # Current rate is 100
+    assert result == "Do nothing", "You should not sell cryptocurrency when " \
+                                   "prediction_rate / current_rate == 0.95"
+
+
+def test_rate_105_percent_do_nothing(monkeypatch: pytest.MonkeyPatch) -> None:
+    """Test that no action is taken when prediction rate is exactly 105% of the
+    current rate."""
+
+    def mock_get_exchange_rate_prediction(_: float) -> float:
+        return 105  # Mock prediction rate exactly 105% of the current rate
+
+    monkeypatch.setattr(main, "get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction)
+
+    result = main.cryptocurrency_action(100)  # Current rate is 100
+    assert result == "Do nothing", "You should not buy cryptocurrency when " \
+                                   "prediction_rate / current_rate == 1.05"
