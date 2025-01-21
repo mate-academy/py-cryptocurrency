eaadbf5c3bb4d301fc4e3968b4bf0233c4f5a595
Dmytro Kornitskyi <d.kornitskyi@ukr.net>
2025-01-21 10:35:19 +0200

    Fix flake8

--- app/test_main.py
+++ app/test_main.py
@@ -11,26 +11,26 @@ def mock_exchange_rate_prediction() -> None:
         yield s
 
 
-def test_should_buy_more(mock_exchange_rate_prediction: any):
+def test_should_buy_more(mock_exchange_rate_prediction: any) -> None:
     mock_exchange_rate_prediction.return_value = 1.06
     assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
-def test_should_sell_all(mock_exchange_rate_prediction: any):
+def test_should_sell_all(mock_exchange_rate_prediction: any) -> None:
     mock_exchange_rate_prediction.return_value = 0.94
     assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
 
 
-def test_dont_do_anything_if_1(mock_exchange_rate_prediction: any):
+def test_dont_do_anything_if_1(mock_exchange_rate_prediction: any) -> None:
     mock_exchange_rate_prediction.return_value = 1
     assert cryptocurrency_action(1) == "Do nothing"
 
 
-def test_dont_do_anything_if_095(mock_exchange_rate_prediction: any):
+def test_dont_do_anything_if_095(mock_exchange_rate_prediction: any) -> None:
     mock_exchange_rate_prediction.return_value = 0.95
     assert cryptocurrency_action(1) == "Do nothing"
 
 
-def test_dont_do_anything_if_105(mock_exchange_rate_prediction: any):
+def test_dont_do_anything_if_105(mock_exchange_rate_prediction: any) -> None:
     mock_exchange_rate_prediction.return_value = 1.05
     assert cryptocurrency_action(1) == "Do nothing"
