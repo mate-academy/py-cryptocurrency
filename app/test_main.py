91eaf89f0a4584ddd87df05cd9ef9dae32d79306
IrynaBrenko <brenko.iryna1997@gmail.com>
2025-01-22 12:48:06 +0200

    Solution2255551

--- app/test_main.py
+++ app/test_main.py
@@ -5,22 +5,26 @@ from app.main import cryptocurrency_action
 
 class TestCryptocurrencyAction(unittest.TestCase):
     @patch("app.main.get_exchange_rate_prediction")
-    def test_exchange_rate_is_more_than_5_percent_higher(self, mock_exchange: int | float) -> None:
+    def test_exchange_rate_is_more_than_5_percent_higher(
+            self,
+            mock_exchange: int | float
+    ) -> None:
         mock_exchange.return_value = 1.05
         current_rate = 1
         self.assertEqual(
             cryptocurrency_action(current_rate), "Buy more cryptocurrency"
         )
 
-
     @patch("app.main.get_exchange_rate_prediction")
-    def test_exchange_rate_is_more_than_5_percent_lower(self, mock_exchange: int | float) -> None:
+    def test_exchange_rate_is_more_than_5_percent_lower(
+            self,
+            mock_exchange: int | float
+    ) -> None:
         mock_exchange.return_value = 0.95
         current_rate = 1
         self.assertEqual(
             cryptocurrency_action(current_rate), "Sell all your cryptocurrency"
         )
-
 
     @patch("app.main.get_exchange_rate_prediction")
     def test_different_is_not_much(self, mock_exchange: int | float) -> None:
