7c667c5ae861388935195bb1399045ccb60a3f34
Alex Plugin <plugin0785@gmail.com>
2025-01-29 16:38:35 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+from unittest.mock import patch
+
+from app.main import cryptocurrency_action
+
+
+def test_should_suggest_buy_if_current_rate_more_than_5():
+    with patch("app.main.get_exchange_rate_prediction", return_value=1.4):
+        assert cryptocurrency_action(1.3) == "Buy more cryptocurrency"
+
+
+def test_should_suggest_sell_if_current_rate_less_than_5():
+    with patch("app.main.get_exchange_rate_prediction", return_value=1.4):
+        assert cryptocurrency_action(1.5) == "Sell all your cryptocurrency"
+
+
+def test_should_suggest_do_nothing_if_current_rate_has_small_differance():
+    with patch("app.main.get_exchange_rate_prediction", return_value=1.4):
+        assert cryptocurrency_action(1.42) == "Do nothing"
+
+
+def test_should_suggest_do_nothing_if_current_rate_exactly_5_percent_higher():
+    with patch("app.main.get_exchange_rate_prediction", return_value=1.05):
+        assert cryptocurrency_action(1.0) == "Do nothing"
+
+
+def test_should_suggest_do_nothing_if_current_rate_exactly_5_percent_lower():
+    with patch("app.main.get_exchange_rate_prediction", return_value=0.95):
+        assert cryptocurrency_action(1.0) == "Do nothing"
+
+
