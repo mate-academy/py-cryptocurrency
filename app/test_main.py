c2ef7063d6efcfe89e2f7c4d01d799116c1f1ba9
Alex Plugin <plugin0785@gmail.com>
2025-01-29 16:48:14 +0200

    Solution3

--- app/test_main.py
+++ app/test_main.py
@@ -3,28 +3,26 @@ from unittest.mock import patch
 from app.main import cryptocurrency_action
 
 
-def test_should_suggest_buy_if_current_rate_more_than_5():
+def test_should_suggest_buy_if_current_rate_more_than_5() -> None:
     with patch("app.main.get_exchange_rate_prediction", return_value=1.4):
         assert cryptocurrency_action(1.3) == "Buy more cryptocurrency"
 
 
-def test_should_suggest_sell_if_current_rate_less_than_5():
+def test_should_suggest_sell_if_current_rate_less_than_5() -> None:
     with patch("app.main.get_exchange_rate_prediction", return_value=1.4):
         assert cryptocurrency_action(1.5) == "Sell all your cryptocurrency"
 
 
-def test_should_suggest_do_nothing_if_current_rate_has_small_differance():
+def test_should_suggest_do_nothing_if_current_rate_has_small_diff() -> None:
     with patch("app.main.get_exchange_rate_prediction", return_value=1.4):
         assert cryptocurrency_action(1.42) == "Do nothing"
 
 
-def test_should_suggest_do_nothing_if_current_rate_exactly_5_percent_higher():
+def test_should_suggest_do_nothing_if_current_rate_exactly_5_higher() -> None:
     with patch("app.main.get_exchange_rate_prediction", return_value=1.05):
         assert cryptocurrency_action(1.0) == "Do nothing"
 
 
-def test_should_suggest_do_nothing_if_current_rate_exactly_5_percent_lower():
+def test_should_suggest_do_nothing_if_current_rate_exactly_5_lower() -> None:
     with patch("app.main.get_exchange_rate_prediction", return_value=0.95):
         assert cryptocurrency_action(1.0) == "Do nothing"
-
-
