29cd468789a1db16464122b414bb28267d6899b5
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2025-02-01 15:03:22 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,12 +7,12 @@ class TestCryptocurrencyAction(unittest.TestCase):
     @mock.patch("app.main.get_exchange_rate_prediction")
     def test_cryptocurrency_action(self, mock_prediction):
         test_cases = [
-            (100, 105.1, "Buy more cryptocurrency"),
-            (100, 94.9, "Sell all your cryptocurrency"),
-            (100, 104.9, "Do nothing"),
-            (100, 95.1, "Do nothing"),
-            (50, 52.6, "Buy more cryptocurrency"),
-            (50, 47.4, "Sell all your cryptocurrency"),
+            (100, 105.01, "Buy more cryptocurrency"),
+            (100, 94.99, "Sell all your cryptocurrency"),
+            (100, 105.0, "Do nothing"),
+            (100, 95.0, "Do nothing"),
+            (50, 52.51, "Buy more cryptocurrency"),
+            (50, 47.49, "Sell all your cryptocurrency"),
             (50, 50, "Do nothing"),
         ]
 
