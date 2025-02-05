47abe3497e0cdc5f31a434889afbc5a829a104fc
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-02-05 19:18:25 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,23 +1,26 @@
 from app.main import cryptocurrency_action
-from unittest.mock import patch
-import pytest
-from typing import Union
+import unittest
+from unittest import mock
 
 
-@pytest.mark.parametrize("current_rate, predicted_rate, expected", [
-    (100, 106, "Buy more cryptocurrency"),
-    (100, 105.01, "Buy more cryptocurrency"),
-    (100, 105, "Do nothing"),
-    (100, 104.99, "Do nothing"),
-    (100, 95.01, "Do nothing"),
-    (100, 95, "Do nothing"),
-    (100, 94.99, "Sell all your cryptocurrency"),
-    (100, 94, "Sell all your cryptocurrency"),
-])
-def test_cryptocurrency_action(current_rate: Union[int, float],
-                               predicted_rate: Union[int, float],
-                               expected: str) -> None:
-    with patch("app.main.get_exchange_rate_prediction",
-               return_value=predicted_rate):
-        result = cryptocurrency_action(current_rate)
-        assert result == expectedgit
+class TestCryptocurrencyAction(unittest.TestCase):
+
+    @mock.patch(
+        "app.main.get_exchange_rate_prediction"
+    )
+    def test_cryptocurrency_action(self, mock_prediction: mock.MagicMock) \
+            -> None:
+        test_cases = [
+            (100, 105.01, "Buy more cryptocurrency"),
+            (100, 94.99, "Sell all your cryptocurrency"),
+            (100, 105.0, "Do nothing"),
+            (100, 95.0, "Do nothing"),
+            (50, 52.51, "Buy more cryptocurrency"),
+            (50, 47.49, "Sell all your cryptocurrency"),
+            (50, 50, "Do nothing"),
+        ]
+
+        for current_rate, predicted_rate, expected in test_cases:
+            mock_prediction.return_value = predicted_rate
+            result = cryptocurrency_action(current_rate)
+            self.assertEqual(result, expected)
