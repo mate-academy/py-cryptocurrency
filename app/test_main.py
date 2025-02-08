b20b64bd7e2219fc8e36b3f3e606bab4a320b664
RuslanTopal <baranotik15@gmail.com>
2025-02-08 08:57:57 +0000

    correct my flake8 mistake

--- app/test_main.py
+++ app/test_main.py
@@ -9,7 +9,7 @@ from .main import get_exchange_rate_prediction, cryptocurrency_action
         (100, "increase", 0.5, round(100 / 0.5, 2)),
         (100, "decrease", 0.2, round(100 * 0.2, 2)),
     ],
-    ids = [
+    ids=[
         "increase_case",
         "decrease_case",
     ]
@@ -20,24 +20,28 @@ def test_get_exchange_rate_prediction(
         mock_random: float,
         expected_result: float,
 ) -> None:
-    with mock.patch("random.choice", return_value=mock_choice), \
-        mock.patch("random.random", return_value=mock_random):
+    with (mock.patch(
+            "random.choice", return_value=mock_choice
+    ), mock.patch(
+        "random.random", return_value=mock_random
+    )):
         result = get_exchange_rate_prediction(exchange_rate)
 
-        assert result == expected_result, f"Expected {expected_result}, but it turned out {result}"
+        assert result == expected_result, (f"Expected {expected_result}, "
+                                           f"but it turned out {result}")
 
 
 @pytest.mark.parametrize(
     "current_rate,prediction_rate,expected_result",
     [
         (100, 200, "Buy more cryptocurrency"),
-        (100, 10, "Sell all your cryptocurrency" ),
+        (100, 10, "Sell all your cryptocurrency"),
         (100, 99, "Do nothing"),
         (100, 101, "Do nothing"),
         (100, 95, "Do nothing"),
         (100, 105, "Do nothing"),
     ],
-    ids = [
+    ids=[
         "increase_case",
         "decrease_case",
         "normal_decrease_case",
@@ -51,7 +55,11 @@ def test_cryptocurrency_action(
     prediction_rate: int,
     expected_result: str
 ) -> None:
-    with mock.patch("app.main.get_exchange_rate_prediction", return_value=prediction_rate):
+    with mock.patch(
+            "app.main.get_exchange_rate_prediction",
+            return_value=prediction_rate
+    ):
         result = cryptocurrency_action(current_rate)
 
-    assert result == expected_result, f"Expected {expected_result}, but it turned out {result}"
+    assert result == expected_result, (f"Expected {expected_result}, "
+                                       f"but it turned out {result}")
