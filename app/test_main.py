f96dbc93f06ce75a4a46829ce288617c7a3a3d05
Odin-max <meh84741@gmail.com>
2025-02-11 22:52:45 +0200

    1

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,19 @@
-# write your code here
+from unittest.mock import patch
+from app.main import get_exchange_rate_prediction, cryptocurrency_action 
+
+
+def test_buy_more_cryptocurrency() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value = 105.1):
+        assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+    
+def test_sell_all_cryptocurrency() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=94.9):
+        assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+def test_do_nothing() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=100):
+        assert cryptocurrency_action(100) == "Do nothing"
+    with patch("app.main.get_exchange_rate_prediction", return_value=104.9):
+        assert cryptocurrency_action(100) == "Do nothing"
+    with patch("app.main.get_exchange_rate_prediction", return_value=95.1):
+        assert cryptocurrency_action(100) == "Do nothing"
