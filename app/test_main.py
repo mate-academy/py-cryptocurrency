cd67eec2196e7c97f2a2f3b5f8917ccac64278c2
Dmytro.Poberezhnyi <foxymood@gmail.com>
2025-02-12 12:33:22 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,11 +2,21 @@ from unittest.mock import patch
 from app.main import cryptocurrency_action
 
 
-def test_cryptocurrency_action() -> None:
-    with patch("app.main.get_exchange_rate_prediction") as mock_prediction:
-        mock_prediction.side_effect = [106, 94, 102, 98]
-
+def test_buy_more_cryptocurrency() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=106):
         assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+def test_sell_all_cryptocurrency() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=94):
         assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+def test_do_nothing_105() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=105):
         assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_do_nothing_95() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=95):
         assert cryptocurrency_action(100) == "Do nothing"
