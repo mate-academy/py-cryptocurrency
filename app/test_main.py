d8c563258920a2a5ce34a469eb19195e164bf5a8
PythonToGo <pythontogoplease@gmail.com>
2025-02-13 21:16:34 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,33 @@
-# write your code here
+from app.main import cryptocurrency_action
+import pytest
+from unittest.mock import patch
+
+
+@pytest.fixture
+def mock_predicion_high() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=106):
+        yield
+
+
+@pytest.fixture
+def mock_prediction_low() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=94):
+        yield
+
+
+@pytest.fixture
+def mock_prediction_same() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=100):
+        yield
+
+
+def test_should_buy_more(mock_prediction_high: None) -> None:
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+def test_should_sell_all(mock_prediction_low: None) -> None:
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+def test_should_do_nothing(mock_prediction_same: None) -> None:
+    assert cryptocurrency_action(100) == "Do nothing"
