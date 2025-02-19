0f46feaaea220b115869db401ef19e63440a634c
Ruslan Priadko <r.priadko@ukr.net>
2025-02-19 10:35:21 +0200

    test_do_nothing_if_minor_up_changes changed

--- app/test_main.py
+++ app/test_main.py
@@ -14,8 +14,8 @@ def test_do_nothing_if_minor_down_changes(
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_do_nothing_if_minor_up_changes(
         test_rate_prediction: Callable) -> None:
-    test_rate_prediction.return_value = 2
-    assert cryptocurrency_action(1.9) == "Do nothing"
+    test_rate_prediction.return_value = 1.05
+    assert cryptocurrency_action(1) == "Do nothing"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
