dc4a900609c931f68645c499aeb83d9170bd66ec
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-02-27 09:32:44 +0200

    Finish

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+import pytest
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    "current_rate, prediction_rate, expected",
+    [
+        (100, 106, "Buy more cryptocurrency"),
+        (100, 94, "Sell all your cryptocurrency"),
+        (100, 104, "Do nothing"),
+        (100, 96, "Do nothing"),
+    ]
+)
+def test_control_signals_for_buy_and_sell(
+        current_rate: int | float,
+        prediction_rate: int | float,
+        expected: str
+) -> None:
+    with patch(
+            "app.main.get_exchange_rate_prediction",
+            return_value=prediction_rate
+    ):
+        result = cryptocurrency_action(current_rate)
+        assert result == expected
