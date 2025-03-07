f5a21b6de58ac7bd57952cbbb3cd286698c25257
AndyStar <andy_star@ukr.net>
2025-03-07 12:57:16 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,36 @@
-# write your code here
+from unittest import mock
+
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_up(get_exchange_rate_prediction: float) -> None:
+
+    get_exchange_rate_prediction.return_value = 1.1
+    result = cryptocurrency_action(1)
+    print(result)
+    assert result == "Buy more cryptocurrency"
+    get_exchange_rate_prediction.assert_called_once()
+    get_exchange_rate_prediction.assert_called_once_with(1)
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_down(get_exchange_rate_prediction: float) -> None:
+
+    get_exchange_rate_prediction.return_value = 1.88
+    result = cryptocurrency_action(2)
+    print(result)
+    assert result == "Sell all your cryptocurrency"
+    get_exchange_rate_prediction.assert_called_once()
+    get_exchange_rate_prediction.assert_called_once_with(2)
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_still(get_exchange_rate_prediction: float) -> None:
+
+    get_exchange_rate_prediction.return_value = 3.00
+    result = cryptocurrency_action(3)
+    print(result)
+    assert result == "Do nothing"
+    get_exchange_rate_prediction.assert_called_once()
+    get_exchange_rate_prediction.assert_called_once_with(3)
