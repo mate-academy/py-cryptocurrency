f4d2a365896893205df7948682c70853eae941cf
Oleksii Kartashov <oleksii.kartashov1@gmail.com>
2025-03-08 22:01:03 +0200

    'Solution_cryptocurrency'

--- app/test_main.py
+++ app/test_main.py
@@ -9,7 +9,7 @@ def test_rate_95_percent_do_nothing(mock_get_prediction: patch) -> None:
 
     result = main.cryptocurrency_action(current_rate)
 
-    assert result == "Do nothing", f"Expected \"Do nothing\" but got {result}"
+    assert result == "Do nothing", f"Expected 'Do nothing' but got {result}"
 
 
 @patch("app.main.get_exchange_rate_prediction")
@@ -19,4 +19,4 @@ def test_rate_105_percent_do_nothing(mock_get_prediction: patch) -> None:
 
     result = main.cryptocurrency_action(current_rate)
 
-    assert result == "Do nothing", f"Expected \"Do nothing\" but got {result}"
+    assert result == "Do nothing", f"Expected 'Do nothing' but got {result}"
