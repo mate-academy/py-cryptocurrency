d5f5124a557a7cf81e50cfe5fc064bdc8454ec54
Polina Haltseva <polinayut@gmail.com>
2025-03-08 12:11:49 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,24 @@
-# write your code here
+import pytest
+from typing import Any
+from app.main import cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    "current, percent, result",
+    [
+        (100, 1.5, "Buy more cryptocurrency"),
+        (100, 1.05, "Do nothing"),
+        (100, 0.95, "Do nothing"),
+        (100, 0.94, "Sell all your cryptocurrency"),
+    ]
+)
+def test_should_return_to_manage_finances(
+    monkeypatch: Any,
+    current: int,
+    percent: float | int,
+    result: str
+) -> None:
+    monkeypatch.setattr("app.main.get_exchange_rate_prediction",
+                        lambda rate: current * percent
+                        )
+    assert cryptocurrency_action(current) == result
