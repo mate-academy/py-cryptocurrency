8a0d3b6dcc8c54ca1fbdffa10d9353f41747b804
Khrystyna Sen <anollina@ukr.net>
2025-03-09 01:45:18 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,28 +1,46 @@
-from pytest_mock import MockerFixture
+from typing import Any, Union
+
 from app.main import cryptocurrency_action
 
 
-def test_buy_more_cryptocurrency(mocker: MockerFixture) -> None:
-    mocker.patch("app.main.get_exchange_rate_prediction", return_value=1.06)
+def test_buy_more_cryptocurrency(monkeypatch: Any) -> None:
+    def mock_get_exchange_rate_prediction(
+            exchange_rate: Union[int, float]) -> float:
+        return 1.06
+    monkeypatch.setattr("app.main.get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction)
     current_rate = 1.00
     result = cryptocurrency_action(current_rate)
     assert result == "Buy more cryptocurrency"
 
 
-def test_sell_all_cryptocurrency(mocker: MockerFixture) -> None:
-    mocker.patch("app.main.get_exchange_rate_prediction", return_value=0.94)
+def test_sell_all_cryptocurrency(monkeypatch: Any) -> None:
+    def mock_get_exchange_rate_prediction(
+            exchange_rate: Union[int, float]) -> float:
+        return 0.94
+    monkeypatch.setattr("app.main.get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction)
     current_rate = 1.00
     result = cryptocurrency_action(current_rate)
     assert result == "Sell all your cryptocurrency"
 
 
-def test_do_nothing(mocker: MockerFixture) -> None:
-    mocker.patch("app.main.get_exchange_rate_prediction", return_value=1.05)
+def test_do_nothing(monkeypatch: Any) -> None:
+    def mock_get_exchange_rate_prediction(
+            exchange_rate: Union[int, float]) -> float:
+        return 1.05
+
+    def mock_get_exchange_rate_prediction2(
+            exchange_rate: Union[int, float]) -> float:
+        return 0.95
+
+    monkeypatch.setattr("app.main.get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction)
     current_rate = 1.00
     result = cryptocurrency_action(current_rate)
     assert result == "Do nothing"
 
-    mocker.patch("app.main.get_exchange_rate_prediction", return_value=0.95)
-    current_rate = 1.00
+    monkeypatch.setattr("app.main.get_exchange_rate_prediction",
+                        mock_get_exchange_rate_prediction2)
     result = cryptocurrency_action(current_rate)
     assert result == "Do nothing"
