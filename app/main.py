31813be556fbfc2f1a5a5f225212c0280923aef9
Oleksandr Kurin <profysss8712@gmail.com>
2025-03-10 15:56:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,12 +3,24 @@ from typing import Union
 
 
 def get_exchange_rate_prediction(exchange_rate: Union[int, float]) -> float:
+    """
+    Прогнозирует курс криптовалюты на основе текущего курса.
+
+    :param exchange_rate: Текущий курс криптовалюты.
+    :return: Прогнозируемый курс криптовалюты.
+    """
     if random.choice(["increase", "decrease"]) == "increase":
         return round(exchange_rate / random.random(), 2)
     return round(exchange_rate * random.random(), 2)
 
 
 def cryptocurrency_action(current_rate: Union[int, float]) -> str:
+    """
+    Определяет действие по отношению к криптовалюте на основе текущего курса и прогноза.
+
+    :param current_rate: Текущий курс криптовалюты.
+    :return: Рекомендация (купить, продать, ничего не делать).
+    """
     prediction_rate = get_exchange_rate_prediction(current_rate)
     if prediction_rate / current_rate > 1.05:
         return "Buy more cryptocurrency"
--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+import pytest
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    "current_rate, predicted_rate, expected_action",
+    [
+        (100, 106, "Buy more cryptocurrency"),  # Курс увеличится на 6%
+        (100, 94, "Sell all your cryptocurrency"),  # Курс уменьшится на 6%
+        (100, 104, "Do nothing"),  # Курс увеличится на 4%
+        (100, 96, "Do nothing"),  # Курс уменьшится на 4%
+    ],
+)
+def test_cryptocurrency_action(
+    current_rate: int, predicted_rate: float, expected_action: str
+) -> None:
+    """
+    Тестирует функцию cryptocurrency_action на различных сценариях.
+
+    :param current_rate: Текущий курс криптовалюты.
+    :param predicted_rate: Прогнозируемый курс криптовалюты.
+    :param expected_action: Ожидаемое действие.
+    """
+    with patch("app.main.get_exchange_rate_prediction", return_value=predicted_rate):
+        assert cryptocurrency_action(current_rate) == expected_action
