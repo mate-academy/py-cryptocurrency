31478c9a3d6299d3e06fb3ee72e0f5a9762dcb52
dima <giomozda74@gmail.com>
2025-03-12 18:32:52 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from app.main import cryptocurrency_action
+from unittest.mock import patch
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_exchange_rate_more_then_5_percent(mock_exchange_rate: any) -> None:
+    mock_exchange_rate.return_value = 110
+    result = cryptocurrency_action(100)
+    assert result == "Buy more cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_exchange_rate_lower_then_5_percent(mock_exchange_rate: any) -> None:
+    mock_exchange_rate.return_value = 90
+    result = cryptocurrency_action(100)
+    assert result == "Sell all your cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_exchange_rate_different(mock_exchange_rate: any) -> None:
+    mock_exchange_rate.return_value = 102
+    result = cryptocurrency_action(100)
+    assert result == "Do nothing"
