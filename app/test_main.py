390f38ee91ce2f2632dc4378b2589a6e6382045e
dima <giomozda74@gmail.com>
2025-03-12 18:40:17 +0200

    solution2

--- app/test_main.py
+++ app/test_main.py
@@ -4,20 +4,29 @@ from unittest.mock import patch
 
 @patch("app.main.get_exchange_rate_prediction")
 def test_exchange_rate_more_then_5_percent(mock_exchange_rate: any) -> None:
-    mock_exchange_rate.return_value = 110
+    mock_exchange_rate.return_value = 105.1
     result = cryptocurrency_action(100)
     assert result == "Buy more cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction")
 def test_exchange_rate_lower_then_5_percent(mock_exchange_rate: any) -> None:
-    mock_exchange_rate.return_value = 90
+    mock_exchange_rate.return_value = 94.9
     result = cryptocurrency_action(100)
     assert result == "Sell all your cryptocurrency"
 
 
 @patch("app.main.get_exchange_rate_prediction")
 def test_exchange_rate_different(mock_exchange_rate: any) -> None:
-    mock_exchange_rate.return_value = 102
+    mock_exchange_rate.return_value = 105
+    result = cryptocurrency_action(100)
+    assert result == "Do nothing"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_exchange_rate_different_with_low_predict_value(
+        mock_exchange_rate: any
+) -> None:
+    mock_exchange_rate.return_value = 95
     result = cryptocurrency_action(100)
     assert result == "Do nothing"
