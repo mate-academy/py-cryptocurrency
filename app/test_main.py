5911d7b9dcc00488f84c8d4bac55138ac26748fa
ViktorBl734 <harbourprogress@gmail.com>
2025-03-14 19:59:55 +0200

    Cryptocurrency

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_goes_right(mocked_prediction: float) -> None:
+    mocked_prediction.return_value = 106
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+    mocked_prediction.return_value = 105
+    assert cryptocurrency_action(100) == "Do nothing"
+
+    mocked_prediction.return_value = 90
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+    mocked_prediction.return_value = 102
+    assert cryptocurrency_action(100) == "Do nothing"
+
+    mocked_prediction.return_value = 98
+    assert cryptocurrency_action(100) == "Do nothing"
+
+    mocked_prediction.return_value = 95
+    assert cryptocurrency_action(100) == "Do nothing"
