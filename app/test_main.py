dbf970cf492f01897ada6c409c5b8c920297aaf1
Platon Goncharevskyi <platongoncharevskyi@gmail.com>
2025-03-17 00:37:30 +0200

    solution upd1

--- app/test_main.py
+++ app/test_main.py
@@ -3,17 +3,17 @@ from app.main import cryptocurrency_action
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action(mock_prediction: int) -> None:
-    mock_prediction.return_value = 105.1
+def test_cryptocurrency_action(mock_prediction):
+    mock_prediction.return_value = 106
     assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
-    mock_prediction.return_value = 94.9
+    mock_prediction.return_value = 94
     assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
 
-    mock_prediction.return_value = 104.9
+    mock_prediction.return_value = 104
     assert cryptocurrency_action(100) == "Do nothing"
 
-    mock_prediction.return_value = 95.1
+    mock_prediction.return_value = 96
     assert cryptocurrency_action(100) == "Do nothing"
 
     mock_prediction.return_value = 100
