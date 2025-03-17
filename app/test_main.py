c3b74d357eae1df92e8a67cabd0045a65e5bbf37
Zahar <galushkozahar>
2025-03-17 18:29:21 +0200

    Develop

--- app/test_main.py
+++ app/test_main.py
@@ -1,23 +1,24 @@
-from unittest import TestCase
+import unittest
 
 from unittest.mock import patch
 
 from app.main import cryptocurrency_action
 
 
-class TestCryptocurrencyAction(TestCase):
+class TestCryptocurrencyAction(unittest.TestCase):
+
     @patch("app.main.get_exchange_rate_prediction")
-    def test_but_more_crypto(self, mock_prediction: int) -> None:
+    def test_buy_more_cryptocurrency(self, mock_prediction: int) -> None:
         mock_prediction.return_value = 110
         self.assertEqual(cryptocurrency_action(100), "Buy more cryptocurrency")
 
     @patch("app.main.get_exchange_rate_prediction")
-    def test_sell_all_crypto(self, mock_prediction: int) -> None:
+    def test_sell_all_cryptocurrency(self, mock_prediction: int) -> None:
         mock_prediction.return_value = 90
         self.assertEqual(cryptocurrency_action(100),
                          "Sell all your cryptocurrency")
 
     @patch("app.main.get_exchange_rate_prediction")
-    def test_do_nothing(self, make_prediction: int) -> None:
-        make_prediction.return_value = 104
+    def test_do_nothing(self, mock_prediction: int) -> None:
+        mock_prediction.return_value = 104
         self.assertEqual(cryptocurrency_action(100), "Do nothing")
