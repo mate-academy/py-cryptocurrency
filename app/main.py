f19deb8563e4f48c2a7f769a196236c36b3920e3
Roman Kostiv <rkostiv@gmail.com>
2025-03-18 19:57:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import random
 from typing import Union
 
-
 def get_exchange_rate_prediction(exchange_rate: Union[int, float]) -> float:
     if random.choice(["increase", "decrease"]) == "increase":
         return round(exchange_rate / random.random(), 2)
@@ -15,3 +14,4 @@ def cryptocurrency_action(current_rate: Union[int, float]) -> str:
     if prediction_rate / current_rate < 0.95:
         return "Sell all your cryptocurrency"
     return "Do nothing"
+
--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,13 @@
-# write your code here
+from unittest.mock import patch
+
+from app.main import cryptocurrency_action
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action(mock_get_exchange_rate_prediction):
+    mock_get_exchange_rate_prediction.return_value = 2
+
+    result = cryptocurrency_action(mock_get_exchange_rate_prediction)
+
+    assert result == "Buy more cryptocurrency"
+    assert result == "Sell all your cryptocurrency"
+    assert result == "Do nothing"
