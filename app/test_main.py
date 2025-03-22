aedcca5a49395b457dd7d6d45afb3d38df912934
Oleksandr <aleksandrpaskoo22@gmail.com>
2025-03-22 18:05:33 +0100

    Implement

--- app/test_main.py
+++ app/test_main.py
@@ -16,9 +16,9 @@ def test_check_when_we_buy_more(
     assert current_prediction == "Buy more cryptocurrency"
 
 
-@mock.patch("app.main.get_exchange_rate_prediction", return_value=13.1)
+@mock.patch("app.main.get_exchange_rate_prediction", return_value=10)
 def test_check_when_we_sell(mocked_rate_prediction: mock.MagicMock) -> None:
-    current_prediction = cryptocurrency_action(10)
+    current_prediction = cryptocurrency_action(13.1)
     assert current_prediction == "Sell all your cryptocurrency"
 
 
