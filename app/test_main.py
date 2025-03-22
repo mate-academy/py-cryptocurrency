79f5a108794f4045cb051f358c673cf0ea532ce8
Oleksandr <aleksandrpaskoo22@gmail.com>
2025-03-22 18:19:07 +0100

    Implement

--- app/test_main.py
+++ app/test_main.py
@@ -9,16 +9,16 @@ def test_check_if_function_was_called(
     mocked_prediction.assert_called_once()
 
 
-@mock.patch("app.main.get_exchange_rate_prediction", return_value=15)
+@mock.patch("app.main.get_exchange_rate_prediction", return_value=12.6)
 def test_check_when_we_buy_more(
         mocked_rate_prediction: mock.MagicMock) -> None:
     current_prediction = cryptocurrency_action(12)
     assert current_prediction == "Buy more cryptocurrency"
 
 
-@mock.patch("app.main.get_exchange_rate_prediction", return_value=10)
+@mock.patch("app.main.get_exchange_rate_prediction", return_value=9.5)
 def test_check_when_we_sell(mocked_rate_prediction: mock.MagicMock) -> None:
-    current_prediction = cryptocurrency_action(13.1)
+    current_prediction = cryptocurrency_action(10)
     assert current_prediction == "Sell all your cryptocurrency"
 
 
