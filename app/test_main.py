7acad96f73f12268a642459a35658f7511205013
Max <smolinskijmaksim1@gmail.com>
2025-03-24 18:39:36 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,19 +2,19 @@ from unittest.mock import patch
 from app.main import cryptocurrency_action
 
 
-def test_should_buy_more_when_predicted_rate_more_than_5_percent_higher():
+def test_should_buy_more_when_predicted_rate_more_than_5_percent_higher() -> None:
     with patch("app.main.get_exchange_rate_prediction") as mock_pred:
         mock_pred.return_value = 105.01
         assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
-def test_should_sell_all_when_predicted_rate_more_than_5_percent_lower():
+def test_should_sell_all_when_predicted_rate_more_than_5_percent_lower() -> None:
     with patch("app.main.get_exchange_rate_prediction") as mock_pred:
         mock_pred.return_value = 94.99
         assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
 
 
-def test_should_do_nothing_when_predicted_rate_within_5_percent():
+def test_should_do_nothing_when_predicted_rate_within_5_percent() -> None:
     test_cases = [105, 95, 104.999, 95.001, 102, 98]
     for rate in test_cases:
         with patch("app.main.get_exchange_rate_prediction") as mock_pred:
@@ -22,7 +22,7 @@ def test_should_do_nothing_when_predicted_rate_within_5_percent():
             assert cryptocurrency_action(100) == "Do nothing"
 
 
-def test_exact_5_percent_changes():
+def test_exact_5_percent_changes() -> None:
     with patch("app.main.get_exchange_rate_prediction") as mock_pred:
         mock_pred.return_value = 105
         assert cryptocurrency_action(100) == "Do nothing"
