28e50fd306b11a2934cc64ee88d79f21c7bef12c
Dmytro Lisovyi <kenedy11@ukr.net>
2025-03-24 22:50:53 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -9,17 +9,57 @@ def mocked_get_exchange_rate_prediction() -> None:
         yield mock_test
 
 
-def test_cryptocurrency_action(
+def test_cryptocurrency_action_buy_more(
         mocked_get_exchange_rate_prediction: int | float
 ) -> None:
-    mocked_get_exchange_rate_prediction.return_value = 0.9
-    result = cryptocurrency_action(1.06)
-    assert result == "Sell all your cryptocurrency "
 
-    mocked_get_exchange_rate_prediction.return_value = 1.9
-    result = cryptocurrency_action(1.11)
+    mocked_get_exchange_rate_prediction.return_value = 1.1
+    result = cryptocurrency_action(1)
+    assert result == "Buy more cryptocurrency"
+
+
+def test_cryptocurrency_action_sell_all(
+        mocked_get_exchange_rate_prediction: int | float
+) -> None:
+
+    mocked_get_exchange_rate_prediction.return_value = 0.9
+    result = cryptocurrency_action(1)
+    assert result == "Sell all your cryptocurrency"
+
+
+def test_cryptocurrency_action_do_nothing(
+        mocked_get_exchange_rate_prediction: int | float
+) -> None:
+
+    mocked_get_exchange_rate_prediction.return_value = 1.03
+    result = cryptocurrency_action(1)
+    assert result == "Do nothing"
+
+    mocked_get_exchange_rate_prediction.return_value = 0.97
+    result = cryptocurrency_action(1)
+    assert result == "Do nothing"
+
+    mocked_get_exchange_rate_prediction.return_value = 1
+    result = cryptocurrency_action(1)
+    assert result == "Do nothing"
+
+
+def test_cryptocurrency_action_boundary_conditions(
+        mocked_get_exchange_rate_prediction: int | float
+) -> None:
+
+    mocked_get_exchange_rate_prediction.return_value = 1.05
+    result = cryptocurrency_action(1)
+    assert result == "Do nothing"
+
+    mocked_get_exchange_rate_prediction.return_value = 0.95  #
+    result = cryptocurrency_action(1)
+    assert result == "Do nothing"
+
+    mocked_get_exchange_rate_prediction.return_value = 1.06
+    result = cryptocurrency_action(1)
     assert result == "Buy more cryptocurrency"
 
     mocked_get_exchange_rate_prediction.return_value = 0.94
     result = cryptocurrency_action(1)
-    assert result == "Do nothing"
+    assert result == "Sell all your cryptocurrency"
