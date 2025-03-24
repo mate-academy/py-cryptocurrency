efcd4b6a0e7104990b13502d31473bd455d5db53
Dmytro Lisovyi <kenedy11@ukr.net>
2025-03-24 22:18:23 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,18 +7,18 @@ def mocked_get_exchange_rate_prediction() -> None:
     with mock.patch("app.main.get_exchange_rate_prediction") as mock_test:
         yield mock_test
 
-def test_cryptocurrency_action(mocked_get_exchange_rate_prediction: int | float) -> None:
-    mocked_get_exchange_rate_prediction.return_value = 0.9
+def test_cryptocurrency_action(mocked_get_exchange_rate_prediction) -> None:
+    mocked_get_exchange_rate_prediction.return_value = 1.03
     result = cryptocurrency_action(1.06)
     assert result == "Do nothing"
 
-    mocked_get_exchange_rate_prediction.return_value = 1.9
+    mocked_get_exchange_rate_prediction.return_value = 2
     result = cryptocurrency_action(1.11)
     assert result == "Buy more cryptocurrency"
 
-    mocked_get_exchange_rate_prediction.return_value = 1
-    result = cryptocurrency_action(1)
-    assert result == "Sell all your cryptocurrency "
+    mocked_get_exchange_rate_prediction.return_value = 3
+    result = cryptocurrency_action(3.2)
+    assert result == "Sell all your cryptocurrency"
 
 
 
