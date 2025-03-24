cf68af60e772dea9a20f19aee875d9fb34cbc1b0
Max <smolinskijmaksim1@gmail.com>
2025-03-24 18:11:01 +0100

    Fix error

--- app/test_main.py
+++ app/test_main.py
@@ -8,13 +8,11 @@ def test_should_buy_more_when_predicted_rate_more_than_5_percent_higher()\
         mock_pred.return_value = 105.01
         assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
-
 def test_should_sell_all_when_predicted_rate_more_than_5_percent_lower()\
         -> None:
     with patch('app.main.get_exchange_rate_prediction') as mock_pred:
         mock_pred.return_value = 94.99
         assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
-
 
 def test_should_do_nothing_when_predicted_rate_within_5_percent() -> None:
     test_cases = [105, 95, 104.999, 95.001, 102, 98]
@@ -22,7 +20,6 @@ def test_should_do_nothing_when_predicted_rate_within_5_percent() -> None:
         with patch('app.main.get_exchange_rate_prediction') as mock_pred:
             mock_pred.return_value = rate
             assert cryptocurrency_action(100) == "Do nothing"
-
 
 def test_exact_5_percent_changes() -> None:
     with patch('app.main.get_exchange_rate_prediction') as mock_pred:
