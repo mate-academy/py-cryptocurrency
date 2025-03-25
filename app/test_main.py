20d128983d6f63f2fbbcc1da3694fdc93a048a5b
Oleksii Churbanov <lyoshatop@gmail.com>
2025-03-25 14:10:25 +0200

    implement tests for py-cryptocurrency

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,10 @@
-# write your code here
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+def test_function_has_been_called() -> None:
+    with (mock.patch("app.main.get_exchange_rate_prediction")
+          as mocked_prediction):
+        mocked_prediction.return_value = 110
+        cryptocurrency_action(100)
+        mocked_prediction.assert_called_with(100)
