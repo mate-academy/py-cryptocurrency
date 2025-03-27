d00eebaed509ee8fb20f38955f2c383d492eee9d
Fallen Hero <failent.hero@gmail.com>
2025-03-27 18:50:18 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -8,7 +8,7 @@ from app.main import cryptocurrency_action
     "rate_prediction,current_rate,message",
     [
         pytest.param(110, 100, "Buy more cryptocurrency", id="110, 100, buy"),
-        pytest.param(90, 100, "Sell all your cryptocurrency", id="90, 100, sell"),
+        pytest.param(90, 100, "Sell all your cryptocurrency", id="sell"),
         pytest.param(100, 104, "Do nothing", id="100, 104, no action"),
         pytest.param(105, 100, "Do nothing", id="105, 100, no action"),
         pytest.param(95, 100, "Do nothing", id="95, 100, no action"),
@@ -18,17 +18,18 @@ from app.main import cryptocurrency_action
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_cryptocurrency_action(
         mock_prediction: Callable,
-        rate_prediction: int|float,
-        current_rate: int|float,
-        message: str):
+        rate_prediction: int | float,
+        current_rate: int | float,
+        message: str) -> None:
 
     mock_prediction.return_value = rate_prediction
 
     assert cryptocurrency_action(current_rate) == message
     mock_prediction.assert_called_once_with(current_rate)
 
+
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_divide_on_zero_error(mock_prediction: Callable):
+def test_divide_on_zero_error(mock_prediction: Callable) -> None:
 
     mock_prediction.return_value = 0
     with pytest.raises(ZeroDivisionError):
