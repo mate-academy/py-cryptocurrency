4a77d42f1f40cce1a4d95d20de9bdb06c9baabd3
Oleksii Churbanov <lyoshatop@gmail.com>
2025-03-31 19:01:51 +0300

    implement tests for py-cryptocurrency

--- app/test_main.py
+++ app/test_main.py
@@ -1,10 +1,20 @@
+import pytest
 from unittest import mock
 from app.main import cryptocurrency_action
 
 
-def test_function_has_been_called() -> None:
-    with (mock.patch("app.main.get_exchange_rate_prediction")
-          as mocked_prediction):
-        mocked_prediction.return_value = 110
-        cryptocurrency_action(100)
-        mocked_prediction.assert_called_with(100)
+@pytest.mark.parametrize(
+    "prediction, current, result",
+    [
+        (110, 100, "Buy more cryptocurrency"),
+        (90, 100, "Sell all your cryptocurrency"),
+        (100, 100, "Do nothing"),
+        (105, 100, "Do nothing"),
+        (95, 100, "Do nothing")
+    ]
+)
+def test_crypto_action(prediction: int | float,
+                       current: int | float,
+                       result: str) -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=prediction):
+        assert cryptocurrency_action(current) == result
