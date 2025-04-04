71dda31b78e15a522daafd2aa0dfa67ad683b701
Farzam <farzamgt@gmail.com>
2025-04-04 12:48:24 +0300

    Fix code by review, remove fixture.

--- app/test_main.py
+++ app/test_main.py
@@ -3,27 +3,24 @@ from unittest import mock
 from app.main import cryptocurrency_action
 
 
-@pytest.fixture
-def mock_get_exchange_rate_prediction() -> mock.Mock:
-    with mock.patch("app.main.get_exchange_rate_prediction") as mock_func:
-        yield mock_func
-
-
 @pytest.mark.parametrize(
-    "current_rate, predicted_rate, expected_action",
+    "current_rate, predicted_rate, expected",
     [
-        (100, 110, "Buy more cryptocurrency"),
-        (100, 90, "Sell all your cryptocurrency"),
-        (100, 104, "Do nothing"),
-        (100, 95, "Do nothing")
+        (100, 106, "Buy more cryptocurrency"),
+        (100, 94, "Sell all your cryptocurrency"),
+        (100, 105, "Do nothing"),
+        (100, 95, "Do nothing"),
+        (100, 100, "Do nothing"),
     ]
 )
-def test_cryptocurrency_action(
-        current_rate: float, predicted_rate: float, expected_action: str,
-        mock_get_exchange_rate_prediction: mock.Mock
+def test_for_cryptocurrency_action(
+        current_rate: int,
+        predicted_rate: int,
+        expected: str
 ) -> None:
-    mock_get_exchange_rate_prediction.return_value = predicted_rate
-    result = cryptocurrency_action(current_rate)
-    assert result == expected_action, (
-        f"Expected action: '{expected_action}', but got: '{result}'"
-    )
+    with mock.patch(
+            "app.main.get_exchange_rate_prediction",
+            return_value=predicted_rate
+    ):
+        result = cryptocurrency_action(current_rate)
+        assert result == expected
