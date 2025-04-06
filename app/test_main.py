4f01dc58ed258eb83037c34553669cb8b8faf086
Marcin <grzechnikmarcin@wp.pl>
2025-04-06 19:56:47 +0200

    My solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,26 @@
-import unittest
-from unittest.mock import patch
+from typing import Union
+import pytest
+from unittest.mock import patch, MagicMock
 from app.main import cryptocurrency_action
 
 
-class TestCryptocurrencyAction(unittest.TestCase):
+@pytest.mark.parametrize(
+    "current_rate,prediction_rate,expected",
+    [
+        (100, 106, "Buy more cryptocurrency"),
+        (100, 94, "Sell all your cryptocurrency"),
+        (100, 100, "Do nothing"),
+        (100, 105, "Do nothing"),
+        (100, 95, "Do nothing")
+    ]
+)
+@patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action(
+        mocked_get_exchange_rate_prediction: MagicMock,
+        current_rate: Union[int, float],
+        prediction_rate: float,
+        expected: str
+) -> None:
+    mocked_get_exchange_rate_prediction.return_value = prediction_rate
 
-    @patch("app.main.get_exchange_rate_prediction")
-    def test_buy_more_cryptocurrency(self, mock_prediction: float) -> None:
-        current_rate = 100
-        mock_prediction.return_value = 105.01  # Więcej niż 5% wyższy
-        result = cryptocurrency_action(current_rate)
-        self.assertEqual(result, "Buy more cryptocurrency")
-
-    @patch("app.main.get_exchange_rate_prediction")
-    def test_sell_all_cryptocurrency(self, mock_prediction: float) -> None:
-        current_rate = 100
-        mock_prediction.return_value = 94.99  # Więcej niż 5% niższy
-        result = cryptocurrency_action(current_rate)
-        self.assertEqual(result, "Sell all your cryptocurrency")
-
-    @patch("app.main.get_exchange_rate_prediction")
-    def test_do_nothing(self, mock_prediction: float) -> None:
-        current_rate = 100
-        mock_prediction.return_value = 103  # W zakresie 5%
-        result = cryptocurrency_action(current_rate)
-        self.assertEqual(result, "Do nothing")
-
-
-if __name__ == "__main__":
-    unittest.main()
+    assert cryptocurrency_action(current_rate) == expected
