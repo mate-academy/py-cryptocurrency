e7e58a990b292e2baf89b24f3d007e9159f1ba14
Oleksandr Lytvyn <litvinalex632@gmail.com>
2025-04-15 15:30:25 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,11 +4,11 @@ from unittest import mock
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_rate_95_do_nothing(mock_prediction: any) -> None:
-    mock_prediction.return_value = 14285.71
-    assert cryptocurrency_action(15000) == "Do nothing"
+    mock_prediction.return_value = 15000
+    assert cryptocurrency_action(14285.71) == "Do nothing"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test_rate_105_do_nothing(mock_prediction: any) -> None:
-    mock_prediction.return_value = 190.48
-    assert cryptocurrency_action(200) == "Do nothing"
+    mock_prediction.return_value = 200
+    assert cryptocurrency_action(190.48) == "Do nothing"
