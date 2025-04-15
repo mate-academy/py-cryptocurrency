1a12568a702ee45aec016d098df1bceb9ce567af
Alexandr <oleksandrglushen2005@gmail.com>
2025-04-15 10:52:51 +0300

    fixed code 2

--- app/test_main.py
+++ app/test_main.py
@@ -7,8 +7,10 @@ from unittest import mock
     "current_rate, predicted_rate, expected",
     [
         (100, 106, "Buy more cryptocurrency"),
+        (100, 105, "Do nothing"),
+        (100, 95, "Do nothing"),
         (100, 94, "Sell all your cryptocurrency"),
-        (100, 103, "Do nothing")
+        (100, 103, "Do nothing"),
     ]
 )
 @mock.patch("app.main.get_exchange_rate_prediction")
@@ -16,6 +18,6 @@ def test_all_crypto_situation(mock_prediction: int, current_rate: int,
                               predicted_rate: int, expected: int) -> None:
     mock_prediction.return_value = predicted_rate
 
-    result = cryptocurrency_action(100)
+    result = cryptocurrency_action(current_rate)
 
     assert result == expected
