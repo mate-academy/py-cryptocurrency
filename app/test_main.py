32756d292830777296568fe410bf21a5d4074076
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-29 01:07:55 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -6,7 +6,7 @@ from unittest import mock
 
 
 # Сильне зростання: prediction / current_rate > 1.05
-@patch("app.main.random.random", return_value=0.1)
+@patch("app.main.random.random", return_value=1.05)
 @patch("app.main.random.choice", return_value="increase")
 def test_buy_more(mock_choice: Mock, mock_random: Mock) -> None:
     result = cryptocurrency_action(100)
@@ -16,7 +16,7 @@ def test_buy_more(mock_choice: Mock, mock_random: Mock) -> None:
 
 
 # Сильне падіння: prediction / current_rate < 0.95
-@mock.patch("app.main.random.random", return_value=0.1)
+@mock.patch("app.main.random.random", return_value=0.95)
 @mock.patch("app.main.random.choice", return_value="decrease")
 def test_sell_all(mock_choice: Mock, mock_random: Mock) -> None:
     result = cryptocurrency_action(100)
