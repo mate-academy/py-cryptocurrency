309ed1a1fcc1129e1b3845e531d769bacdde6ebd
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-29 01:19:02 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -26,8 +26,8 @@ def test_sell_all(mock_choice: Mock, mock_random: Mock) -> None:
 
 
 # Невелика зміна курсу: між -5% і +5%
-@mock.patch("app.main.random.random", return_value=0.95)
-@mock.patch("app.main.random.choice", return_value="decrease")
+@mock.patch("app.main.random.random", return_value=1.05)
+@mock.patch("app.main.random.choice", return_value="increase")
 def test_do_nothing(mock_choice: Mock, mock_random: Mock) -> None:
     result = cryptocurrency_action(100)
     assert result == "Do nothing"
