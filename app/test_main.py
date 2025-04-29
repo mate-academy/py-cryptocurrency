d472072d202a932a0e9b0044a0030c3e82f04a35
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-29 02:07:03 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -2,8 +2,6 @@ from app.main import cryptocurrency_action
 
 from unittest.mock import patch, Mock
 
-from unittest import mock
-
 
 # Сильне зростання: prediction / current_rate > 1.05
 @patch("app.main.random.random", return_value=0.2)
@@ -16,8 +14,8 @@ def test_buy_more(mock_choice: Mock, mock_random: Mock) -> None:
 
 
 # Сильне падіння: prediction / current_rate < 0.95
-@mock.patch("app.main.random.random", return_value=0.9)
-@mock.patch("app.main.random.choice", return_value="decrease")
+@patch("app.main.random.random", return_value=0.9)
+@patch("app.main.random.choice", return_value="decrease")
 def test_sell_all(mock_choice: Mock, mock_random: Mock) -> None:
     result = cryptocurrency_action(100)
     assert result == "Sell all your cryptocurrency"
@@ -26,8 +24,8 @@ def test_sell_all(mock_choice: Mock, mock_random: Mock) -> None:
 
 
 # Невелика зміна курсу: між -5% і +5%
-@mock.patch("app.main.random.random", return_value=0.95)
-@mock.patch("app.main.random.choice", return_value="decrease")
+@patch("app.main.random.random", return_value=0.98)
+@patch("app.main.random.choice", return_value="decrease")
 def test_rate_95_percent_do_nothing(mock_choice: Mock,
                                     mock_random: Mock) -> None:
     result = cryptocurrency_action(100)
@@ -36,8 +34,8 @@ def test_rate_95_percent_do_nothing(mock_choice: Mock,
     mock_random.assert_called_once()
 
 
-@mock.patch("app.main.random.random", return_value=1.05)
-@mock.patch("app.main.random.choice", return_value="increase")
+@patch("app.main.random.random", return_value=1.05)
+@patch("app.main.random.choice", return_value="increase")
 def test_rate_105_percent_do_nothing(mock_choice: Mock,
                                      mock_random: Mock) -> None:
     result = cryptocurrency_action(100)
