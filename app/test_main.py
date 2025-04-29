8430e1e1b43ba1724158946f7cfb9f4eeb780d4d
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-29 02:34:51 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -24,7 +24,7 @@ def test_sell_all(mock_choice: Mock, mock_random: Mock) -> None:
 
 
 # Невелика зміна курсу: між -5% і +5%
-@patch("app.main.random.random", return_value=0.95)
+@patch("app.main.random.random", return_value=0.951)
 @patch("app.main.random.choice", return_value="decrease")
 def test_rate_95_percent_do_nothing(mock_choice: Mock,
                                     mock_random: Mock) -> None:
@@ -34,7 +34,7 @@ def test_rate_95_percent_do_nothing(mock_choice: Mock,
     mock_random.assert_called_once()
 
 
-@patch("app.main.random.random", return_value=1.05)
+@patch("app.main.random.random", return_value=1.049)
 @patch("app.main.random.choice", return_value="increase")
 def test_rate_105_percent_do_nothing(mock_choice: Mock,
                                      mock_random: Mock) -> None:
