26ac0a0c6159653deb68131cd2e1c74c040d235f
Nazariy Vesklyarov <nazariy.vesklyarov@gmail.com>
2025-05-21 11:10:22 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_buy_more_cryptocurrency(mock_prediction: object) -> None:
+    mock_prediction.return_value = 105.1
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_sell_all_cryptocurrency(mock_prediction: object) -> None:
+    mock_prediction.return_value = 94.9
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing_upper_boundary(mock_prediction: object) -> None:
+    mock_prediction.return_value = 105.0
+    assert cryptocurrency_action(100) == "Do nothing"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing_lower_boundary(mock_prediction: object) -> None:
+    mock_prediction.return_value = 95.0
+    assert cryptocurrency_action(100) == "Do nothing"
