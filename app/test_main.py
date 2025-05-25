90b0d7dd82497372451d0d97ace961253009e2e8
Vitalii Prushak <v.prushak@gmail.com>
2025-05-25 22:26:40 +0300

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -5,9 +5,16 @@ from app.main import cryptocurrency_action
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action_do_nothing(mocked_prediction: float) -> None:
-    mocked_prediction.return_value = 2.0
+def test_cryptocurrency_action_upper_line_do_nothing(mocked_prediction: float) -> None:
+    mocked_prediction.return_value = 2.1
     result = cryptocurrency_action(2)
+    assert result == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_lower_line_do_nothing(mocked_prediction: float) -> None:
+    mocked_prediction.return_value = 0.95
+    result = cryptocurrency_action(1)
     assert result == "Do nothing"
 
 
