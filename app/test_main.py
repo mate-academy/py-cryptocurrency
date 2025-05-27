87d281071aecd8383c55cf17c9183118871253c2
Antonov Andrew <prestigemilitary@gmail.com>
2025-05-27 17:29:06 +0300

    Don't understand mistake

--- app/test_main.py
+++ app/test_main.py
@@ -7,13 +7,12 @@ from main import cryptocurrency_action
 def test_cryptocurrency_action(
         mock_get_exchange_rate_prediction: mock.MagicMock,
 ) -> None:
-    current_rate = 1
+    rate = 1
     mock_get_exchange_rate_prediction.return_value = 0.95
-    assert cryptocurrency_action(current_rate) == "Do nothing"
+    assert cryptocurrency_action(rate) == "Do nothing"
     mock_get_exchange_rate_prediction.return_value = 1.05
-    assert cryptocurrency_action(current_rate) == "Do nothing"
+    assert cryptocurrency_action(rate) == "Do nothing"
     mock_get_exchange_rate_prediction.return_value = 1.06
-    assert cryptocurrency_action(current_rate) == "Buy more cryptocurrency"
+    assert cryptocurrency_action(rate) == "Buy more cryptocurrency"
     mock_get_exchange_rate_prediction.return_value = 0.94
-    assert (cryptocurrency_action(current_rate) ==
-            "Sell all your cryptocurrency")
+    assert (cryptocurrency_action(rate) == "Sell all your cryptocurrency")
