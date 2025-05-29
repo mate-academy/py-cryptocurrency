c64ec3b4609480a7f122cec3a09f962d225e22ea
Anastasiia Matsebon <anastasiiamatsebon@gmail.com>
2025-05-29 20:46:54 +0200

    task

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_buy_more_cryptocurrency(mock_prediction: int) -> None:
+    mock_prediction.return_value = 110  # > 5% increase from 100
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_sell_all_cryptocurrency(mock_prediction: int) -> None:
+    mock_prediction.return_value = 90  # > 5% decrease from 100
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing_when_small_increase(mock_prediction: int) -> None:
+    mock_prediction.return_value = 104.9  # < 5% increase from 100
+    assert cryptocurrency_action(100) == "Do nothing"
+
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing_when_small_decrease(mock_prediction: int) -> None:
+    mock_prediction.return_value = 95.1  # > 95% of 100
+    assert cryptocurrency_action(100) == "Do nothing"
