07de829e5bb38cc64bc20aa40229268a9dfb61da
nkkmkk <nikitmkk@gmail.com>
2025-06-06 16:21:37 +0200

    test added

--- app/test_main.py
+++ app/test_main.py
@@ -6,18 +6,19 @@ from app.main import cryptocurrency_action
 
 
 @pytest.mark.parametrize(
-    "exchange_rate, expected",
+    "exchange_rate, current_rate, expected",
     [
-        (110, "Buy more cryptocurrency"),
-        (94.5, "Sell all your cryptocurrency"),
-        (105, "Do nothing")
+        (0.95, 1, "Do nothing"),
+        (1.06, 1, "Buy more cryptocurrency"),
+        (0.90, 1, "Sell all your cryptocurrency")
     ]
 )
 def test_cryptocurrency_action(
-        exchange_rate : Union[int, float],
+        exchange_rate: Union[int, float],
+        current_rate: Union[int, float],
         expected: str
 ) -> None:
     with mock.patch("app.main.get_exchange_rate_prediction") \
             as mocked_get_exchange:
         mocked_get_exchange.return_value = exchange_rate
-        assert cryptocurrency_action(current_rate=100) == expected
+        assert cryptocurrency_action(current_rate) == expected
