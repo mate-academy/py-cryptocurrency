1d349213658ce7381b737304dd3821b3fa8cc7e7
Ihor Pershin <issue.07.08.09@gmail.com>
2025-06-10 22:19:56 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,27 +1,41 @@
 from typing import Union
 import pytest
+from unittest.mock import patch
 from app.main import cryptocurrency_action
 
 
 @pytest.mark.parametrize(
     "current_rate, predicted_rate, expected",
     [
-        (100, 105.00, "Do nothing"),
         (100, 105.01, "Buy more cryptocurrency"),
-        (100, 95.00, "Do nothing"),
-        (100, 94.99, "Sell all your cryptocurrency")
+        (100, 94.99, "Sell all your cryptocurrency"),
+        (100, 104.99, "Do nothing"),
+        (100, 95.01, "Do nothing"),
     ]
 )
 def test_cryptocurrency_action(
-        mocker: pytest.MockFixture,
         current_rate: Union[int, float],
         predicted_rate: Union[int, float],
         expected: str
 ) -> None:
-    mocker.patch(
-        "app.main.get_exchange_rate_prediction",
-        return_value=predicted_rate
-    )
+    with patch(
+            "app.main.get_exchange_rate_prediction",
+            return_value=predicted_rate
+    ):
+        assert cryptocurrency_action(current_rate) == expected
 
-    result = cryptocurrency_action(current_rate)
-    assert result == expected
+
+def test_do_nothing_on_exactly_5_percent_increase() -> None:
+    with patch(
+            "app.main.get_exchange_rate_prediction",
+            return_value=105.0
+    ):
+        assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_do_nothing_on_exactly_5_percent_decrease() -> None:
+    with patch(
+            "app.main.get_exchange_rate_prediction",
+            return_value=95.0
+    ):
+        assert cryptocurrency_action(100) == "Do nothing"
