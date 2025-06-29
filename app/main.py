25033a26e654741f1c02915e5bcd3dbcfed74f05
Maksym Buslenko <maksbusl@gmail.com>
2025-06-29 22:41:44 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -15,3 +15,16 @@ def cryptocurrency_action(current_rate: Union[int, float]) -> str:
     if prediction_rate / current_rate < 0.95:
         return "Sell all your cryptocurrency"
     return "Do nothing"
+
+
+
+
+# print(cryptocurrency_action(8))
+#7.16
+#sell all
+# print(cryptocurrency_action(2))
+# 11.21
+# buy more
+# print(cryptocurrency_action(0.7))
+# 0.73
+# Do nothing
