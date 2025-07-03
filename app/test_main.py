3b5d3817f9ac3817ff37f8f3d929290cabfe0cd5
Tamerlan <egtamerlan@gmail.com>
2025-07-03 23:40:21 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+import pytest
+from unittest import mock
+from .main import cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    "current_rate,exception",
+    [
+        (106, "Buy more cryptocurrency"),
+        (94, "Sell all your cryptocurrency"),
+        (105, "Do nothing"),
+        (95, "Do nothing"),
+        (101, "Do nothing")
+    ]
+)
+
+def test_cryptocurrency(
+        current_rate: int | float,
+        exception: str
+) -> None:
+    with mock.patch(
+        "app.main.get_exchange_rate_prediction",
+        return_value=current_rate
+):
+        result = cryptocurrency_action(100)
+        assert result == exception
