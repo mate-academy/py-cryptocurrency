c052663c84dcae85c74e51777a8938da29a5e7f4
Serhii Basok <sergeybsk19@gmail.com>
2025-07-11 21:22:49 +0300

    create cryptocurrency tests for buy and sell

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,39 @@
-# write your code here
+import pytest
+from app.main import cryptocurrency_action
+
+
+@pytest.mark.parametrize(
+    "current_rate, mock_rate, result",
+    [
+        (100.0, 105.01, "Buy more cryptocurrency"),
+        (105.01, 99.75, "Sell all your cryptocurrency"),
+        (100.1, 100.1, "Do nothing")
+    ],
+    ids=[
+        "buy",
+        "sell",
+        "not buy and sell"
+    ]
+)
+def test_for_cryptocurrency_action(current_rate: int | float,
+                                   mock_rate: int | float,
+                                   result: str,
+                                   monkeypatch: pytest.MonkeyPatch
+                                   ) -> None:
+    def mock_get_exchange(*args, **kwargs) -> int:
+        return mock_rate
+    (monkeypatch.setattr
+     ("app.main.get_exchange_rate_prediction", mock_get_exchange))
+    assert cryptocurrency_action(current_rate) == result
+
+
+"""Also method with mock.patch"""
+# from unittest import mock
+# def test_for_cryptocurrency_action(current_rate: int | float,
+#                                    mock_rate: int | float,
+#                                    result: str
+#                                    ) -> None:
+#     with (mock.patch("app.main.get_exchange_rate_prediction")
+#           as mock_prediction):
+#         mock_prediction.return_value = mock_rate
+#         assert cryptocurrency_action(current_rate) == result
