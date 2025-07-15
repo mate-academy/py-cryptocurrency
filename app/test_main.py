dc1f0d504f4fd8bc8fa1720cda0c3b5ea073c63b
Лілія <liliia@MacBook-Air-Lilia.local>
2025-07-15 19:28:02 +0300

    fixed flake8

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,8 @@
 import pytest
 from app.main import cryptocurrency_action
 from unittest import mock
+from typing import Union
+
 
 @pytest.mark.parametrize(
     "rate_prediction, current_rate, expected_result", [
@@ -11,9 +13,13 @@ from unittest import mock
         (57, 60, "Do nothing")
     ]
 )
-
-def test_cryptocurrency_action(rate_prediction, current_rate, expected_result) -> None:
-    with mock.patch("app.main.get_exchange_rate_prediction", return_value=rate_prediction):
-        assert (
-            cryptocurrency_action(current_rate) == expected_result
-        )
+def test_cryptocurrency_action(
+        rate_prediction: Union[int, float],
+        current_rate: Union[int, float],
+        expected_result: str
+) -> None:
+    with mock.patch(
+            "app.main.get_exchange_rate_prediction",
+            return_value=rate_prediction
+    ):
+        assert (cryptocurrency_action(current_rate) == expected_result)
