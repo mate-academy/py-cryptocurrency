9ef73fc83ac982226c0909887ee6cda9c6c7ecef
Nik <your@mail.com>
2025-07-16 20:04:07 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.main import cryptocurrency_action
+from unittest import mock
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_low_rating(mock_function: int) -> None:
+    mock_function.return_value = 94.9
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_high_rating(mock_function: int) -> None:
+    mock_function.return_value = 105.1
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_not_difference_rating(mock_function: int) -> None:
+    mock_function.return_value = 95.1
+    assert cryptocurrency_action(100) == "Do nothing"
