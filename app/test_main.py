c3fafad831deb3659b00764ec49cabfd510d1434
Anna Marchenko <marchenkoanna0464@gmail.com>
2025-07-17 23:48:23 +0200

    all

--- app/test_main.py
+++ app/test_main.py
@@ -3,15 +3,20 @@ from unittest.mock import patch
 
 
 def test_exchange_rate_more_than_5() -> None:
-    with patch("app.main.get_exchange_rate_prediction", return_value=15):
-        assert cryptocurrency_action(1) == "Buy more cryptocurrency"
+    with patch("app.main.get_exchange_rate_prediction", return_value=2.2):
+        assert cryptocurrency_action(2) == "Buy more cryptocurrency"
 
 
 def test_exchange_rate_equal_5() -> None:
-    with patch("app.main.get_exchange_rate_prediction", return_value=1.05):
-        assert cryptocurrency_action(1) == "Do nothing"
+    with patch("app.main.get_exchange_rate_prediction", return_value=1.9):
+        assert cryptocurrency_action(2) == "Do nothing"
 
 
 def test_exchange_rate_less_than_5() -> None:
-    with patch("app.main.get_exchange_rate_prediction", return_value=0.94):
-        assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
+    with patch("app.main.get_exchange_rate_prediction", return_value=1.8):
+        assert cryptocurrency_action(2) == "Sell all your cryptocurrency"
+
+
+def test_equal_lover() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=2.1):
+        assert cryptocurrency_action(2) == "Do nothing"
