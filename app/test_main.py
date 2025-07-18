b7c899dab70d9369d6e53fbac87a65fb00a39f24
Dmytro <DmytroHakh@gmail.com>
2025-07-18 18:53:53 +0300

    solution 2.0

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,5 @@
-from pytest import mark
 from unittest import mock
+from unittest.mock import MagicMock
 
 import pytest
 
@@ -7,21 +7,27 @@ from app.main import cryptocurrency_action
 
 
 @pytest.fixture()
-def mock_get_exchange_rate_prediction():
-    with mock.patch("app.main.get_exchange_rate_prediction") as mocked_prediction:
-        yield mocked_prediction
+def mock_get_exchange_rate_prediction() -> MagicMock:
+    with mock.patch("app.main.get_exchange_rate_prediction") as mocked_pred:
+        yield mocked_pred
 
 
-def test_buy_cryptocurrency_action(mock_get_exchange_rate_prediction) -> None:
+def test_buy_cryptocurrency_action(
+        mock_get_exchange_rate_prediction: MagicMock
+) -> None:
     mock_get_exchange_rate_prediction.return_value = 10
     assert cryptocurrency_action(1) == "Buy more cryptocurrency"
 
 
-def test_sell_cryptocurrency_action(mock_get_exchange_rate_prediction) -> None:
+def test_sell_cryptocurrency_action(
+        mock_get_exchange_rate_prediction: MagicMock
+) -> None:
     mock_get_exchange_rate_prediction.return_value = 1
     assert cryptocurrency_action(2) == "Sell all your cryptocurrency"
 
 
-def test_do_nothing_cryptocurrency_action(mock_get_exchange_rate_prediction) -> None:
+def test_do_nothing_cryptocurrency_action(
+        mock_get_exchange_rate_prediction: MagicMock
+) -> None:
     mock_get_exchange_rate_prediction.return_value = 1
     assert cryptocurrency_action(1) == "Do nothing"
