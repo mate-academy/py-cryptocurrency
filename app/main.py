d9de5a305210474d06ed081303de8f089c274d3a
Jakub <jakubmichalski89@fgmail.com>
2025-07-25 13:48:12 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import random
 from typing import Union
 
+
 def get_exchange_rate_prediction(exchange_rate: Union[int, float]) -> float:
     if random.choice(["increase", "decrease"]) == "increase":
         return round(exchange_rate / random.random(), 2)
     return round(exchange_rate * random.random(), 2)
+
 
 def cryptocurrency_action(current_rate: Union[int, float]) -> str:
     prediction_rate = get_exchange_rate_prediction(current_rate)
