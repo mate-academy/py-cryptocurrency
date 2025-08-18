c787eaba6b81312ec205e7e0cdb12109e5e2ac6b
Tomasz Herman <tomekh2015@gmail.com>
2025-08-18 15:29:16 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,15 +3,17 @@ from typing import Union
 
 
 def get_exchange_rate_prediction(exchange_rate: Union[int, float]) -> float:
-    if random.choice(["increase", "decrease"]) == "increase":
-        return round(exchange_rate / random.random(), 2)
-    return round(exchange_rate * random.random(), 2)
+    change_percent = random.uniform(-0.1, 0.1)
+    predicted_rate = exchange_rate * (1 + change_percent)
+    return round(predicted_rate, 2)
 
 
 def cryptocurrency_action(current_rate: Union[int, float]) -> str:
     prediction_rate = get_exchange_rate_prediction(current_rate)
-    if prediction_rate / current_rate > 1.05:
+    ratio = prediction_rate / current_rate
+
+    if ratio > 1.05:
         return "Buy more cryptocurrency"
-    if prediction_rate / current_rate < 0.95:
+    if ratio < 0.95:
         return "Sell all your cryptocurrency"
     return "Do nothing"
--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+from unittest.mock import patch
+from app.main import cryptocurrency_action
+
+
+def test_ratio_95_do_nothing() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=95):
+        assert cryptocurrency_action(100) == "Do nothing", (
+            "You should not sell cryptocurrency"
+            " when prediction_rate / current_rate == 0.95"
+        )
+
+
+def test_ratio_105_do_nothing() -> None:
+    with patch("app.main.get_exchange_rate_prediction", return_value=105):
+        assert cryptocurrency_action(100) == "Do nothing", (
+            "You should not buy cryptocurrency "
+            "when prediction_rate / current_rate == 1.05"
+        )
