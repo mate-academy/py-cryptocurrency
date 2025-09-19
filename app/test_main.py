1325b50f21e2d7091d223bcd8e236d109fefb62f
Gena <gena.anzin@ukr.net>
2025-09-19 16:06:21 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -3,19 +3,22 @@ from app.main import cryptocurrency_action
 from unitest import mock
 
 
-def test_cryptocurrency_action_sell_all_cryptocurrency():
-    with mock.patch("app.main.get_exchange_rate_prediction") as mock_prediction:
+def test_cryptocurrency_action_sell_all_cryptocurrency() -> None:
+    with (mock.patch("app.main.get_exchange_rate_prediction")
+          as mock_prediction):
         mock_prediction.return_value = 94
         assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
 
 
-def test_cryptocurrency_action_buy_cryptocurrency():
-    with mock.patch("app.main.get_exchange_rate_prediction") as mock_prediction:
+def test_cryptocurrency_action_buy_cryptocurrency() -> None:
+    with (mock.patch("app.main.get_exchange_rate_prediction")
+          as mock_prediction):
         mock_prediction.return_value = 106
         assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
-def test_cryptocurrency_action_nothing_cryptocurrency():
-    with mock.patch("app.main.get_exchange_rate_prediction") as mock_prediction:
+def test_cryptocurrency_action_nothing_cryptocurrency() -> None:
+    with (mock.patch("app.main.get_exchange_rate_prediction")
+          as mock_prediction):
         mock_prediction.return_value = 101
         assert cryptocurrency_action(100) == "Do nothing"
