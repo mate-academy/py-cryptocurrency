0c5fe80329ee78bf1ebfcc736947502f8560086b
0lex1y <kotov.oleksiy@icloud.com>
2025-09-19 21:35:08 +0300

    Fix

--- app/test_main.py
+++ app/test_main.py
@@ -7,6 +7,8 @@ from app.main import cryptocurrency_action
     (100, 106, "Buy more cryptocurrency"),
     (100, 94, "Sell all your cryptocurrency"),
     (100, 102, "Do nothing"),
+    (100, 105, "Do nothing"),
+    (100, 95, "Do nothing"),
 ])
 @patch("app.main.get_exchange_rate_prediction")
 def test_cryptocurrency_action(mock_get_exchange_rate_prediction: Mock,
