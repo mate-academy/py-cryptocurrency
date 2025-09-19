c068c589dedc035fc729874459d6cac17afb5752
Gena <gena.anzin@ukr.net>
2025-09-19 16:17:40 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -22,3 +22,19 @@ def test_cryptocurrency_action_nothing_cryptocurrency() -> None:
           as mock_prediction):
         mock_prediction.return_value = 101
         assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_cryptocurrency_action_extreme_case_all_cryptocurrency()\
+        -> None:
+    with (mock.patch("app.main.get_exchange_rate_prediction")
+          as mock_prediction):
+        mock_prediction.return_value = 95
+        assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_cryptocurrency_action_extreme_big_case_all_cryptocurrency()\
+        -> None:
+    with (mock.patch("app.main.get_exchange_rate_prediction")
+          as mock_prediction):
+        mock_prediction.return_value = 105
+        assert cryptocurrency_action(100) == "Do nothing"
