1aabc2e1764e1e0f19cf87e44a613d4378507949
Gabriel <gfraycastilho@gmail.com>
2025-10-02 00:16:34 -0300

    solved

--- app/test_main.py
+++ app/test_main.py
@@ -4,18 +4,18 @@ from app.main import cryptocurrency_action
 
 
 def test_buy_coins():
-    with patch("app.main.get_exchange_rate_prediction", return_value=110):
+    with patch("app.main.get_exchange_rate_prediction", return_value=106):
         result = cryptocurrency_action(100)
         assert result == "Buy more cryptocurrency"
 
 
 def test_sell_all_cryptocurrency():
-    with patch("app.main.get_exchange_rate_prediction", return_value=90):
+    with patch("app.main.get_exchange_rate_prediction", return_value=104):
         result = cryptocurrency_action(100)
         assert result == "Sell all your cryptocurrency"
 
 
-@pytest.mark.parametrize("predicted", [104, 96, 100])  # dentro do limite
+@pytest.mark.parametrize("predicted", [105, 96, 100, 105])  # dentro do limite
 def test_do_nothing(predicted):
     with patch("app.main.get_exchange_rate_prediction", return_value=predicted):
         result = cryptocurrency_action(100)
