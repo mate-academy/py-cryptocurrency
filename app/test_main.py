70ee8a77dd0b81fb2e407127e4c7b832afe8ae00
Kacper Kogut <kogutkacper7@gmail.com>
2025-10-13 18:35:28 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,9 +1,10 @@
 from unittest import mock
+from unittest.mock import MagicMock
 from app.main import cryptocurrency_action
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action_over_1_05(mocked_prediction):
+def test_cryptocurrency_action_over_1_05(mocked_prediction: MagicMock) -> None:
     mocked_prediction.return_value = 2.11
 
     assert cryptocurrency_action(2) == "Buy more cryptocurrency"
@@ -12,7 +13,9 @@ def test_cryptocurrency_action_over_1_05(mocked_prediction):
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action_under_0_95(mocked_prediction):
+def test_cryptocurrency_action_under_0_95(
+        mocked_prediction: MagicMock
+) -> None:
     mocked_prediction.return_value = 1.89
 
     assert cryptocurrency_action(2) == "Sell all your cryptocurrency"
@@ -21,7 +24,9 @@ def test_cryptocurrency_action_under_0_95(mocked_prediction):
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action_between_0_95_and_1_05(mocked_prediction):
+def test_cryptocurrency_action_between_0_95_and_1_05(
+        mocked_prediction: MagicMock
+) -> None:
     mocked_prediction.return_value = 2.02
 
     assert cryptocurrency_action(2) == "Do nothing"
@@ -30,7 +35,9 @@ def test_cryptocurrency_action_between_0_95_and_1_05(mocked_prediction):
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action_equal_1_05(mocked_prediction):
+def test_cryptocurrency_action_equal_1_05(
+        mocked_prediction: MagicMock
+) -> None:
     mocked_prediction.return_value = 2.10
 
     assert cryptocurrency_action(2) == "Do nothing"
@@ -39,10 +46,11 @@ def test_cryptocurrency_action_equal_1_05(mocked_prediction):
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action_equal_0_95(mocked_prediction):
+def test_cryptocurrency_action_equal_0_95(
+        mocked_prediction: MagicMock
+) -> None:
     mocked_prediction.return_value = 1.90
 
     assert cryptocurrency_action(2) == "Do nothing"
 
     mocked_prediction.assert_called_once_with(2)
-
