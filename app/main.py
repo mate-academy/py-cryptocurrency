5d1f1a13060ee6f9e594eb85cb8e0dc94614bbf8
Ihor Zasimenko <boxletter2011@gmail.com>
2025-10-21 19:35:19 +0300

    and try again

--- app/main.py
+++ app/main.py
@@ -7,11 +7,12 @@ def get_exchange_rate_prediction(exchange_rate: Union[int, float]) -> float:
         return round(exchange_rate / random.random(), 2)
     return round(exchange_rate * random.random(), 2)
 
-
 def cryptocurrency_action(current_rate: Union[int, float]) -> str:
     prediction_rate = get_exchange_rate_prediction(current_rate)
-    if prediction_rate / current_rate > 1.05:
+    rate_ratio = prediction_rate / current_rate
+
+    if rate_ratio > 1.05 + 1e-9:
         return "Buy more cryptocurrency"
-    if prediction_rate / current_rate < 0.95:
+    if rate_ratio < 0.95 - 1e-9:
         return "Sell all your cryptocurrency"
     return "Do nothing"
