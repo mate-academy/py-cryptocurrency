eb01c3fc62ad5925aaa4db07f56ef39c995e7ac7
viloker <yarmolykvadym@gmail.com>
2025-11-05 10:10:11 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -11,16 +11,18 @@ from app.main import cryptocurrency_action
         (6.31, 6, "Buy more cryptocurrency"),
         (4.2, 4, "Do nothing"),
         (2.85, 3, "Do nothing"),
+        (5, 5, "Do nothing"),
+        (3.8, 4, "Do nothing"),
         (7.5, 8, "Sell all your cryptocurrency"),
-        (5, 5, "Do nothing")
+        (-9, 3.3, "Sell all your cryptocurrency"),
+
     ]
 
 )
 def test_cryptocurrency_action(
         prediction_rate: int | float,
         current_rate: int | float,
-        expected: int | float
-) -> None:
+        expected: int | float) -> None:
     with mock.patch("app.main.get_exchange_rate_prediction") as mock_predict:
         mock_predict.return_value = prediction_rate
         assert cryptocurrency_action(current_rate) == expected
