59c2cbefed86c9def7bb6ee550db2645f2c097a8
Vadym Vynokur <king.krollie@gmail.com>
2025-11-08 10:53:32 +0200

    cahange 100 to 1

--- app/test_main.py
+++ app/test_main.py
@@ -7,17 +7,17 @@ from app.main import cryptocurrency_action
 @pytest.mark.parametrize(
     "prediction, expected",
     [
-        (67, "Sell all your cryptocurrency"),
-        (1, "Sell all your cryptocurrency"),
-        (100, "Do nothing"),
-        (101, "Do nothing"),
-        (96, "Do nothing"),
-        (106, "Buy more cryptocurrency"),
-        (222, "Buy more cryptocurrency"),
+        (0.67, "Sell all your cryptocurrency"),
+        (0.94, "Sell all your cryptocurrency"),
+        (0.95, "Do nothing"),
+        (1, "Do nothing"),
+        (1.05, "Do nothing"),
+        (1.06, "Buy more cryptocurrency"),
+        (2, "Buy more cryptocurrency"),
     ]
 )
 def test_cryptocurrency_action(prediction: int, expected: int) -> None:
     with mock.patch("app.main.get_exchange_rate_prediction",
                     return_value=prediction):
-        result = cryptocurrency_action(100)
+        result = cryptocurrency_action(1)
         assert result == expected
