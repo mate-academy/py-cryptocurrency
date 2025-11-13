839f35576a5db285c5dd07f297f07c7f11341870
Mike <tradewcs@gmail.com>
2025-11-13 18:05:32 +0200

    added edge testcases

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,9 @@
+from pytest import MonkeyPatch
+
 from app.main import cryptocurrency_action
 
 
-def test_buy_more(monkeypatch) -> None:
+def test_buy_more(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr(
         "app.main.get_exchange_rate_prediction",
         lambda current_rate: current_rate * 1.06
@@ -10,7 +12,7 @@ def test_buy_more(monkeypatch) -> None:
     assert result == "Buy more cryptocurrency"
 
 
-def test_sell_all(monkeypatch) -> None:
+def test_sell_all(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr(
         "app.main.get_exchange_rate_prediction",
         lambda current_rate: current_rate * 0.94
@@ -19,10 +21,28 @@ def test_sell_all(monkeypatch) -> None:
     assert result == "Sell all your cryptocurrency"
 
 
-def test_do_nothing(monkeypatch) -> None:
+def test_do_nothing(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr(
         "app.main.get_exchange_rate_prediction",
         lambda current_rate: current_rate * 1.03
+    )
+    result = cryptocurrency_action(100)
+    assert result == "Do nothing"
+
+
+def test_do_nothin_when_rate_is_95_percent(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        "app.main.get_exchange_rate_prediction",
+        lambda current_rate: current_rate * 0.95
+    )
+    result = cryptocurrency_action(100)
+    assert result == "Do nothing"
+
+
+def test_do_nothin_when_rate_is_105_percent(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        "app.main.get_exchange_rate_prediction",
+        lambda current_rate: current_rate * 1.05
     )
     result = cryptocurrency_action(100)
     assert result == "Do nothing"
