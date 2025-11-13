521e2870724a60d81effb2f755d466d675807d4e
Yan Tumar <jantumar.unity@gmail.com>
2025-11-13 13:56:51 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,24 +1,25 @@
 import pytest
 from unittest import mock
+from unittest.mock import Mock
 from app.main import cryptocurrency_action
 
-BASE_RATE = 100.0
+base_rate = 100.0
 
 
 @pytest.mark.parametrize(
     "mock_rate, current_rate, expected_action",
     [
-        (110.00, BASE_RATE, "Buy more cryptocurrency"),
-        (90.00, BASE_RATE, "Sell all your cryptocurrency"),
-        (105.00, BASE_RATE, "Do nothing"),
-        (95.00, BASE_RATE, "Do nothing"),
-        (102.00, BASE_RATE, "Do nothing"),
-        (98.00, BASE_RATE, "Do nothing"),
+        (110.00, base_rate, "Buy more cryptocurrency"),
+        (90.00, base_rate, "Sell all your cryptocurrency"),
+        (105.00, base_rate, "Do nothing"),
+        (95.00, base_rate, "Do nothing"),
+        (102.00, base_rate, "Do nothing"),
+        (98.00, base_rate, "Do nothing"),
     ],
 )
-@mock.patch('app.main.get_exchange_rate_prediction')
+@mock.patch("app.main.get_exchange_rate_prediction")
 def test_cryptocurrency_action_scenarios(
-        mock_get_prediction,
+        mock_get_prediction: Mock,
         mock_rate: float,
         current_rate: float,
         expected_action: str
