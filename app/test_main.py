2d905cad33d8f49ecb191c7e190f8284b98972bb
Anastasiia Duzhynska <a.duzhynska@gmail.com>
2025-11-13 18:14:01 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+import pytest
+from app import main
+
+
+def test_high_rate_prediction(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 1.06)
+    expected = "Buy more cryptocurrency"
+    assert main.cryptocurrency_action(1.0) == expected
+
+
+def test_low_rate_prediction(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 0.94)
+    expected = "Sell all your cryptocurrency"
+    assert main.cryptocurrency_action(1.0) == expected
+
+
+def test_no_difference_rate_prediction(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 1.02)
+    expected = "Do nothing"
+    assert main.cryptocurrency_action(1.0) == expected
+
