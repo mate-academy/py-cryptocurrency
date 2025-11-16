b88bf2afbb9d5d6be100e9f198f8bd26da89765d
Tarasov Maksym <maksimmm45555@gmail.com>
2025-11-16 19:30:53 +0200

    solved py-cryptocurrency task

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,15 @@
-# write your code here
+from unittest.mock import patch
+
+
+@patch("app.main.get_exchange_rate_prediction")
+@patch("app.main.cryptocurrency_action")
+def test_get_exchange_rate_prediction(mock_prediction_rate: int | float,
+                                      mock_current_rate: int | float
+                                      ) -> str:
+    if (mock_prediction_rate > mock_current_rate
+            + (mock_current_rate / 100 * 5)):
+        return "Buy more cryptocurrency"
+    elif (mock_prediction_rate > mock_current_rate
+          - (mock_current_rate / 100 * 5)):
+        return "Sell all your cryptocurrency"
+    return "Do nothing"
