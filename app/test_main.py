881af99893c46965842bf4d87e4595c594bc3b92
TimoteoRJanzen <timoteojanzen123@gmail.com>
2025-11-21 09:46:50 -0300

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -21,6 +21,11 @@ def test_predict_action_sell(mock_rate_prediction: Any) -> None:
     assert cryptocurrency_action(1) == "Sell all your cryptocurrency"
 
 
-def test_predict_action_do_nothing(mock_rate_prediction: Any) -> None:
+def test_predict_action_do_nothing_low(mock_rate_prediction: Any) -> None:
     mock_rate_prediction.return_value = 0.95
+    assert cryptocurrency_action(1) == "Do nothing"
+
+
+def test_predict_action_do_nothing_high(mock_rate_prediction: Any) -> None:
+    mock_rate_prediction.return_value = 1.05
     assert cryptocurrency_action(1) == "Do nothing"
