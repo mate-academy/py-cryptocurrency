96e02b4414e48d457655a7171312776a36a9a557
alexnrn2204 <alexnrn2204@gmail.com>
2025-12-07 07:26:35 +0200

    develop

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,48 @@
-# write your code here
+from unittest import TestCase, mock
+from app.main import cryptocurrency_action
+
+
+class TestCryptocurrency(TestCase):
+    @mock.patch("app.main.get_exchange_rate_prediction")
+    def test_crypto_action_buy(self,
+                               mock_exchange_rate: mock.MagicMock
+                               ) -> None:
+        mock_exchange_rate.return_value = 110
+
+        result = cryptocurrency_action(100)
+
+        assert result == "Buy more cryptocurrency"
+        mock_exchange_rate.assert_called_once_with(100)
+
+    @mock.patch("app.main.get_exchange_rate_prediction")
+    def test_crypto_action_sell(self,
+                                mock_exchange_rate: mock.MagicMock
+                                ) -> None:
+        mock_exchange_rate.return_value = 100
+
+        result = cryptocurrency_action(110)
+
+        assert result == "Sell all your cryptocurrency"
+        mock_exchange_rate.assert_called_once_with(110)
+
+    @mock.patch("app.main.get_exchange_rate_prediction")
+    def test_crypto_action_0_95(self,
+                                mock_exchange_rate: mock.MagicMock
+                                ) -> None:
+        mock_exchange_rate.return_value = 100
+
+        result = cryptocurrency_action(95)
+
+        assert result == "Do nothing"
+        mock_exchange_rate.assert_called_once_with(95)
+
+    @mock.patch("app.main.get_exchange_rate_prediction")
+    def test_crypto_action_1_05(self,
+                                mock_exchange_rate: mock.MagicMock
+                                ) -> None:
+        mock_exchange_rate.return_value = 100
+
+        result = cryptocurrency_action(105)
+
+        assert result == "Do nothing"
+        mock_exchange_rate.assert_called_once_with(105)
