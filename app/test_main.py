16af77f74cef3ac1443b2bf0fd8bfaf3bd04ba71
Vladislav <ilichoff88@gmail.com>
2025-12-11 16:38:58 +0200

    'Solution1'

--- app/test_main.py
+++ app/test_main.py
@@ -1,50 +1,31 @@
+from typing import Union
 from unittest.mock import patch, Mock
+
+import pytest
+
 from app.main import cryptocurrency_action
 
 
+@pytest.mark.parametrize(
+    "current_rate, exchange_rate, action",
+    [
+        (1.00, 1.10, "Buy more cryptocurrency"),
+        (1.00, 0.90, "Sell all your cryptocurrency"),
+        (1.00, 1.05, "Do nothing"),
+        (1.00, 0.95, "Do nothing"),
+        (1.00, 1.0501, "Buy more cryptocurrency"),
+        (1.00, 0.9499, "Sell all your cryptocurrency"),
+    ],
+)
 @patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action_buy_coin(mock_exchange_rate: Mock) -> None:
-    mock_exchange_rate.return_value = 1.10
+def test_cryptocurrency_action_parametrized(
+    mock_get_exchange_rate_prediction: Mock,
+    current_rate: Union[int, float],
+    exchange_rate: Union[int, float],
+    action: str,
+) -> None:
+    mock_get_exchange_rate_prediction.return_value = exchange_rate
 
-    actual_result = cryptocurrency_action(1.00)
-    assert actual_result == "Buy more cryptocurrency"
+    result = cryptocurrency_action(current_rate)
 
-
-@patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action_sell_coin(mock_exchange_rate: Mock) -> None:
-    mock_exchange_rate.return_value = 0.90
-
-    actual_result = cryptocurrency_action(1.00)
-    assert actual_result == "Sell all your cryptocurrency"
-
-
-@patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action_max_current_rate(mock_exchange_rate: Mock) -> None:
-    mock_exchange_rate.return_value = 1.05
-
-    actual_result = cryptocurrency_action(1.00)
-    assert actual_result == "Do nothing"
-
-
-@patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action_min_current_rate(mock_exchange_rate: Mock) -> None:
-    mock_exchange_rate.return_value = 0.95
-
-    actual_result = cryptocurrency_action(1.00)
-    assert actual_result == "Do nothing"
-
-
-@patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action_buy_just_over_threshold(mock_exchange_rate: Mock) -> None:
-    mock_exchange_rate.return_value = 1.0501
-
-    actual_result = cryptocurrency_action(1.00)
-    assert actual_result == "Buy more cryptocurrency"
-
-
-@patch("app.main.get_exchange_rate_prediction")
-def test_cryptocurrency_action_sell_just_under_threshold(mock_exchange_rate: Mock) -> None:
-    mock_exchange_rate.return_value = 0.9499
-
-    actual_result = cryptocurrency_action(1.00)
-    assert actual_result == "Sell all your cryptocurrency"
+    assert result == action
