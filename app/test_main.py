8a8cf670cf183440d919e1302ec3524ed91b971c
SSSSSADO <grushkovik.danil@gmail.com>
2025-12-12 12:01:16 +0300

    Fix annotations

--- app/test_main.py
+++ app/test_main.py
@@ -3,21 +3,21 @@ from unittest import mock
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_buy_more(mock_prediction):
+def test_buy_more(mock_prediction) -> None:
     current = 100
     mock_prediction.return_value = 106  # +6%
     assert cryptocurrency_action(current) == "Buy more cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_sell_all(mock_prediction):
+def test_sell_all(mock_prediction) -> None:
     current = 100
     mock_prediction.return_value = 90  # -10%
     assert cryptocurrency_action(current) == "Sell all your cryptocurrency"
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_do_nothing(mock_prediction):
+def test_do_nothing(mock_prediction) -> None:
     current = 100
     mock_prediction.return_value = 103  # +3%
     assert cryptocurrency_action(current) == "Do nothing"
