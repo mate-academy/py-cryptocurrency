699e440749015d8b81d1e991d9a37b22063888da
Mykhaylo Zhyhan <mz5157e@gre.ac.uk>
2025-12-17 19:22:05 +0000

    tests with mock function get_exchange_rate_prediction that tests function cryptocurrency_action returns correct actions.

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,6 @@
 from app.main import cryptocurrency_action
 from unittest.mock import patch
+import pytest
 
 
 def test_buy_more_cryptocurrency_when_prediction_higher_5_percent() -> None:
@@ -16,8 +17,14 @@ def test_sell_cryptocurrency_prediction_lower_more_than_5_percent() -> None:
     assert result == "Sell all your cryptocurrency"
 
 
-def do_nothing() -> None:
+@pytest.mark.parametrize(
+    "prediction_rate",
+    [95, 105]  # boundary values included
+)
+def test_do_nothing(prediction_rate):
     current_rate = 100
-    for prediction_rate in (95, 105):
-        with patch("app.main.get_exchange_rate_prediction", prediction_rate):
-            assert cryptocurrency_action(current_rate) == "Do nothing"
+
+    with patch("app.main.get_exchange_rate_prediction", return_value=prediction_rate):
+        result = cryptocurrency_action(current_rate)
+
+    assert result == "Do nothing"
