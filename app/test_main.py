75652253520c281725a0e576b820d0937bfe1595
Artem <artem90078@gmail.com>
2025-12-31 18:02:39 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,32 @@
-# write your code here
+from unittest import mock
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_buy(mock_prediction):
+    mock_prediction.return_value = 105.1
+    assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_sell(mock_prediction):
+    mock_prediction.return_value = 94.9
+    assert cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_do_nothing_upper_boundary(mock_prediction):
+    mock_prediction.return_value = 105.0
+    assert cryptocurrency_action(100) == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_do_nothing_lower_boundary(mock_prediction):
+    mock_prediction.return_value = 95.0
+    assert cryptocurrency_action(100) == "Do nothing"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action_do_nothing_equal(mock_prediction):
+    mock_prediction.return_value = 100.0
+    assert cryptocurrency_action(100) == "Do nothing"
