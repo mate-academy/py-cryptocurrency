53fc6eaef2e3c4dbc86bd4ec89e8b2bf5fc0ae61
Bohdan <bogdanbelyj@gmail.com>
2026-01-06 21:23:09 -0700

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+from unittest import mock
+from app.main import cryptocurrency_action
+from typing import Union
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_should_buy_when_prediction_increase(
+        mocked_rate: Union[int, float]
+) -> None:
+    mocked_rate.return_value = 120
+    result = cryptocurrency_action(100)
+    assert result == "Buy more cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_should_sell_when_prediction_decrease(
+        mocked_rate: Union[int, float]
+) -> None:
+    mocked_rate.return_value = 90
+    result = cryptocurrency_action(100)
+    assert result == "Sell all your cryptocurrency"
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_should_do_nothing_when_prediction_stay_stable(
+        mocked_rate: Union[int, float]
+) -> None:
+    mocked_rate.return_value = 102
+    result = cryptocurrency_action(100)
+    assert result == "Do nothing"
