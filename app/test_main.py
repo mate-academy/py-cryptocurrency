b240f53a5c4d46a00135bbabbb3ece56247ed15a
chojanpineapple <johan.apr99@gmail.com>
2026-01-14 16:32:41 +0000

    test

--- app/test_main.py
+++ app/test_main.py
@@ -7,23 +7,20 @@ def test_buy_more_crypto() -> None:
         "app.main.get_exchange_rate_prediction",
         return_value=110.0
     ):
-        result = cryptocurrency_action(100.0)
-        assert result == "Buy more cryptocurrency"
+        assert cryptocurrency_action(100.0) == "Buy more cryptocurrency"
 
 
 def test_sell_all_crypto() -> None:
     with mock.patch(
         "app.main.get_exchange_rate_prediction",
-        return_value=90.0
+        return_value=94.0
     ):
-        result = cryptocurrency_action(100.0)
-        assert result == "Sell all your cryptocurrency"
+        assert cryptocurrency_action(100.0) == "Sell all your cryptocurrency"
 
 
 def test_do_nothing() -> None:
     with mock.patch(
         "app.main.get_exchange_rate_prediction",
-        return_value=103.0
+        return_value=100.0
     ):
-        result = cryptocurrency_action(100.0)
-        assert result == "Do nothing"
+        assert cryptocurrency_action(100.0) == "Do nothing"
