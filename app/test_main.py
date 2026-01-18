2d0c2f2f93233af23c98b93855891b86aa45e2f5
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2026-01-18 15:02:06 +0100

    created test for cryptocurency_action function with patch on get_exchange_rate_prediction func, parametrized

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from collections.abc import Callable
+from unittest import mock
+from app.main import cryptocurrency_action
+
+import pytest
+
+@pytest.mark.parametrize(
+    "current_rate, prediction_rate, expected_result",
+    [
+        (100.0, 106.0, "Buy more cryptocurrency"),
+        (100.0, 120.0, "Buy more cryptocurrency"),
+        (100.0, 94.0, "Sell all your cryptocurrency"),
+        (100.0, 80.0, "Sell all your cryptocurrency"),
+        (100.0, 95.0, "Do nothing"),
+        (100.0, 100.0, "Do nothing"),
+        (100.0, 105.0, "Do nothing"),
+    ]
+)
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_cryptocurrency_action(
+        mocked_get_exchange_rate_prediction: Callable,
+        current_rate: float,
+        prediction_rate: float,
+        expected_result: str):
+    mocked_get_exchange_rate_prediction.return_value = prediction_rate
+    assert cryptocurrency_action(current_rate) == expected_result
